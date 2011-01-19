4488c67ee92081a2809c2bc3ec57038a6664be6f
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-01-19 13:54:05 +0000

    GPIO macros functional
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1280 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/kernel/include/OS_Family_Defines.h
+++ system/hal/arch/avr32/uc3/kernel/include/OS_Family_Defines.h
@@ -7,12 +7,24 @@
 #ifndef HAL_FAMILY_OS_DEFS_H_
 #define HAL_FAMILY_OS_DEFS_H_
 
-#define OS_GPIO_PORT_CONFIG(_PORT_CONFIG, _BIT, _VALUE)
-#define OS_GPIO_PORT_HIGH(_PORT, _BIT)
-#define OS_GPIO_PORT_LOW(_PORT, _BIT)
-#define OS_GPIO_PORT_TOGGLE(_PORT, _BIT)
+#include <avr32/io.h>
 
-#define OS_GPIO_PORT_ISHIGH(_PORT, _BIT)	(false)
-#define OS_GPIO_PORT_ISLOW(_PORT, _BIT)		(true)
+// _VALUE == 1 output
+// _VALUE == 0 input
+#define OS_GPIO_PORT_CONFIG(_PORT_CONFIG, _BIT, _VALUE) \
+	{ \
+	(AVR32_GPIO.port[(_BIT)>>5].gpers = (1<< ((_BIT) & 0x1F))); \
+	if( _VALUE > 0) \
+	(AVR32_GPIO.port[(_BIT)>>5].oders = (1<< ((_BIT) & 0x1F))); \
+        else \
+	(AVR32_GPIO.port[(_BIT)>>5].oderc = (1<< ((_BIT) & 0x1F))); \
+	}
+
+#define OS_GPIO_PORT_HIGH(_PORT, _BIT) (AVR32_GPIO.port[(_BIT)>>5].ovrs = (1<< ((_BIT) & 0x1F)))
+#define OS_GPIO_PORT_LOW(_PORT, _BIT) (AVR32_GPIO.port[(_BIT)>>5].ovrc = (1<< ((_BIT) & 0x1F)))
+#define OS_GPIO_PORT_TOGGLE(_PORT, _BIT) (AVR32_GPIO.port[(_BIT)>>5].ovrt = (1<< ((_BIT) & 0x1F)))
+
+#define OS_GPIO_PORT_ISHIGH(_PORT, _BIT)	((AVR32_GPIO.port[(_BIT)>>5].pvr & (1<< ((_BIT) & 0x1F)))!=0 ? true : false)
+#define OS_GPIO_PORT_ISLOW(_PORT, _BIT)		((AVR32_GPIO.port[(_BIT)>>5].pvr & (1<< ((_BIT) & 0x1F)))==0 ? true : false)
 
 #endif /* HAL_FAMILY_OS_DEFS_H_ */
