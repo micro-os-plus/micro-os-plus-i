0be3883d235087fc617d0e3e639a745a50a9177b
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-01-19 14:03:17 +0000

    gpio ports defined
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1286 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/boards/Atmel/evk1104/include/DeviceDebugI2C_Board_Defines.h
+++ system/hal/boards/Atmel/evk1104/include/DeviceDebugI2C_Board_Defines.h
@@ -7,16 +7,18 @@
 #ifndef HAL_BOARD_DEVICEDEBUGI2C_DEFINES_H_
 #define HAL_BOARD_DEVICEDEBUGI2C_DEFINES_H_
 
+#include <avr32/io.h>
+
 #define OS_CFGINT_DEBUG_I2C_EMU_INIT_DELAY_COUNT        (100)
 #define OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT             (2)
 
-#define OS_CONFIG_DEBUG_SCL_PORT                        GPIOC
-#define OS_CONFIG_DEBUG_SCL_PORT_CONFIG                 OS_CONFIG_DEBUG_SCL_PORT->CRL
-#define OS_CONFIG_DEBUG_SCL_BIT                         6
+#define OS_CONFIG_DEBUG_SCL_PORT                        GPIO
+#define OS_CONFIG_DEBUG_SCL_PORT_CONFIG                 OS_CONFIG_DEBUG_SCL_PORT
+#define OS_CONFIG_DEBUG_SCL_BIT                         AVR32_PIN_PA03
 
-#define OS_CONFIG_DEBUG_SDA_PORT                        GPIOC
-#define OS_CONFIG_DEBUG_SDA_PORT_CONFIG                 OS_CONFIG_DEBUG_SDA_PORT->CRL
-#define OS_CONFIG_DEBUG_SDA_BIT                         7
+#define OS_CONFIG_DEBUG_SDA_PORT                        GPIO
+#define OS_CONFIG_DEBUG_SDA_PORT_CONFIG                 OS_CONFIG_DEBUG_SDA_PORT
+#define OS_CONFIG_DEBUG_SDA_BIT                         AVR32_PIN_PA04
 
 
 #include "hal/boards/AVI/include/DeviceDebugI2CAddr_Defines.h"
