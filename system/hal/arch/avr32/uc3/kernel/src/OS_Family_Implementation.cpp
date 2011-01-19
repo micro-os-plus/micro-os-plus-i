2d0a860a2c7d035fd525fa5ce597c145b75f8fca
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-01-19 13:59:44 +0000

    minimum hw init
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1283 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/kernel/src/OS_Family_Implementation.cpp
+++ system/hal/arch/avr32/uc3/kernel/src/OS_Family_Implementation.cpp
@@ -8,61 +8,70 @@
 
 #if defined(OS_CONFIG_FAMILY_AVR32UC3)
 
+#include "hal/arch/avr32/uc3/lib/include/pm.h"
 
-void __init_data_and_bss(void) __attribute__((noinline));
-void __init_static_constructors(void) __attribute__((noinline));
+void
+__init_data_and_bss(void) __attribute__((noinline));
+void
+__init_static_constructors(void) __attribute__((noinline));
 
-extern "C" int main(void);
+extern "C" int
+main(void);
 
-extern "C" void os_processor_init(void) __attribute__( ( naked ));
+extern "C" void
+os_processor_init(void) __attribute__( ( naked ));
 
 //int xx = 0x12345678;
 
-void os_processor_init(void)
-  {
+void
+os_processor_init(void)
+{
   asm volatile (
       " lda.w   sp, _estack\n"
       :::
   );
-#if false
-    SystemInit();
+#if true
+  // Switch to external Oscillator 0
+  pm_switch_to_osc0(&AVR32_PM, OS_CFGLONG_OSCILLATOR_HZ, AVR32_PM_OSCCTRL0_STARTUP_2048_RCOSC);
+  gpio_local_init();
 #endif
 
-    // be sure we start with interrupts disabled
-    OS::interruptsDisable();
+  // be sure we start with interrupts disabled
+  OS::interruptsDisable();
 
-    OSScheduler::ledActiveInit();
-    OSScheduler::ISRledActiveOn();
+  OSScheduler::ledActiveInit();
+  OSScheduler::ISRledActiveOn();
 
 #if false
-    wdt_enable(WDTO_2S);
+  wdt_enable(WDTO_2S);
 
 #if !defined(OS_EXCLUDE_MULTITASKING)
-    set_sleep_mode(SLEEP_MODE_IDLE);
-    sleep_enable();
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
 #endif
 #endif
 
-    __init_data_and_bss();
+  __init_data_and_bss();
 
 #if defined(DEBUG)
-    OSDeviceDebug::earlyInit();
+  OSDeviceDebug::earlyInit();
 #endif
 
-    OS::earlyInit();
+  OS::earlyInit();
 
-    //__init_static_constructors();
+  //__init_static_constructors();
 
-    main(); // call standard main()
+  main(); // call standard main()
 
-    for (;;)
-      ;
-  }
+  for (;;)
+    ;
+}
 
-void OS::saveResetBits(void)
-  {
-    ms_resetBits = -1;
-  }
+void
+OS::saveResetBits(void)
+{
+  ms_resetBits = -1;
+}
 
 #endif /* OS_CONFIG_FAMILY_AVR32UC3 */
 
