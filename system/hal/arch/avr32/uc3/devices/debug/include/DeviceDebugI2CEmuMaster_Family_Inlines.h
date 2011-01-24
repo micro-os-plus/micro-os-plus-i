c7bee4d0efc9bf9b5a3849773db7725fa793be9d
Liviu Ionescu <ilg@livius.net>
2011-01-24 00:22:13 +0000

    updated with new OUTPUT/INPUT GPIO macros; re-indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1309 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/debug/include/DeviceDebugI2CEmuMaster_Family_Inlines.h
+++ system/hal/arch/avr32/uc3/devices/debug/include/DeviceDebugI2CEmuMaster_Family_Inlines.h
@@ -1,5 +1,5 @@
 /*
- *	Copyright (C) 2009 Liviu Ionescu.
+ *	Copyright (C) 2011 Liviu Ionescu.
  *
  *	This file is part of the uOS++ distribution.
  */
@@ -11,125 +11,113 @@
 
 // emulate i2c master by bitbanging
 
-inline bool OSDeviceDebug::implCanTransmit(void)
-  {
-    return true;
-  }
+inline bool
+OSDeviceDebug::implCanTransmit(void)
+{
+  return true;
+}
 
-inline bool OSDeviceDebug::implDevicePresent(void)
-  {
-    return !masterSdaIsLow();
-  }
+inline bool
+OSDeviceDebug::implDevicePresent(void)
+{
+  return !masterSdaIsLow();
+}
 
-inline void OSDeviceDebug::implWDReset(void)
-  {
+inline void
+OSDeviceDebug::implWDReset(void)
+{
 #if false
-    IWDG_ReloadCounter();
+  IWDG_ReloadCounter();
 #endif
-  }
+}
 
 // ---
 
-inline void masterInit(void)
-  {
-    // enable clock for APB2 where GPIOC is located
+inline void
+masterInit(void)
+{
+  ;
+}
+
+inline void
+masterSclInit(void)
+{
+  OS_GPIO_PORT_LOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT);
+  OS_GPIO_PORT_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+masterSclHigh(void)
+{
+  // The pull-up will do the job
+  OS_GPIO_PORT_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+masterSclLow(void)
+{
+  OS_GPIO_PORT_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline bool
+masterSclIsLow(void)
+{
+  return OS_GPIO_PORT_ISLOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT) ? true
+      : false;
+}
+
+inline void
+masterSdaInit(void)
+{
+  OS_GPIO_PORT_LOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT);
+  OS_GPIO_PORT_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+masterSdaHigh(void)
+{
+  // The pull-up will do the job
+  OS_GPIO_PORT_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+masterSdaLow(void)
+{
+  OS_GPIO_PORT_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline bool
+masterSdaIsLow(void)
+{
+  return OS_GPIO_PORT_ISLOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT) ? true
+      : false;
+}
+
+inline void
+masterSclSynchronize(void)
+{
+  while (masterSclIsLow())
+    {
 #if false
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-
-    // code to blink the debug leds before testing I2C debug
-    OS_CONFIG_DEBUG_SCL_PORT->CRH = (OS_CONFIG_DEBUG_SCL_PORT->CRH
-        & ~_CBV(12, 0x3, 0x3)) | _CBV(12, GPIO_Mode_Out_PP, GPIO_Speed_50MHz);
-    OS_CONFIG_DEBUG_SCL_PORT->CRH = (OS_CONFIG_DEBUG_SCL_PORT->CRH
-        & ~_CBV(10, 0x3, 0x3)) | _CBV(10, GPIO_Mode_Out_PP, GPIO_Speed_50MHz);
-
-    int i;
-    i = 0;
-    while (i < 10)
-      {
-        if ((i & 1) != 0)
-          GPIOC->BRR |= _BV(12);
-        else
-          GPIOC->BSRR |= _BV(12);
-
-        if ((i & 2) != 0)
-          GPIOC->BRR |= _BV(10);
-        else
-          GPIOC->BSRR |= _BV(10);
-
-        int delay;
-        delay = 500000;
-        while (delay)
-          {
-            asm volatile( "nop":: );
-            delay--;
-          }
-
-        i++;
-      }
+      IWDG_ReloadCounter();
 #endif
-  }
+    }
+}
 
-inline void masterSclInit(void)
-  {
-    OS_GPIO_PORT_LOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT);
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SCL_PORT_CONFIG, OS_CONFIG_DEBUG_SCL_BIT, 0);
-  }
-
-inline void masterSclHigh(void)
-  {
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SCL_PORT_CONFIG, OS_CONFIG_DEBUG_SCL_BIT, 0);
-  }
-
-inline void masterSclLow(void)
-  {
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SCL_PORT_CONFIG, OS_CONFIG_DEBUG_SCL_BIT, 1);
-  }
-
-inline bool masterSclIsLow(void)
-  {
-    return OS_GPIO_PORT_ISLOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT) ? true
-        : false;
-  }
-
-inline void masterSdaInit(void)
-  {
-    OS_GPIO_PORT_LOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT);
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SDA_PORT_CONFIG, OS_CONFIG_DEBUG_SDA_BIT, 0);
-  }
-
-inline void masterSdaHigh(void)
-  {
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SDA_PORT_CONFIG, OS_CONFIG_DEBUG_SDA_BIT, 0);
-  }
-
-inline void masterSdaLow(void)
-  {
-    OS_GPIO_PORT_CONFIG(OS_CONFIG_DEBUG_SDA_PORT_CONFIG, OS_CONFIG_DEBUG_SDA_BIT, 1);
-  }
-
-inline bool masterSdaIsLow(void)
-  {
-    return OS_GPIO_PORT_ISLOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT) ? true
-        : false;
-  }
-
-inline void masterSclSynchronize(void)
-  {
-    while (masterSclIsLow())
-      {
+inline void
+masterSdaSynchronize(void)
+{
+  while (masterSdaIsLow())
+    {
 #if false
-    	IWDG_ReloadCounter();
+      IWDG_ReloadCounter();
 #endif
-      }
-  }
-
-inline void masterSdaSynchronize(void)
-  {
-    while (masterSdaIsLow())
-      {
-#if false
-        IWDG_ReloadCounter();
-#endif
-      }
-  }
+    }
+}
 #endif /*HAL_DEVICEDEBUGI2CEMUMASTER_INLINES_H_*/
