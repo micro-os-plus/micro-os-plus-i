ed81017a260b0b38e6c775353d6f26832091aa95
Liviu Ionescu <ilg@livius.net>
2011-01-24 00:20:08 +0000

    idle defined as sleep 0
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1307 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/kernel/include/OS_Family_Inlines.h
+++ system/hal/arch/avr32/uc3/kernel/include/OS_Family_Inlines.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2009 Liviu Ionescu.
+ *      Copyright (C) 2011 Liviu Ionescu.
  *
  *	This file is part of the uOS++ distribution.
  */
@@ -9,61 +9,57 @@
 
 #include <avr32/io.h>
 #include "hal/arch/avr32/lib/include/compiler.h"
+#include "hal/arch/avr32/uc3/lib/include/pm.h"
 
-inline void OSImpl::CPUidle(void)
-  {
-    asm volatile("	":::);
+extern unsigned long _evba;
 
-#if false
-    set_sleep_mode(SLEEP_MODE_IDLE);
-    sleep_cpu();
-#endif
-  }
+inline void
+OSImpl::CPUidle(void)
+{
+  asm volatile
+  (
+      " sleep 0 \n"
+      :::
+  );
+}
 
-inline void OSImpl::CPUsleep(void)
-  {
-    asm volatile("	":::);
+inline void
+OSImpl::CPUsleep(void)
+{
+  asm volatile
+  (
+      " sleep 0 \n"     /* To be updated !!! */
+      :::
+  );
+}
 
-#if false
-    set_sleep_mode(SLEEP_MODE_EXT_STANDBY);
-    sleep_cpu();
-#endif
-  }
+inline void
+OSImpl::CPUdeepSleep(void)
+{
+  asm volatile
+  (
+      " sleep 0 \n"     /* To be updated !!! */
+      :::
+  );
+}
 
-inline void OSImpl::CPUdeepSleep(void)
-  {
-    asm volatile("	":::);
-
-#if false
-    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-    sleep_cpu();
-#endif
-  }
-
-inline void OSImpl::WDTreset(void)
-  {
-    ;
-  }
+inline void
+OSImpl::WDTreset(void)
+{
+  ;
+}
 
 // as per manual, JMP 0 is not recommended since
 // registers are not set to their initial status
-inline void OSImpl::SOFTreset(void)
-  {
-#if false
-    wdt_enable(WDTO_15MS);
-    cli();
-#endif
-    for (;;)
-      ; // trigger WD
-  }
-
-#if (defined __GNUC__)
-__attribute__((__always_inline__))
-#endif
-extern __inline__ void gpio_local_init(void)
+inline void
+OSImpl::SOFTreset(void)
 {
-  Set_system_register(AVR32_CPUCR,
-                      Get_system_register(AVR32_CPUCR) | AVR32_CPUCR_LOCEN_MASK);
+#if false
+  wdt_enable(WDTO_15MS);
+  cli();
+#endif
+  for (;;)
+    ; // trigger WD
 }
 
 #endif /*HAL_FAMILY_OS_INLINES_H_*/
