858682dfaa98bc95ddaf8d34b257d330238d4d51
Liviu Ionescu <ilg@livius.net>
2011-01-24 00:33:12 +0000

    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1315 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ samples/blinkX3Coop/include/LedVar.h
@@ -0,0 +1,52 @@
+/*
+ *	Copyright (C) 2007-2008 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#ifndef LEDVAR_H_
+#define LEDVAR_H_
+
+#include "portable/kernel/include/uOS.h"
+
+class LedVar
+  {
+public:
+  LedVar(unsigned char iBit);
+
+  void toggle(void);
+  void init(void);
+
+  unsigned char bitNumber(void);
+
+private:
+  unsigned char m_iBit;
+  };
+
+inline LedVar::LedVar(unsigned char iBit)
+  {
+    m_iBit = iBit;
+  }
+
+inline void LedVar::init(void)
+  {
+    // init led port as output
+    OS_GPIO_PORT_CONFIG_OUTPUT(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+  }
+
+inline void LedVar::toggle(void)
+  {
+    os.sched.criticalEnter();
+      {
+        // toggle led
+        OS_GPIO_PORT_TOGGLE(APP_CONFIG_LED_PORT, m_iBit);
+      }
+    os.sched.criticalExit();
+  }
+
+inline unsigned char LedVar::bitNumber(void)
+  {
+    return m_iBit;
+  }
+
+#endif /*LEDVAR_H_*/
---
+++ samples/blinkX3Coop/src/main.cpp
@@ -0,0 +1,42 @@
+/*
+ *	Copyright (C) 2007-2008 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/uOS.h"
+
+#include "TaskBlink.h"
+
+// ---------------------------------------------------------------------------
+
+/*
+ * Perform a series of short busy wait steps of 1 tick separated
+ * by yields.
+ */
+
+void
+delayWithYield(schedTicks_t n)
+{
+  for (; n--;)
+    {
+      os.busyWaitMillis(1); // busy wait one tick
+      os.sched.yield(); // release control to next task
+    }
+}
+
+// ---------------------------------------------------------------------------
+
+#define MILLIS  (1000/3)
+
+// tasks allocated on static storage
+TaskBlink task1("A", APP_CONFIG_LED1, MILLIS * 1);
+TaskBlink task2("B", APP_CONFIG_LED2, MILLIS * 2);
+TaskBlink task3("C", APP_CONFIG_LED3, MILLIS * 4);
+
+// ---------------------------------------------------------------------------
+
+// message to be displayed on the debugging device at startup
+#if defined(DEBUG)
+const char greeting[] = "AVI blinkX3Coop v1.1";
+#endif
