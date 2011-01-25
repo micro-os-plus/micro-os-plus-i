7873b1a07404389bc155baf7e0b08396d31c286b
Liviu Ionescu <ilg@livius.net>
2011-01-25 22:49:04 +0000

    functional system timer init and routine
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1342 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/kernel/src/OSTimer_Arch_Implementation.cpp
+++ system/hal/arch/avr32/kernel/src/OSTimer_Arch_Implementation.cpp
@@ -11,94 +11,150 @@
 #if defined(OS_CONFIG_ARCH_AVR32)
 
 #include "portable/kernel/include/OS.h"
+
+#include <avr32/io.h>
 #include "hal/arch/avr32/lib/include/compiler.h"
 #include "hal/arch/avr32/uc3/lib/include/intc.h"
 #include "hal/arch/avr32/uc3/lib/include/pm.h"
 
+#if defined(DEBUG) && defined(OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS)
 static int i;
-
-
-__attribute__((__interrupt__)) void TikTestIsr(void);
-
-void OSTimerTicks::init(void)
-  {
-  volatile avr32_pm_t* pm = &AVR32_PM;
-  volatile avr32_tc_t* tc_reg = &AVR32_TC0;
-#if true
-  //initialize  exception vector base address
-  INTC_init_interrupts();
-  // enable clock for TC0
-  pm->pbamask |= 1 << 16;
-  // initialize registers for every Channel
-  // disable channel
-  tc_reg->channel[0].CCR.clkdis = 1;
-  // clear interrupt flags, by reading SR
-  tc_reg->channel[0].sr;
-  // reset register
-  tc_reg->channel[0].cmr = 0;
-  // set waveform mode as default
-  tc_reg->channel[0].CMR.waveform.wave = 1;
-  // for waveform mode the TIOB must not be input
-  tc_reg->channel[0].CMR.waveform.eevt = 2;
-  //disable all interrupt sources
-  tc_reg->channel[0].idr = 0xFFFFFFFF;
-  //register the interrupt
-  INTC_register_interrupt(TikTestIsr,AVR32_TC0_IRQ0,0);
-  //clock source PBA/2;
-  tc_reg->channel[0].CMR.waveform.tcclks = 1;
-  // counter UP mode with automatic trigger on RC Compare
-  tc_reg->channel[0].CMR.waveform.wavsel = 2;
-  // no event on TIOA when RC compare
-  tc_reg->channel[0].CMR.waveform.acpc = 0;
-  // no event on TIOB when RC compare
-  tc_reg->channel[0].CMR.waveform.bcpc = 0;
-  // enable channel, as it might be disabled
-  tc_reg->channel[0].CCR.clken = 1;
-  // start channel using software trigger
-  tc_reg->channel[0].CCR.swtrg = 1;
-  // set RC value
-  tc_reg->channel[0].RC.rc = 0xFFFF;
-   // counter UP mode with automatic trigger on RC Compare
-  tc_reg->channel[0].CMR.waveform.wavsel = 2;
-   // set interrupt source RC Compare
-  tc_reg->channel[0].IER.cpcs = 1;
 #endif
+
+__attribute__((__interrupt__)) void
+TikTestIsr(void);
+
+__attribute__((__interrupt__)) void
+SysTick_Handler(void);
+
+void
+OSTimerTicks::init(void)
+{
 #if defined(DEBUG)
 
-    OSDeviceDebug::putString("OSTimerTicks::init()");
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("OSTimerTicks::init()");
+  OSDeviceDebug::putNewLine();
 
 #endif
 
-    i = 0;
-  }
+  volatile avr32_pm_t* pm = &AVR32_PM;
+  volatile avr32_tc_t* tc_reg = &OS_CFGVAR_TIMER;
 
-void SysTick_Handler(void)
-  {
-    /* This is an interrupt function. */
-    OSScheduler::ISRledActiveOn();
+#if true
 
-    OSScheduler::timerTicks.interruptServiceRoutine();
+  //initialise  exception vector base address
+  INTC_init_interrupts();
+
+  // enable clock for TC0
+#if OS_CFGINT_TIMER_ID == 0
+  pm->pbamask |= 1 << 16;
+#elif OS_CFGINT_TIMER_ID == 1
+  pm->pbamask |= 1 << 17;
+#else
+#error "Illegal OS_CFGINT_TIMER_ID"
+#endif
+
+  // initialise registers for every Channel
+
+  // disable channel
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CCR.clkdis = 1;
+
+  // clear interrupt flags, by reading SR
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].sr;
+
+  // reset register
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].cmr = 0;
+
+  // set waveform mode as default
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.wave = 1;
+
+  // for waveform mode the TIOB must not be input
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.eevt = 2;
+  //disable all interrupt sources
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].idr = 0xFFFFFFFF;
+
+  //register the interrupt
+  INTC_register_interrupt(SysTick_Handler, OS_CFGINT_TIMER_IRQ_ID,
+      OS_CFGINT_TIMER_IRQ_LEVEL);
+
+  //clock source
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.tcclks
+      = OS_CFGINT_TIMER_CLOCK_SELECT;
+
+  // counter UP mode with automatic trigger on RC Compare
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.wavsel = 2;
+  // no event on TIOA when RC compare
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.acpc = 0;
+  // no event on TIOB when RC compare
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.bcpc = 0;
+  // enable channel, as it might be disabled
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CCR.clken = 1;
+
+  // start channel using software trigger
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CCR.swtrg = 1;
+
+#define DIVIDER (1 << ((OS_CFGINT_TIMER_CLOCK_SELECT-2)*2+1))
+#define COUNTER (OS_CFGLONG_OSCILLATOR_HZ/OS_CFGINT_TIMER_PRESCALLER/DIVIDER/OS_CFGINT_TICK_RATE_HZ)
+
+#if defined(DEBUG)
+
+  OSDeviceDebug::putDec(OS_CFGINT_TIMER_PRESCALLER);
+  OSDeviceDebug::putChar(',');
+  OSDeviceDebug::putDec(DIVIDER);
+  OSDeviceDebug::putChar(',');
+  OSDeviceDebug::putDec(COUNTER);
+  OSDeviceDebug::putNewLine();
+
+#endif
+
+  // set RC value
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].RC.rc = COUNTER;
+
+  // counter UP mode with automatic trigger on RC Compare
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].CMR.waveform.wavsel = 2;
+
+  // set interrupt source RC Compare
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].IER.cpcs = 1;
+
+#endif
+
+#if defined(DEBUG) && defined(OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS)
+  i = 0;
+#endif
+}
+
+__attribute__((__interrupt__)) void
+SysTick_Handler(void)
+{
+  // Clear TC interrupt
+  // TODO: check if we do it first or last?
+  volatile avr32_tc_t* tc_reg = &OS_CFGVAR_TIMER;
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].sr;
+
+  /* This is an interrupt function. */
+  OSScheduler::ISRledActiveOn();
+
+  OSScheduler::timerTicks.interruptServiceRoutine();
 
 #if defined(OS_INCLUDE_OSSCHEDULER_TIMERSECONDS_SOFT)
-    if (++OSTimerSeconds::ms_schedulerTicks == OS_CFGINT_TICK_RATE_HZ)
-      {
-        OSTimerSeconds::ms_schedulerTicks = 0;
+  if (++OSTimerSeconds::ms_schedulerTicks == OS_CFGINT_TICK_RATE_HZ)
+    {
+      OSTimerSeconds::ms_schedulerTicks = 0;
 
-        OSScheduler::timerSeconds.interruptServiceRoutine();
-      }
+      OSScheduler::timerSeconds.interruptServiceRoutine();
+    }
 #endif
 
-#if defined(DEBUG)
+#if defined(DEBUG) && defined(OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS)
 
-    if ((i++ % OS_CFGINT_TICK_RATE_HZ) == 0)
-      OSDeviceDebug::putChar('!');
+  if ((i++ % OS_CFGINT_TICK_RATE_HZ) == 0)
+    OSDeviceDebug::putChar('!');
 
 #endif
 
-    if (OSScheduler::requireContextSwitch())
-      OSScheduler::ISRcontextSwitchRequest();
-  }
+  //if (OSScheduler::requireContextSwitch())
+  //  OSScheduler::ISRcontextSwitchRequest();
+}
 
 #if defined(OS_INCLUDE_OSSCHEDULER_TIMERSECONDS)
 
@@ -132,18 +188,23 @@ TIMER2_OVF_vect(void)
 
 #endif
 
-__attribute__((__interrupt__)) void TikTestIsr(void)
+#if false
+__attribute__((__interrupt__)) void
+TikTestIsr(void)
 {
-  volatile avr32_tc_t* tc_reg = &AVR32_TC0;
+  // Clear TC interrupt
+  // TODO: should we do it first or last?
+  volatile avr32_tc_t* tc_reg = &OS_CFGVAR_TIMER;
+  tc_reg->channel[OS_CFGINT_TIMER_CHANNEL].sr;
 
- int val =  tc_reg->channel[0].cv;
 #if defined(DEBUG)
 
-    if ((i++ % OS_CFGINT_TICK_RATE_HZ) == 0)
-      OSDeviceDebug::putChar('!');
+  if ((i++ % OS_CFGINT_TICK_RATE_HZ) == 0)
+    OSDeviceDebug::putChar('!');
 
 #endif
 }
+#endif
 
 #endif /* defined(OS_CONFIG_ARCH_AVR32) */
 
