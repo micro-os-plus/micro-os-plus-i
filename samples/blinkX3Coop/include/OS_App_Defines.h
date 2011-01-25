d6e4a89dd2da4753f8bfb4ff8f4e860c068cd3a5
Liviu Ionescu <ilg@livius.net>
2011-01-25 22:52:11 +0000

    explicit timer defs; mark second on debugger
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1343 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3Coop/include/OS_App_Defines.h
+++ samples/blinkX3Coop/include/OS_App_Defines.h
@@ -8,11 +8,11 @@
 #define APP_OS_DEFINES_H_
 
 // override the board definitions, if needed
-//#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
+#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
 
-//#define OS_CFGINT_TICK_RATE_HZ                          (1000)
-//#define OS_CFGINT_TIMER_PRESCALLER                      (64)
-//#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
+#define OS_CFGINT_TICK_RATE_HZ                          (1000)
+#define OS_CFGINT_TIMER_PRESCALLER                      (4)
+#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
 
 #define OS_CFGINT_TASKS_TABLE_SIZE                      (3)
 
@@ -22,6 +22,8 @@
 // One of the following must be defined in Eclipse project or Makefile
 //#define OS_CONFIG_DEBUG_DEVICE_I2C                    1
 //#define OS_CONFIG_DEBUG_DEVICE_USART			1
+
+#define OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS       (1)
 
 //#define OS_DEBUG_OSSCHEDULER_STACKINITIALIZE            (1)
 #define OS_DEBUG_CONSTRUCTORS                           (1)
