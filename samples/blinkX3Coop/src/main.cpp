8d82aff3b618fd6aac9f1e7fdd8029e0ca11fe4f
Liviu Ionescu <ilg@livius.net>
2011-01-25 22:53:17 +0000

    delayWithYield() removed, replaced by sleep()
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1344 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3Coop/src/main.cpp
+++ samples/blinkX3Coop/src/main.cpp
@@ -10,29 +10,12 @@
 
 // ---------------------------------------------------------------------------
 
-/*
- * Perform a series of short busy wait steps of 1 millisecond separated
- * by yields.
- */
-
-void
-delayWithYield(schedTicks_t n)
-{
-  for (; n--;)
-    {
-      os.busyWaitMillis(1); // busy wait one millisecond
-      os.sched.yield(); // release control to next task
-    }
-}
-
-// ---------------------------------------------------------------------------
-
-#define MILLIS  (1000/3)
+#define TICKS  (OS_CFGINT_TICK_RATE_HZ)
 
 // tasks allocated on static storage
-TaskBlink task1("A", APP_CONFIG_LED1, MILLIS * 1);
-TaskBlink task2("B", APP_CONFIG_LED2, MILLIS * 2);
-TaskBlink task3("C", APP_CONFIG_LED3, MILLIS * 4);
+TaskBlink task1("A", APP_CONFIG_LED1, TICKS * 1);
+TaskBlink task2("B", APP_CONFIG_LED2, TICKS * 2);
+TaskBlink task3("C", APP_CONFIG_LED3, TICKS * 4);
 
 // ---------------------------------------------------------------------------
 
