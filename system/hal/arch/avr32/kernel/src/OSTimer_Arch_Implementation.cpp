dbc36526742c2dd32df0293d8ebd71af1dad47bf
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-01-25 14:31:47 +0000

    configure timer
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1338 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/kernel/src/OSTimer_Arch_Implementation.cpp
+++ system/hal/arch/avr32/kernel/src/OSTimer_Arch_Implementation.cpp
@@ -11,12 +11,58 @@
 #if defined(OS_CONFIG_ARCH_AVR32)
 
 #include "portable/kernel/include/OS.h"
-
+#include "hal/arch/avr32/lib/include/compiler.h"
+#include "hal/arch/avr32/uc3/lib/include/intc.h"
+#include "hal/arch/avr32/uc3/lib/include/pm.h"
 
 static int i;
 
+
+__attribute__((__interrupt__)) void TikTestIsr(void);
+
 void OSTimerTicks::init(void)
   {
+  volatile avr32_pm_t* pm = &AVR32_PM;
+  volatile avr32_tc_t* tc_reg = &AVR32_TC0;
+#if true
+  //initialize  exception vector base address
+  INTC_init_interrupts();
+  // enable clock for TC0
+  pm->pbamask |= 1 << 16;
+  // initialize registers for every Channel
+  // disable channel
+  tc_reg->channel[0].CCR.clkdis = 1;
+  // clear interrupt flags, by reading SR
+  tc_reg->channel[0].sr;
+  // reset register
+  tc_reg->channel[0].cmr = 0;
+  // set waveform mode as default
+  tc_reg->channel[0].CMR.waveform.wave = 1;
+  // for waveform mode the TIOB must not be input
+  tc_reg->channel[0].CMR.waveform.eevt = 2;
+  //disable all interrupt sources
+  tc_reg->channel[0].idr = 0xFFFFFFFF;
+  //register the interrupt
+  INTC_register_interrupt(TikTestIsr,AVR32_TC0_IRQ0,0);
+  //clock source PBA/2;
+  tc_reg->channel[0].CMR.waveform.tcclks = 1;
+  // counter UP mode with automatic trigger on RC Compare
+  tc_reg->channel[0].CMR.waveform.wavsel = 2;
+  // no event on TIOA when RC compare
+  tc_reg->channel[0].CMR.waveform.acpc = 0;
+  // no event on TIOB when RC compare
+  tc_reg->channel[0].CMR.waveform.bcpc = 0;
+  // enable channel, as it might be disabled
+  tc_reg->channel[0].CCR.clken = 1;
+  // start channel using software trigger
+  tc_reg->channel[0].CCR.swtrg = 1;
+  // set RC value
+  tc_reg->channel[0].RC.rc = 0xFFFF;
+   // counter UP mode with automatic trigger on RC Compare
+  tc_reg->channel[0].CMR.waveform.wavsel = 2;
+   // set interrupt source RC Compare
+  tc_reg->channel[0].IER.cpcs = 1;
+#endif
 #if defined(DEBUG)
 
     OSDeviceDebug::putString("OSTimerTicks::init()");
@@ -85,6 +131,19 @@ TIMER2_OVF_vect(void)
 #endif
 
 #endif
+
+__attribute__((__interrupt__)) void TikTestIsr(void)
+{
+  volatile avr32_tc_t* tc_reg = &AVR32_TC0;
+
+ int val =  tc_reg->channel[0].cv;
+#if defined(DEBUG)
+
+    if ((i++ % OS_CFGINT_TICK_RATE_HZ) == 0)
+      OSDeviceDebug::putChar('!');
+
+#endif
+}
 
 #endif /* defined(OS_CONFIG_ARCH_AVR32) */
 
