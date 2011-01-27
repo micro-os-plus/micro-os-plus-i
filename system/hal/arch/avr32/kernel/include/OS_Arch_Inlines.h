e1f3bfd491e29ecc7793d4e373111f4a68295935
Liviu Ionescu <ilg@livius.net>
2011-01-27 22:55:13 +0000

    code moved to family file
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1373 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/kernel/include/OS_Arch_Inlines.h
+++ system/hal/arch/avr32/kernel/include/OS_Arch_Inlines.h
@@ -7,63 +7,7 @@
 #ifndef HAL_ARCH_OS_INLINES_H_
 #define HAL_ARCH_OS_INLINES_H_
 
-#include "hal/arch/avr32/lib/include/compiler.h"
-
-inline void
-OSImpl::returnFromInterrupt(void)
-{
-  asm volatile (
-      " rete \n"
-      :::
-  );
-  for (;;)
-    ; // noreturn
-}
-
-inline void
-OSImpl::returnFromSubroutine(void)
-{
-  asm volatile
-  (
-      " mov pc, lr \n"
-      :::
-  );
-  for (;;)
-    ; // noreturn
-}
-
-inline void
-OSImpl::NOP(void)
-{
-  asm volatile
-  (
-      " nop \n"
-      :::
-  );
-}
-
-inline void
-OSImpl::interruptsEnable(void)
-{
-  Enable_global_interrupt();
-}
-
-inline void
-OSImpl::interruptsDisable(void)
-{
-  Disable_global_interrupt();
-}
-
-inline void
-OSImpl::interruptsClearMask(void)
-{
-  ;
-}
-
-inline void
-OSImpl::interruptsSetMask(void)
-{
-  ;
-}
+// placeholder for content common to all families
+// right now everything was put in the family file
 
 #endif /*HAL_ARCH_OS_INLINES_H_*/
