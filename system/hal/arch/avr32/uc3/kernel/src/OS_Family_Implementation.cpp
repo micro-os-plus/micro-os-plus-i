9c38b864869af0a9249c656f1af9dcc06932ef39
Liviu Ionescu <ilg@livius.net>
2011-01-27 23:01:04 +0000

    inits moved to portable section
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1380 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/kernel/src/OS_Family_Implementation.cpp
+++ system/hal/arch/avr32/uc3/kernel/src/OS_Family_Implementation.cpp
@@ -8,75 +8,6 @@
 
 #if defined(OS_CONFIG_FAMILY_AVR32UC3)
 
-void
-os_init_data_and_bss(void) __attribute__((noinline));
-
-void
-os_init_static_constructors(void) __attribute__((noinline));
-
-extern "C" int
-main(void);
-
-extern "C" void
-os_reset_handler(void) __attribute__( ( naked, noreturn ));
-
-//int xx = 0x12345678;
-
-void
-OSImpl::CPUinit(void)
-{
-  // Switch to external Oscillator 0
-  pm_switch_to_osc0(&AVR32_PM, OS_CFGLONG_OSCILLATOR_HZ,
-      AVR32_PM_OSCCTRL0_STARTUP_2048_RCOSC);
-
-  // initialise local bus; without it GPIO does not work
-  Set_system_register(AVR32_CPUCR,
-      Get_system_register(AVR32_CPUCR) | AVR32_CPUCR_LOCEN_MASK);
-
-  // Set up EVBA so interrupts can be enabled later.
-  Set_system_register(AVR32_EVBA, (int)&_evba);
-}
-
-void
-os_reset_handler(void)
-{
-  asm volatile (
-      " lda.w   sp, _estack\n"
-      :::
-  );
-
-  // be sure we start with interrupts disabled
-  OS::interruptsDisable();
-
-  OSImpl::CPUinit();
-
-  OSScheduler::ledActiveInit();
-  OSScheduler::ISRledActiveOn();
-
-  os_init_data_and_bss();
-
-#if false
-  if (xx != 0x12345678)
-    {
-      OSDeviceDebug::putString("dataInit() failed");
-      OSDeviceDebug::putNewLine();
-    }
-#endif
-
-#if defined(DEBUG)
-  OSDeviceDebug::earlyInit();
-#endif
-
-  OS::earlyInit();
-
-  os_init_static_constructors();
-
-  main(); // call standard main()
-
-  // noreturn
-  for (;;)
-    ;
-}
 
 void
 OS::saveResetBits(void)
@@ -92,7 +23,7 @@ os_ex_handle(unsigned short n)
 {
 #if defined(DEBUG)
   OSDeviceDebug::putNewLine();
-  OSDeviceDebug::putString_P(PSTR("Exception = "));
+  OSDeviceDebug::putString("Exception = ");
   OSDeviceDebug::putDec(n);
   OSDeviceDebug::putNewLine();
 #endif
