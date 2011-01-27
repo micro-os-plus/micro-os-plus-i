36b4b834ffbf6b20ee5ac51e8ce56e19adc82045
Liviu Ionescu <ilg@livius.net>
2011-01-27 23:08:22 +0000

    re-indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1386 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3/include/LedVar.h
+++ samples/blinkX3/include/LedVar.h
@@ -10,52 +10,64 @@
 #include "portable/kernel/include/uOS.h"
 
 class LedVar
-  {
+{
 public:
   LedVar(unsigned char iBit);
 
-  void toggle(void);
-  void init(void);
+  void
+  toggle(void);
+  void
+  init(void);
 
-  unsigned char bitNumber(void);
+  unsigned char
+  bitNumber(void);
 
 private:
   unsigned char m_iBit;
-  };
+};
 
-inline LedVar::LedVar(unsigned char iBit)
-  {
-    m_iBit = iBit;
-  }
+inline
+LedVar::LedVar(unsigned char iBit)
+{
+  m_iBit = iBit;
+}
 
 #if !defined(APP_CONFIG_LED_PORT_CONFIG) || !defined(APP_CONFIG_LED_PORT)
 #error "APP_CONFIG_LED_PORT_* missing"
 #endif
 
-inline void LedVar::init(void)
-  {
-    os.sched.criticalEnter();
-      {
-        // init led port as output
-        OS_GPIO_PORT_CONFIG(APP_CONFIG_LED_PORT_CONFIG, m_iBit, 1);
-        OS_GPIO_PORT_HIGH(APP_CONFIG_LED_PORT, m_iBit);
-      }
-    os.sched.criticalExit();
-  }
+inline void
+LedVar::init(void)
+{
+  os.sched.criticalEnter();
+    {
+      // init led port as output
+      OS_GPIO_PIN_CONFIG_OUTPUT(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
 
-inline void LedVar::toggle(void)
-  {
-    os.sched.criticalEnter();
-      {
-        // toggle led
-        OS_GPIO_PORT_TOGGLE(APP_CONFIG_LED_PORT, m_iBit);
-      }
-    os.sched.criticalExit();
-  }
+#if defined(APP_CONFIG_LED_ISACTIVE_LOW)
+      OS_GPIO_PIN_HIGH(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+#else
+      OS_GPIO_PIN_LOW(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+#endif
+    }
+  os.sched.criticalExit();
+}
 
-inline unsigned char LedVar::bitNumber(void)
-  {
-    return m_iBit;
-  }
+inline void
+LedVar::toggle(void)
+{
+  os.sched.criticalEnter();
+    {
+      // toggle led
+      OS_GPIO_PIN_TOGGLE(APP_CONFIG_LED_PORT, m_iBit);
+    }
+  os.sched.criticalExit();
+}
+
+inline unsigned char
+LedVar::bitNumber(void)
+{
+  return m_iBit;
+}
 
 #endif /*LEDVAR_H_*/
