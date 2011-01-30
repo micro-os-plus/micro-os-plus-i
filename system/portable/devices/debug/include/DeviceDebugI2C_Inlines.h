97ab447d2d13685d098bfd08b48fbd13126e47e9
Liviu Ionescu <ilg@livius.net>
2011-01-30 22:35:19 +0000

    common bitbanging code moved here
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1404 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/debug/include/DeviceDebugI2C_Inlines.h
+++ system/portable/devices/debug/include/DeviceDebugI2C_Inlines.h
@@ -72,6 +72,124 @@ extern void masterCleanup(void);
 static const int I2C_WRITE = 0x0;
 static const int I2C_READ = 0x1;
 
+#if !defined(OS_EXCLUDE_OSDEVICEDEBUG_IMPLEMENTATION)
+// ---------------------------------------------------------------------------
+
+// emulate i2c master by bitbanging
+
+inline bool
+OSDeviceDebug::implCanTransmit(void)
+{
+  return true;
+}
+
+inline bool
+OSDeviceDebug::implDevicePresent(void)
+{
+  return !masterSdaIsLow();
+}
+
+inline void
+OSDeviceDebug::implWDReset(void)
+{
+#if false
+  IWDG_ReloadCounter();
+#endif
+}
+
+// ---
+
+inline void
+masterInit(void)
+{
+  ;
+}
+
+inline void
+masterSclInit(void)
+{
+  OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT);
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+masterSclHigh(void)
+{
+  // The pull-up will do the job
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+masterSclLow(void)
+{
+  OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline bool
+masterSclIsLow(void)
+{
+  return OS_GPIO_PIN_ISLOW(OS_CONFIG_DEBUG_SCL_PORT, OS_CONFIG_DEBUG_SCL_BIT) ? true
+      : false;
+}
+
+inline void
+masterSdaInit(void)
+{
+  OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT);
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+masterSdaHigh(void)
+{
+  // The pull-up will do the job
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+masterSdaLow(void)
+{
+  OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline bool
+masterSdaIsLow(void)
+{
+  return OS_GPIO_PIN_ISLOW(OS_CONFIG_DEBUG_SDA_PORT, OS_CONFIG_DEBUG_SDA_BIT) ? true
+      : false;
+}
+
+inline void
+masterSclSynchronize(void)
+{
+  while (masterSclIsLow())
+    {
+#if false
+      IWDG_ReloadCounter();
+#endif
+    }
+}
+
+inline void
+masterSdaSynchronize(void)
+{
+  while (masterSdaIsLow())
+    {
+#if false
+      IWDG_ReloadCounter();
+#endif
+    }
+}
+
+#endif /* OS_EXCLUDE_OSDEVICEDEBUG_IMPLEMENTATION */
+
+
 #if defined(OS_CONFIG_ARCH_AVR8)
 #include "hal/arch/avr8/devices/debug/include/DeviceDebugI2CEmuMaster_Inlines.h"
 
