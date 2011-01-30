5d1fb37c5727c5e35e4dacfb3b03c2f0f72e8246
Liviu Ionescu <ilg@livius.net>
2011-01-30 22:38:41 +0000

    ledActive code moved to portable folders
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1409 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/kernel/include/OSScheduler_Arch_Inlines.h
+++ system/hal/arch/avr32/kernel/include/OSScheduler_Arch_Inlines.h
@@ -7,68 +7,8 @@
 #ifndef HAL_ARCH_OSSCHEDULER_INLINES_H_
 #define HAL_ARCH_OSSCHEDULER_INLINES_H_
 
-#include "hal/arch/avr32/lib/include/compiler.h"
+// placeholder for content common to all families
 
-//------------------------------------------------------------------------------------
-
-/*
- * Manage the activity LED
- */
-inline void OSScheduler::ledActiveInit(void)
-  {
-#if defined(OS_CONFIG_ACTIVE_LED_PORT) && defined(OS_CONFIG_ACTIVE_LED_PORT_CONFIG)
-        OS_GPIO_PIN_CONFIG_ENABLE(OS_CONFIG_ACTIVE_LED_PORT_CONFIG, OS_CONFIG_ACTIVE_LED_BIT);
-        // Turn off led, i.e. HIGH if active low
-#if defined(OS_CONFIG_ACTIVE_LED_ISACTIVE_LOW)
-        OS_GPIO_PIN_HIGH(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#else
-        OS_GPIO_PIN_LOW(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#endif
-        OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_ACTIVE_LED_PORT_CONFIG, OS_CONFIG_ACTIVE_LED_BIT);
-#else
-#error "OS_CONFIG_ACTIVE_LED_* missing"
-#endif
-  }
-
-/* Turn LED on (on interrupts) */
-inline void OSScheduler::ISRledActiveOn(void)
-  {
-#if defined(OS_CONFIG_ACTIVE_LED_ISACTIVE_LOW)
-    OS_GPIO_PIN_LOW(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#else
-    OS_GPIO_PIN_HIGH(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#endif
-   }
-
-// TODO: if GPIO actions are atomic, we need no critical sections
-inline void OSScheduler::ledActiveOn(void)
-  {
-    OSScheduler::criticalEnter();
-      {
-        ISRledActiveOn();
-      }
-    OSScheduler::criticalExit();
-  }
-
-/* Turn LED off (on interrupts) */
-inline void OSScheduler::ISRledActiveOff(void)
-  {
-#if defined(OS_CONFIG_ACTIVE_LED_ISACTIVE_LOW)
-    OS_GPIO_PIN_HIGH(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#else
-    OS_GPIO_PIN_LOW(OS_CONFIG_ACTIVE_LED_PORT, OS_CONFIG_ACTIVE_LED_BIT);
-#endif
-  }
-
-/* Turn LED off (at sleep) */
-inline void OSScheduler::ledActiveOff(void)
-  {
-    OSScheduler::criticalEnter();
-      {
-        ISRledActiveOff();
-      }
-    OSScheduler::criticalExit();
-    }
-
+// right now everything was defined in the family file
 
 #endif /*HAL_ARCH_OSSCHEDULER_INLINES_H_*/
