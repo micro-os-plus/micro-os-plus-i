a6f941fbf9f12256d5c748e84389ad2f6647d03d
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-02-01 19:53:28 +0000

    Add includes and defines for timer-counter configuration
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1437 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/nestedInterrupts/include/TaskBlinkNested.h
+++ samples/nestedInterrupts/include/TaskBlinkNested.h
@@ -11,21 +11,35 @@
 
 #include "LedVar.h"
 
+#include <avr32/io.h>
+#include "hal/arch/avr32/lib/include/compiler.h"
+#include "hal/arch/avr32/uc3/lib/include/intc.h"
+#include "hal/arch/avr32/uc3/lib/include/pm.h"
+
+#define TSKBLKNEST_TIMER                        (AVR32_TC0)
+#define TSKBLKNEST_CHANNEL                      (1)
+#define TSKBLKNEST_CFGINT_TIMER_IRQ_ID          (AVR32_TC0_IRQ1)
+#define TSKBLKNEST_CFGINT_TIMER_IRQ_LEVEL       (1)
+#define TSKBLKNEST_CFGINT_TIMER_CLOCK_SELECT    (3)
+#define TSKBLKNEST_CFGINT_TIMER_PRESCALLER      (4)
+#define TSKBLKNEST_CFGINT_TICK_RATE_HZ          (3000)
+
 class TaskBlinkNested : public OSTask
-  {
+{
 public:
   // task constructor
   TaskBlinkNested(const char *pName, unsigned char iLed, schedTicks_t rate);
 
   // actual task main code
-  virtual void taskMain(void);
+  virtual void
+  taskMain(void);
 
 private:
   // members
-  OSStack_t m_stack[ (OSTask::STACK_MINIMAL_SIZE + 200) / sizeof(OSStack_t) ];
+  OSStack_t m_stack[(OSTask::STACK_MINIMAL_SIZE + 200) / sizeof(OSStack_t)];
 
   LedVar m_oLed;
   schedTicks_t m_rate;
-  };
+};
 
 #endif /*TASKBLINKBUSYWAIT_H_*/
