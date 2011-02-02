1443f96dd94dbcb883fb7defc96fd02fa17fe475
Liviu Ionescu <ilg@livius.net>
2011-02-02 16:04:22 +0000

    code to lengthen system timer moved here
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1439 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/nestedInterrupts/src/main.cpp
+++ samples/nestedInterrupts/src/main.cpp
@@ -15,7 +15,43 @@
 
 // tasks allocated on static storage
 TaskBlink task1("A", APP_CONFIG_LED1, TICKS * 1);
-//TaskBlinkNested task2("B", APP_CONFIG_LED3, 100 /*TICKS * 4*/);
+TaskBlinkNested task2("B", APP_CONFIG_LED2, TICKS * 1);
+
+
+// ---------------------------------------------------------------------------
+
+void
+busyWaitMicros(unsigned int n)
+{
+  int i;
+  // calibrate from OSC
+  for (i = (OS_CFGLONG_SYSCLOCK_HZ / 1000 * n / OS_CFGINT_BUSYWAIT_CALIBRATION); i--;)
+    {
+      OSImpl::NOP();
+    }
+}
+
+// artificially make the system timer interrupt longer, so that we
+// increase the chance of having nested interrupts.
+
+#define APP_INCLUDE_LENGTHEN_SYSTICK    1
+#define APP_INCLUDE_BUSY_WAIT           1
+#define APP_BUSY_PROCENTAGE             40
+#define APP_EVENT_DUMMY                 0x1111
+#define APP_CFGINT_NOTIFIES             100
+
+void
+OSSchedulerImpl::applicationInterruptTick(void)
+{
+#if defined(APP_INCLUDE_LENGTHEN_SYSTICK)
+#if defined(APP_INCLUDE_BUSY_WAIT)
+  busyWaitMicros(1000000/OS_CFGINT_TICK_RATE_HZ*APP_BUSY_PROCENTAGE/100);
+#else
+  for (int i = APP_CFGINT_NOTIFIES; --i;)
+    os.sched.eventNotify(APP_EVENT_DUMMY);
+#endif
+#endif
+}
 
 // ---------------------------------------------------------------------------
 
