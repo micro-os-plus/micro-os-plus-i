c98ed905b5dda26b2a9dced77f9296be61cb21bc
Liviu Ionescu <ilg@livius.net>
2011-02-05 22:02:15 +0000

    timer moved to TC1, since for TC0 we cannot set INT1; interruptInit()  interruptAck(), interruptServiceRoutine() added to the task
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1475 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/nestedInterrupts/include/TaskBlinkNested.h
+++ samples/nestedInterrupts/include/TaskBlinkNested.h
@@ -16,13 +16,13 @@
 #include "hal/arch/avr32/uc3/lib/include/intc.h"
 #include "hal/arch/avr32/uc3/lib/include/pm.h"
 
-#define TSKBLKNEST_TIMER                        (AVR32_TC0)
-#define TSKBLKNEST_CHANNEL                      (1)
-#define TSKBLKNEST_CFGINT_TIMER_IRQ_ID          (AVR32_TC0_IRQ1)
+#define TSKBLKNEST_TIMER                        (AVR32_TC1)
+#define TSKBLKNEST_CHANNEL                      (0)
+#define TSKBLKNEST_CFGINT_TIMER_IRQ_ID          (AVR32_TC1_IRQ0)
 #define TSKBLKNEST_CFGINT_TIMER_IRQ_LEVEL       (1)
 #define TSKBLKNEST_CFGINT_TIMER_CLOCK_SELECT    (3)
 #define TSKBLKNEST_CFGINT_TIMER_PRESCALLER      (4)
-#define TSKBLKNEST_CFGINT_TICK_RATE_HZ          (9000)
+//#define TSKBLKNEST_CFGINT_TICK_RATE_HZ          (9000)
 
 class TaskBlinkNested : public OSTask
 {
@@ -34,12 +34,23 @@ public:
   virtual void
   taskMain(void);
 
+  void
+  interruptInit(void);
+
+  void
+  interruptAck(void);
+
+  void
+  interruptServiceRoutine(void) __attribute__ ((noinline));
+
 private:
   // members
   OSStack_t m_stack[(OSTask::STACK_MINIMAL_SIZE + 200) / sizeof(OSStack_t)];
 
   LedVar m_oLed;
   schedTicks_t m_rate;
+
+  schedTicks_t m_count;
 };
 
 #endif /*TASKBLINKBUSYWAIT_H_*/
