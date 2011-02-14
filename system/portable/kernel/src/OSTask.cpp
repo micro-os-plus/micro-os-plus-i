9ae13886959ceb73237bfe6ce1d4a7a09963db80
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-02-14 16:32:26 +0000

    eventNotify() added; reindented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1532 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/src/OSTask.cpp
+++ system/portable/kernel/src/OSTask.cpp
@@ -15,103 +15,104 @@
 // C++ style constructor, run taskMain
 OSTask::OSTask(const char *pName, const OSStack_t *pStack,
     unsigned short stackSize, OSTaskPriority_t priority)
-  {
-    OSTask::init(pName, (OSTaskMainPtr_t) &OSTask::staticMain, this, pStack,
-        stackSize, priority);
-  }
+{
+  OSTask::init(pName, (OSTaskMainPtr_t) &OSTask::staticMain, this, pStack,
+      stackSize, priority);
+}
 
 // C style constructor, run any function
 OSTask::OSTask(const char *pName, OSTaskMainPtr_t entryPoint,
     void *pParameters, const OSStack_t *pStack, unsigned short stackSize,
     OSTaskPriority_t priority)
-  {
-    OSTask::init(pName, entryPoint, pParameters, pStack, stackSize, priority);
-  }
+{
+  OSTask::init(pName, entryPoint, pParameters, pStack, stackSize, priority);
+}
 
 /* stack size is in multiples of OSStack_t */
-void OSTask::init(const char *pName, OSTaskMainPtr_t entryPoint,
-    void *pParameters, const OSStack_t *pStack, unsigned short stackSize,
+void
+OSTask::init(const char *pName, OSTaskMainPtr_t entryPoint, void *pParameters,
+    const OSStack_t *pStack, unsigned short stackSize,
     OSTaskPriority_t priority)
-  {
-    // no stack, sorry...
-    if (pStack == 0 || stackSize == 0)
-      return;
+{
+  // no stack, sorry...
+  if (pStack == 0 || stackSize == 0)
+    return;
 
-    // no entry, sorry...
-    if (entryPoint == 0)
-      return;
+  // no entry, sorry...
+  if (entryPoint == 0)
+    return;
 
-    // no name, sorry...
-    if (pName == 0)
-      return;
+  // no name, sorry...
+  if (pName == 0)
+    return;
 
 #if defined(DEBUG)
-      {
-        OSDeviceDebug::putString_P(PSTR("OSTask('"));
-        OSDeviceDebug::putString(pName);
-        OSDeviceDebug::putString_P(PSTR("',"));
-        OSDeviceDebug::putPtr((void*)entryPoint);
-        OSDeviceDebug::putChar(',');
-        OSDeviceDebug::putPtr(pParameters);
-        OSDeviceDebug::putChar(',');
-        OSDeviceDebug::putPtr(pStack);
-        OSDeviceDebug::putChar(',');
-        OSDeviceDebug::putHex(stackSize);
-        OSDeviceDebug::putChar(',');
-        OSDeviceDebug::putHex(priority);
-        OSDeviceDebug::putString_P(PSTR(")="));
-        OSDeviceDebug::putPtr(this);
-        OSDeviceDebug::putNewLine();
-      }
+    {
+      OSDeviceDebug::putString_P(PSTR("OSTask('"));
+      OSDeviceDebug::putString(pName);
+      OSDeviceDebug::putString_P(PSTR("',"));
+      OSDeviceDebug::putPtr((void*) entryPoint);
+      OSDeviceDebug::putChar(',');
+      OSDeviceDebug::putPtr(pParameters);
+      OSDeviceDebug::putChar(',');
+      OSDeviceDebug::putPtr(pStack);
+      OSDeviceDebug::putChar(',');
+      OSDeviceDebug::putHex(stackSize);
+      OSDeviceDebug::putChar(',');
+      OSDeviceDebug::putHex(priority);
+      OSDeviceDebug::putString_P(PSTR(")="));
+      OSDeviceDebug::putPtr(this);
+      OSDeviceDebug::putNewLine();
+    }
 #endif
 
-    m_pName = pName;
-    m_entryPoint = entryPoint;
-    m_pStackBottom = (unsigned char *) pStack;
-    m_stackSize = stackSize;
-    m_staticPriority = priority;
-    m_pParameters = pParameters;
+  m_pName = pName;
+  m_entryPoint = entryPoint;
+  m_pStackBottom = (unsigned char *) pStack;
+  m_stackSize = stackSize;
+  m_staticPriority = priority;
+  m_pParameters = pParameters;
 
-    m_event = OSEvent::OS_NONE;
-    m_eventWaitReturn = OSEventWaitReturn::OS_NONE;
-    m_isSuspended = false;
-    m_isWaiting = false;
-    //m_hasReturnValue = false;
+  m_event = OSEvent::OS_NONE;
+  m_eventWaitReturn = OSEventWaitReturn::OS_NONE;
+  m_isSuspended = false;
+  m_isWaiting = false;
+  //m_hasReturnValue = false;
 
-    //m_isPreempted = false;
+  //m_isPreempted = false;
 
 #if defined(OS_INCLUDE_OSTASK_SLEEP)
-    // by default, tasks will enter deep sleep
-    m_allowSleep = true;
+  // by default, tasks will enter deep sleep
+  m_allowSleep = true;
 #endif
 
 #if defined(OS_INCLUDE_OSTASK_VIRTUALWATCHDOG)
-    m_WDseconds = 0;
+  m_WDseconds = 0;
 #endif
 
 #if defined(OS_INCLUDE_OSTASK_INTERRUPTION)
-    m_isInterrupted = false;
+  m_isInterrupted = false;
 #endif
-    // register this task to scheduler
-    m_id = OSScheduler::taskRegister(this);
+  // register this task to scheduler
+  m_id = OSScheduler::taskRegister(this);
 
-    // fill stack with constant pattern(0x5A)
-    memset((void *) pStack, STACK_FILL_BYTE, stackSize);
+  // fill stack with constant pattern(0x5A)
+  memset((void *) pStack, STACK_FILL_BYTE, stackSize);
 
-    // initialize stack so that a context restore will be performed
-    m_pStack = OSSchedulerImpl::stackInitialize((OSStack_t*) (&pStack[stackSize
-        / sizeof(OSStack_t) - 1]), entryPoint, pParameters, m_id);
+  // initialize stack so that a context restore will be performed
+  m_pStack = OSSchedulerImpl::stackInitialize((OSStack_t*) (&pStack[stackSize
+      / sizeof(OSStack_t) - 1]), entryPoint, pParameters, m_id);
 
-    // pStack points to first register on arm and
-    // to byte before first register on AVR8
+  // pStack points to first register on arm and
+  // to byte before first register on AVR8
 
 #if defined(DEBUG) && false
-      {
-        OSDeviceDebug::putPtr(m_pStack);
-        OSDeviceDebug::putNewLine();
-      }
+    {
+      OSDeviceDebug::putPtr(m_pStack);
+      OSDeviceDebug::putNewLine();
+    }
 #endif
-  }
+}
 
 #if false
 OSTask::~OSTask()
@@ -126,112 +127,124 @@ OSTask::~OSTask()
 #endif
 
 // redirect to virtual function
-void OSTask::staticMain(OSTask * pt)
-  {
-    pt->taskMain();
+void
+OSTask::staticMain(OSTask * pt)
+{
+  pt->taskMain();
 
-    // normally should not reach here
+  // normally should not reach here
 #if defined(DEBUG)
-    OSDeviceDebug::putString_P(PSTR("taskMain loop exit"));
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString_P(PSTR("taskMain loop exit"));
+  OSDeviceDebug::putNewLine();
 #endif
 
-    OS::SOFTreset();
-  }
+  OS::SOFTreset();
+}
 
 // overriden by actual implementation
-void OSTask::taskMain(void)
-  {
-    // loop
-    for (;;)
-      ;
-  }
+void
+OSTask::taskMain(void)
+{
+  // loop
+  for (;;)
+    ;
+}
 
 // Suspend and remove from ready list
-void OSTask::suspend(void)
-  {
-    OSScheduler::criticalEnter();
-      {
-        m_isSuspended = true;
-        OSReadyList::remove(this);
-      }
-    OSScheduler::criticalExit();
-  }
+void
+OSTask::suspend(void)
+{
+  OSScheduler::criticalEnter();
+    {
+      m_isSuspended = true;
+      OSReadyList::remove(this);
+    }
+  OSScheduler::criticalExit();
+}
 
 // Resume and insert into ready list
-void OSTask::resume(void)
-  {
-    OSScheduler::criticalEnter();
-      {
-        m_isSuspended = false;
-        OSReadyList::insert(this);
-      }
-    OSScheduler::criticalExit();
-  }
+void
+OSTask::resume(void)
+{
+  OSScheduler::criticalEnter();
+    {
+      m_isSuspended = false;
+      OSReadyList::insert(this);
+    }
+  OSScheduler::criticalExit();
+}
 
-char const * OSTask::getName(void)
-  {
-    return m_pName;
-  }
+char const *
+OSTask::getName(void)
+{
+  return m_pName;
+}
 
-unsigned char * OSTask::getStackBottom(void)
-  {
-    return m_pStackBottom;
-  }
+unsigned char *
+OSTask::getStackBottom(void)
+{
+  return m_pStackBottom;
+}
 
-OSStack_t * OSTask::getStack(void)
-  {
-    return m_pStack;
-  }
+OSStack_t *
+OSTask::getStack(void)
+{
+  return m_pStack;
+}
 
-unsigned short OSTask::getStackSize(void)
-  {
-    return m_stackSize;
-  }
+unsigned short
+OSTask::getStackSize(void)
+{
+  return m_stackSize;
+}
 
 // since all registers are saved on the task stack,
 // the 'context' in simply a pointer to the stack (the address below
 // the deepest register pushed; for offsets to registers please see
 // OSScheduler::stackInitialize()).
-void * OSTask::getContext(void)
-  {
-    return (void *) &m_pStack;
-  }
+void *
+OSTask::getContext(void)
+{
+  return (void *) &m_pStack;
+}
 
-void OSTask::setPriority(OSTaskPriority_t priority)
-  {
-    m_staticPriority = priority;
-  }
+void
+OSTask::setPriority(OSTaskPriority_t priority)
+{
+  m_staticPriority = priority;
+}
 
-void OSTask::setEvent(OSEvent_t event)
-  {
-    m_event = event;
-  }
+void
+OSTask::setEvent(OSEvent_t event)
+{
+  m_event = event;
+}
 
 #if defined(OS_INCLUDE_OSTASK_GETSTACKUSED)
 
-unsigned short OSTask::getStackUsed(void)
-  {
-    unsigned short i;
-    unsigned short r;
-    unsigned char *pc;
+unsigned short
+OSTask::getStackUsed(void)
+{
+  unsigned short i;
+  unsigned short r;
+  unsigned char *pc;
 
-    r = m_stackSize;
-    pc = m_pStackBottom;
+  r = m_stackSize;
+  pc = m_pStackBottom;
 
-    for (i = 0; (i < m_stackSize ) && ( *pc == STACK_FILL_BYTE ); ++i,
-        ++pc, --r)
+  for (i = 0; (i < m_stackSize) && (*pc == STACK_FILL_BYTE); ++i, ++pc, --r)
     ;
 
-    return r;
-  }
+  return r;
+}
 
 #endif
 
-int OSTask::getID(void)
-  {
-    return m_id;
-  }
+int
+OSTask::getID(void)
+{
+  return m_id;
+}
 
 #if defined(OS_INCLUDE_OSTASK_GETPROGRAMCOUNTER)
 
@@ -247,25 +260,29 @@ OSProgramPtr_t OSTask::getProgramCounter(void)
 
 #endif
 
-OSEvent_t OSTask::getEvent(void)
-  {
-    return m_event;
-  }
+OSEvent_t
+OSTask::getEvent(void)
+{
+  return m_event;
+}
 
-OSTaskPriority_t OSTask::getPriority(void)
-  {
-    return m_staticPriority;
-  }
+OSTaskPriority_t
+OSTask::getPriority(void)
+{
+  return m_staticPriority;
+}
 
-bool OSTask::isSuspended(void)
-  {
-    return m_isSuspended;
-  }
+bool
+OSTask::isSuspended(void)
+{
+  return m_isSuspended;
+}
 
-bool OSTask::isWaiting(void)
-  {
-    return m_isWaiting;
-  }
+bool
+OSTask::isWaiting(void)
+{
+  return m_isWaiting;
+}
 
 #if defined(OS_INCLUDE_OSTASK_SLEEP)
 
@@ -368,5 +385,50 @@ OSTask::setInterruption(bool flag)
   }
 
 #endif
+
+int OSTask::eventNotify(OSEvent_t event, OSEventWaitReturn_t ret)
+{
+  if (event == OSEvent::OS_NONE)
+    return 0;
+
+  int cnt;
+  cnt = 0;
+#if defined (OSTASK_NOTIFY_MEASURE)
+  OS_GPIO_PIN_HIGH(OS_CONFIG_ACTIVE_LED_PORT_CONFIG, OS_APP_CONFIG_LED3);
+#endif
+#if defined (OS_INCLUDE_OSTASK_EVENTNOTIFY_REALTIMECRITICAL)
+  OSScheduler::realTimeCriticalEnter();
+#else
+  OSScheduler::criticalEnter();
+#endif
+    {
+      // not-suspended waiting tasks are notified
+      if ((m_isWaiting) && (!m_isSuspended))
+        {
+          OSEvent_t ev;
+          ev = m_event;
+          if (ev == OSEvent::OS_ALL || ev == event)
+            {
+              // wakeup tasks waiting for event or for 0
+              m_isWaiting = false;
+              m_event = OSEvent::OS_NONE; // no longer wait for it
+              m_eventWaitReturn = ret;
+#if defined(DEBUG)
+              if (ret == OSEventWaitReturn::OS_NONE)
+                OSDeviceDebug::putChar('^');
+#endif
+              OSReadyList::insert( this);
+              cnt = 1;
+            }
+        }
+
+    }
+  OSScheduler::criticalExit();
+#if defined (OSTASK_NOTIFY_MEASURE)
+  OS_GPIO_PIN_LOW(OS_CONFIG_ACTIVE_LED_PORT_CONFIG, OS_APP_CONFIG_LED3);
+#endif
+  // return the number of notified tasks
+  return cnt;
+}
 
 #endif /* !defined(OS_EXCLUDE_MULTITASKING) */
