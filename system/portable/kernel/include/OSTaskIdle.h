ece636d4a735128d586184c530dfe9dbaa6de12b
Bogdan Brinza <brinzab@yahoo.com>
2011-02-16 15:45:10 +0000

    Comments added.
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1570 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/include/OSTaskIdle.h
+++ system/portable/kernel/include/OSTaskIdle.h
@@ -18,13 +18,23 @@ class OSTaskIdle : public OSTask
 public:
   OSTaskIdle();
 
+  // Main function task.
+  // If this function run, it means that no other task needs the MCU.
+  // It will try to put the MCU to sleep. After that (when the MCU is waked up)
+  // a yield is made to give the control to other task, if there is another
+  // task which in the meantime needs the MCU.
   virtual void taskMain(void);
+
 #if defined(OS_INCLUDE_OSTASK_SLEEP)
+  // Check if the MCU can be put at sleep.
+  // If the sleep is allowed put the MCU to sleep.
   virtual bool enterSleep(void);
 #endif
 
 private:
   // members
+
+  // Stack used by this task.
   OSStack_t m_stack[OS_CFGINT_IDLE_TASK_STACK_SIZE / sizeof(OSStack_t)];
   };
 
