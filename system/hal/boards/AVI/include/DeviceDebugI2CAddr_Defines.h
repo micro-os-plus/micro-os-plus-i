5c41c7bc5a9c3c3203e9b64a6597e2a741581302
Liviu Ionescu <ilg@livius.net>
2011-02-19 23:10:25 +0000

    comments added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1599 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/boards/AVI/include/DeviceDebugI2CAddr_Defines.h
+++ system/hal/boards/AVI/include/DeviceDebugI2CAddr_Defines.h
@@ -7,8 +7,12 @@
 #ifndef HAL_DEVICEDEBUGI2C_ADDRESSES_DEFINES_H_
 #define HAL_DEVICEDEBUGI2C_ADDRESSES_DEFINES_H_
 
-// these are the addresses used by the proprietary AVI debug device
+// This is the addresses hard-wired in the USB-I2C
+// debug device (Available from Aviation Instruments Intl)
 #define OS_CFGINT8_DEBUG_I2C_DEBUGGER_ADDR	 	(0x28)
+
+// This is our own address on the I2C bus. Needed just as a setting,
+// not really used, the debugged device does not accept commands on I2C.
 #define OS_CFGINT8_DEBUG_I2C_OWN_ADDR			(0xFE)
 
 #endif /*HAL_DEVICEDEBUGI2C_ADDRESSES_DEFINES_H_*/
