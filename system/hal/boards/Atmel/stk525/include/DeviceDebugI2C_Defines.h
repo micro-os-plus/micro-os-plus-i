3a5b8ec16e140187316b134823249c29efc577de
Liviu Ionescu <ilg@livius.net>
2011-02-19 23:14:07 +0000

    SC[LA]_PORT renamed CONFIG, PORT renamed PORT_WRITE
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1601 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/boards/Atmel/stk525/include/DeviceDebugI2C_Defines.h
+++ system/hal/boards/Atmel/stk525/include/DeviceDebugI2C_Defines.h
@@ -10,16 +10,17 @@
 #define OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT             (0)
 #define OS_CFGINT_DEBUG_I2C_EMU_INIT_DELAY_COUNT	(8)
 
-#define OS_CONFIG_DEBUG_SCL_PORT_INIT                   DDRC
-#define OS_CONFIG_DEBUG_SCL_PORT                        PORTC
+#define OS_CONFIG_DEBUG_SCL_PORT_CONFIG                 DDRC
+#define OS_CONFIG_DEBUG_SCL_PORT_WRITE                  PORTC
 #define OS_CONFIG_DEBUG_SCL_PORT_READ                   PINC
 #define OS_CONFIG_DEBUG_SCL_BIT                         PORTC0
 
-#define OS_CONFIG_DEBUG_SDA_PORT_INIT                   DDRC
-#define OS_CONFIG_DEBUG_SDA_PORT                        PORTC
+#define OS_CONFIG_DEBUG_SDA_PORT_CONFIG                 DDRC
+#define OS_CONFIG_DEBUG_SDA_PORT_WRITE                  PORTC
 #define OS_CONFIG_DEBUG_SDA_PORT_READ                   PINC
 #define OS_CONFIG_DEBUG_SDA_BIT                         PORTC1
 
+// Include the AVI file, for the USB-I2C address
 #include "hal/boards/AVI/include/DeviceDebugI2CAddr_Defines.h"
 
-#endif /*HAL_BOARD_DEVICEDEBUGI2C_DEFINES_H_*/
+#endif /* HAL_BOARD_DEVICEDEBUGI2C_DEFINES_H_ */
