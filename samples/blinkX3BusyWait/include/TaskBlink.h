5785f97e30d68804bab5627f81c55b73e8fd38b5
Liviu Ionescu <ilg@livius.net>
2011-02-20 23:15:43 +0000

    re-indented; stack increased with 400
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1642 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3BusyWait/include/TaskBlink.h
+++ samples/blinkX3BusyWait/include/TaskBlink.h
@@ -12,20 +12,21 @@
 #include "LedVar.h"
 
 class TaskBlink : public OSTask
-  {
+{
 public:
   // task constructor
   TaskBlink(const char *pName, unsigned char iLed, schedTicks_t rate);
 
   // actual task main code
-  virtual void taskMain(void);
+  virtual void
+  taskMain(void);
 
 private:
   // members
-  OSStack_t m_stack[ (OSTask::STACK_MINIMAL_SIZE + 200) / sizeof(OSStack_t) ];
+  OSStack_t m_stack[(OSTask::STACK_MINIMAL_SIZE + 400) / sizeof(OSStack_t)];
 
   LedVar m_oLed;
   schedTicks_t m_rate;
-  };
+};
 
-#endif /*TASKBLINK_H_*/
+#endif /* TASKBLINK_H_ */
