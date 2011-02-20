9eee0c32ef6bd4df73cd9a91670af0aef0f83179
Liviu Ionescu <ilg@livius.net>
2011-02-20 23:24:44 +0000

    indented; cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1650 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr8/kernel/include/OS_Arch_Inlines.h
+++ system/hal/arch/avr8/kernel/include/OS_Arch_Inlines.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2007-2009 Liviu Ionescu.
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
  *
  *	This file is part of the uOS++ distribution.
  */
@@ -7,88 +7,100 @@
 #ifndef HAL_OS_ARCH_INLINES_H_
 #define HAL_OS_ARCH_INLINES_H_
 
-inline void OSImpl::returnFromInterrupt(void)
-  {
-    asm volatile( "reti":: );
-    for (;;)
-      ; // noreturn
-  }
+inline void
+OSImpl::returnFromInterrupt(void)
+{
+  asm volatile( "reti":: );
+  for (;;)
+    ; // noreturn
+}
 
-inline void OSImpl::returnFromSubroutine(void)
-  {
-    asm volatile( "ret":: );
-    for (;;)
-      ; // noreturn
-  }
+inline void
+OSImpl::returnFromSubroutine(void)
+{
+  asm volatile( "ret":: );
+  for (;;)
+    ; // noreturn
+}
 
-inline void OSImpl::CPUidle(void)
-  {
-    set_sleep_mode(SLEEP_MODE_IDLE);
-    sleep_cpu();
-  }
+inline void
+OSImpl::CPUidle(void)
+{
+  set_sleep_mode( SLEEP_MODE_IDLE);
+  sleep_cpu();
+}
 
-inline void OSImpl::CPUsleep(void)
-  {
-    set_sleep_mode(SLEEP_MODE_EXT_STANDBY);
-    sleep_cpu();
-  }
+inline void
+OSImpl::CPUsleep(void)
+{
+  set_sleep_mode( SLEEP_MODE_EXT_STANDBY);
+  sleep_cpu();
+}
 
-inline void OSImpl::CPUdeepSleep(void)
-  {
-    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-    sleep_cpu();
-  }
+inline void
+OSImpl::CPUdeepSleep(void)
+{
+  set_sleep_mode( SLEEP_MODE_PWR_SAVE);
+  sleep_cpu();
+}
 
-inline void OSImpl::WDTreset(void)
-  {
-    wdt_reset();
-  }
+inline void
+OSImpl::WDTreset(void)
+{
+  wdt_reset();
+}
 
-inline void OSImpl::NOP(void)
-  {
-    asm volatile( "nop":: );
-  }
+inline void
+OSImpl::NOP(void)
+{
+  asm volatile( "nop":: );
+}
 
 // as per manual, JMP 0 is not recommended since
 // registers are not set to their initial status
-inline void OSImpl::SOFTreset(void)
-  {
-    wdt_enable(WDTO_15MS);
-    cli();
-    for (;;)
-      ; // trigger WD
-  }
+inline void
+OSImpl::SOFTreset(void)
+{
+  wdt_enable( WDTO_15MS);
+  cli();
+  for (;;)
+    ; // trigger WD
+}
 
-inline void OSImpl::interruptsEnable(void)
-  {
-    sei();
-  }
+inline void
+OSImpl::interruptsEnable(void)
+{
+  sei();
+}
 
-inline void OSImpl::interruptsDisable(void)
-  {
-    cli();
-  }
+inline void
+OSImpl::interruptsDisable(void)
+{
+  cli();
+}
 
-inline OSResetBits_t OSImpl::CPUfetchResetBits(void)
-  {
-    OSResetBits_t ret;
-    ret = MCUSR;
-    MCUSR = 0;
-    return ret;
-  }
+inline OSResetBits_t
+OSImpl::CPUfetchResetBits(void)
+{
+  OSResetBits_t ret;
+  ret = MCUSR;
+  MCUSR = 0;
+  return ret;
+}
 
-#if true
 // release processor to next ready task
-inline void OSSchedulerImpl::yield(void)
-  {
-    OSScheduler::contextSave();
-      {
+inline void
+OSSchedulerImpl::yield(void)
+{
+  OSScheduler::contextSave();
+    {
       OSScheduler::performContextSwitch(); // same as from ISRs, no return
-      }
-    OSScheduler::contextRestore();
-    OS::interruptsEnable(); // be sure interrupts remain enabled
-    OS::returnFromSubroutine(); // necessary because it is 'naked'
-  }
-#endif
+    }
+  OSScheduler::contextRestore();
 
-#endif /*HAL_OS_ARCH_INLINES_H_*/
+  // For unknown reasons, interrupts must be re-enabled here
+  OS::interruptsEnable();
+  OS::returnFromSubroutine(); // necessary because it is 'naked'
+}
+
+#endif /* HAL_OS_ARCH_INLINES_H_ */
