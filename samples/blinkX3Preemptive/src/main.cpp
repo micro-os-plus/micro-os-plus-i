86973f4b45bb851bd25783809d87d5a2981c04a5
Liviu Ionescu <ilg@livius.net>
2011-02-20 23:07:08 +0000

    TaskBusyWait rate is 4 sec; greeting removed
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1637 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3Preemptive/src/main.cpp
+++ samples/blinkX3Preemptive/src/main.cpp
@@ -16,11 +16,6 @@
 // tasks allocated on static storage
 TaskBlink task1("A", APP_CONFIG_LED1, TICKS * 1);
 TaskBlink task2("B", APP_CONFIG_LED2, TICKS * 2);
-TaskBlinkBusyWait task3("C", APP_CONFIG_LED3, 100 /*TICKS * 4*/);
+TaskBlinkBusyWait task3("C", APP_CONFIG_LED3, TICKS * 4);
 
 // ---------------------------------------------------------------------------
-
-// message to be displayed on the debugging device at startup
-#if defined(DEBUG)
-const char greeting[] = "AVI blinkX3Preemptive v1.1";
-#endif
