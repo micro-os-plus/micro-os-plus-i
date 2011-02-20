ccdb9c53d2cd1a468410158776b9b43229e084af
Liviu Ionescu <ilg@livius.net>
2011-02-20 23:02:30 +0000

    APP_OS_DEFINES_H_ renamed to OS_APP_DEFINES_H_ OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS replaced by OS_DEBUG_OSTIMERTICKS_ISR_MARK_SECONDS cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1632 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3Preemptive/include/OS_App_Defines.h
+++ samples/blinkX3Preemptive/include/OS_App_Defines.h
@@ -4,28 +4,33 @@
  *	This file is part of the uOS++ distribution.
  */
 
-#ifndef APP_OS_DEFINES_H_
-#define APP_OS_DEFINES_H_
+#ifndef OS_APP_DEFINES_H_
+#define OS_APP_DEFINES_H_
+
+// ----------------------------------------------------------------------------
 
 // override the board definitions, if needed
-#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
+//#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
 
-#define OS_CFGINT_TICK_RATE_HZ                          (1000)
-#define OS_CFGINT_TIMER_PRESCALLER                      (4)
-#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
+//#define OS_CFGINT_TICK_RATE_HZ                          (1000)
+//#define OS_CFGINT_TIMER_PRESCALLER                      (4)
+//#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
 
 #define OS_CFGINT_TASKS_TABLE_SIZE                      (3)
 
+// Only for tests, when ready should not be used
 //#define OS_EXCLUDE_PREEMPTION                           (1)
 
 #define OS_INCLUDE_OS_BUSYWAITMILLIS                    1
+
+// ----------------------------------------------------------------------------
 
 #if defined(DEBUG)
 // One of the following must be defined in Eclipse project or Makefile
 //#define OS_CONFIG_DEBUG_DEVICE_I2C                    1
 //#define OS_CONFIG_DEBUG_DEVICE_USART			1
 
-#define OS_INCLUDE_OSSCHEDULER_TIMER_MARK_SECONDS       (1)
+#define OS_DEBUG_OSTIMERTICKS_ISR_MARK_SECONDS       (1)
 
 //#define OS_DEBUG_OSSCHEDULER_STACKINITIALIZE            (1)
 #define OS_DEBUG_CONSTRUCTORS                           (1)
@@ -36,6 +41,8 @@
 //#define OS_DEBUG_OSSCHEDULER_DUMPSTACK                  (1)
 //#define OS_DEBUG_CONSTRUCTORS_INIT                      (1)
 
-#endif /*DEBUG*/
+#endif /* DEBUG */
 
-#endif /*APP_OS_DEFINES_H_ */
+// ----------------------------------------------------------------------------
+
+#endif /* OS_APP_DEFINES_H_ */
