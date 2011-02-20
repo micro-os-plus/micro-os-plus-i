e69d41f451a20cd45f23c3aa3c10c85449d469a6
Liviu Ionescu <ilg@livius.net>
2011-02-20 22:38:11 +0000

    APP_OS_DEFINES_H_ renamed to OS_APP_DEFINES_H_
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1616 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/calibrateBusyWait/include/OS_App_Defines.h
+++ samples/calibrateBusyWait/include/OS_App_Defines.h
@@ -4,8 +4,8 @@
  *	This file is part of the uOS++ distribution.
  */
 
-#ifndef APP_OS_DEFINES_H_
-#define APP_OS_DEFINES_H_
+#ifndef OS_APP_DEFINES_H_
+#define OS_APP_DEFINES_H_
 
 // override the board definitions, if needed
 //#define OS_CFGLONG_OSCILLATOR_HZ                        (8000000UL)
