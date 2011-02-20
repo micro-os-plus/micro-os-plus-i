c5cda0db16ad4248df6fb915d8f2efc256c11753
Liviu Ionescu <ilg@livius.net>
2011-02-20 22:48:03 +0000

    APP_OS_DEFINES_H_ renamed to OS_APP_DEFINES_H_
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1624 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/sdi12sensor/include/OS_App_Defines.h
+++ samples/sdi12sensor/include/OS_App_Defines.h
@@ -4,8 +4,8 @@
  *	This file is part of the uOS++ distribution.
  */
 
-#ifndef APP_OS_DEFINES_H_
-#define APP_OS_DEFINES_H_
+#ifndef OS_APP_DEFINES_H_
+#define OS_APP_DEFINES_H_
 
 // override the board definitions, if needed
 //#define OS_CFGLONG_OSCILLATOR_HZ                        (7372800UL)
@@ -40,4 +40,4 @@
 //#define OS_CONFIG_DEBUG_DEVICE_USART			1
 #endif /*DEBUG*/
 
-#endif /*APP_OS_DEFINES_H_ */
+#endif /*OS_APP_DEFINES_H_ */
