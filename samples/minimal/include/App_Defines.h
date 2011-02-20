4ee497eb6b9c4f096bf23e430a18d6d10d439370
Liviu Ionescu <ilg@livius.net>
2011-02-20 22:52:13 +0000

    versioning and greeting
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1626 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/minimal/include/App_Defines.h
+++ samples/minimal/include/App_Defines.h
@@ -1,5 +1,5 @@
 /*
- *	Copyright (C) 2007-2008 Liviu Ionescu.
+ *	Copyright (C) 2007-2011 Liviu Ionescu.
  *
  *	This file is part of the uOS++ distribution.
  */
@@ -7,4 +7,19 @@
 #ifndef APP_DEFINES_H_
 #define APP_DEFINES_H_
 
-#endif /*APP_DEFINES_H_ */
+// ----------------------------------------------------------------------------
+
+// Application versioning and greeting definitions
+// More definitions will be computer in uOS.h
+
+#define APP_CFGSTR_APPLICATION_NAME                     "minimal"
+#define APP_CFGSTR_APPLICATION_MANUFACTURER             "AVI"
+
+// Notice: do not use parenthesis! (the values will be stringified)
+#define APP_CFGINT_VERSION_MAJOR                        2
+#define APP_CFGINT_VERSION_MINOR                        1
+#define APP_CFGINT_VERSION_REVISION                     1316
+
+// ----------------------------------------------------------------------------
+
+#endif /* APP_DEFINES_H_ */
