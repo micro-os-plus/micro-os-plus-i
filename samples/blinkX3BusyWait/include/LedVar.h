67b6c5ce3859286de8cceddd052f6abcbebae1f8
Liviu Ionescu <ilg@livius.net>
2011-02-20 23:12:36 +0000

    GPIO port usage bugs fixed; cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1639 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3BusyWait/include/LedVar.h
+++ samples/blinkX3BusyWait/include/LedVar.h
@@ -10,50 +10,59 @@
 #include "portable/kernel/include/uOS.h"
 
 class LedVar
-  {
+{
 public:
   LedVar(unsigned char iBit);
 
-  void toggle(void);
-  void init(void);
+  void
+  toggle(void);
+  void
+  init(void);
 
-  unsigned char bitNumber(void);
+  unsigned char
+  bitNumber(void);
 
 private:
   unsigned char m_iBit;
-  };
+};
 
-inline LedVar::LedVar(unsigned char iBit)
-  {
-    m_iBit = iBit;
-  }
+inline
+LedVar::LedVar(unsigned char iBit)
+{
+  m_iBit = iBit;
+}
 
-inline void LedVar::init(void)
-  {
-    // init led port as output
-    OS_GPIO_PIN_CONFIG_OUTPUT(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+inline void
+LedVar::init(void)
+{
+  // init led port as output
+  OS_GPIO_PIN_CONFIG_ENABLE(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+  OS_GPIO_PIN_CONFIG_OUTPUT(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
 
-    // Turn led off, i.e. HIGH if active low.
 #if defined(APP_CONFIG_LED_ISACTIVE_LOW)
-    OS_GPIO_PIN_HIGH(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+  // Turn led off, i.e. HIGH if active low.
+  OS_GPIO_PIN_HIGH(APP_CONFIG_LED_PORT_WRITE, m_iBit);
 #else
-    OS_GPIO_PIN_LOW(APP_CONFIG_LED_PORT_CONFIG, m_iBit);
+  // Turn led off, i.e. LOW if active high.
+  OS_GPIO_PIN_LOW(APP_CONFIG_LED_PORT_WRITE, m_iBit);
 #endif
-  }
+}
 
-inline void LedVar::toggle(void)
-  {
-    os.sched.criticalEnter();
-      {
-        // toggle led
-        OS_GPIO_PIN_TOGGLE(APP_CONFIG_LED_PORT, m_iBit);
-      }
-    os.sched.criticalExit();
-  }
+inline void
+LedVar::toggle(void)
+{
+  os.sched.criticalEnter();
+    {
+      // Toggle led
+      OS_GPIO_PIN_TOGGLE(APP_CONFIG_LED_PORT_WRITE, m_iBit);
+    }
+  os.sched.criticalExit();
+}
 
-inline unsigned char LedVar::bitNumber(void)
-  {
-    return m_iBit;
-  }
+inline unsigned char
+LedVar::bitNumber(void)
+{
+  return m_iBit;
+}
 
-#endif /*LEDVAR_H_*/
+#endif /* LEDVAR_H_ */
