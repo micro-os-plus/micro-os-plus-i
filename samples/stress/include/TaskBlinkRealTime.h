3db869010288b5c0c79b97a2abfd7d67537bbd5f
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-02-21 17:19:50 +0000

    Copyed from samples Real time interrupt
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1672 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ samples/stress/include/TaskBlinkRealTime.h
@@ -0,0 +1,50 @@
+/*
+ *	Copyright (C) 2007-2011 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#ifndef TASKBLINKREALTIME_H_
+#define TASKBLINKREALTIME_H_
+
+#include "portable/kernel/include/uOS.h"
+
+#include "LedVar.h"
+
+#include <avr32/io.h>
+#include "hal/arch/avr32/lib/include/compiler.h"
+#include "hal/arch/avr32/uc3/lib/include/intc.h"
+#include "hal/arch/avr32/uc3/lib/include/pm.h"
+
+extern bool g_flagNotify;
+
+class TaskBlinkRealTime : public OSTask
+{
+public:
+  // task constructor
+  TaskBlinkRealTime(const char *pName, unsigned char iLed, schedTicks_t rate);
+
+  // actual task main code
+  virtual void
+  taskMain(void);
+
+  void
+  interruptInit(void);
+
+  void
+  interruptAcknowledge(void);
+
+  void
+  interruptServiceRoutine(void) __attribute__ ((noinline));
+
+private:
+  // members
+  OSStack_t m_stack[(OSTask::STACK_MINIMAL_SIZE + 400) / sizeof(OSStack_t)];
+
+  LedVar m_oLed;
+  schedTicks_t m_rate;
+
+  volatile schedTicks_t m_count;
+};
+
+#endif /* TASKBLINKREALTIME_H_ */
