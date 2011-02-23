29f0f394151208e0abc233f4addd2a54a8c75de1
Liviu Ionescu <ilg@livius.net>
2011-02-23 11:14:00 +0000

    all constructors added; cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1701 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
+++ system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
@@ -10,62 +10,91 @@
 
 #if defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSARTBASE)
 
-#include "portable/devices/character/include/DeviceCharacterUsart.h"
+#include "hal/arch/avr32/uc3/devices/character/include/DeviceCharacterBufferedUsartBase.h"
 
 // ----- constructors --------------------------------------------------------
 
 DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(void)
-  {
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
-void DeviceCharacterBufferedUsartBase::setPortAddress(void *pPort)
+// set custom high/low water marks
+DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
+    unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
+    unsigned short txLWM, unsigned char *pRxBuf, unsigned short rxBufSize,
+    unsigned short rxHWM, unsigned short rxLWM) :
+    OSDeviceCharacterBuffered(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
+      rxHWM, rxLWM)
+{
+#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
+#endif
+}
+
+// use default 3/4 high and 1/4 low water marks
+DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
+    unsigned char *pTxBuf, unsigned short txBufSize, unsigned char *pRxBuf,
+    unsigned short rxBufSize) :
+    OSDeviceCharacterBuffered(pTxBuf, txBufSize, pRxBuf, rxBufSize)
+{
+#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
+#endif
+}
+
+void
+DeviceCharacterBufferedUsartBase::setPortAddress(void *pPort)
 {
   m_pPort = pPort;
 }
 
-
-virtual int implPortInit(void)
+int
+DeviceCharacterBufferedUsartBase::implPortInit(void)
 {
-  ; // TODO: Implement it
+  ; // TODO: Implement it (for the moment in Usart1, not here)
   return 0;
 }
 
-virtual int implPortDisable(void)
+int
+DeviceCharacterBufferedUsartBase::implPortDisable(void)
 {
-  ; // TODO: Implement it
+  ; // TODO: Implement it (probably not needed)
   return 0;
 }
 
- void
+void
 DeviceCharacterBufferedUsartBase::implInterruptTxEnable(void)
 {
   ; // TODO: Implement it
 }
 
- void
+void
 DeviceCharacterBufferedUsartBase::implInterruptTxDisable(void)
 {
   ; // TODO: Implement it
 }
 
- unsigned char
+unsigned char
 DeviceCharacterBufferedUsartBase::implPortRead(void)
 {
   ; // TODO: Implement it
   return 0;
 }
 
- void
+void
 DeviceCharacterBufferedUsartBase::implPortWrite(unsigned char b)
 {
   ; // TODO: Implement it
 }
-
 
 #endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSARTBASE) */
 
