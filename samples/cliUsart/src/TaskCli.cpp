2550051e7a1424f7e953415292d393489e38cb1e
Liviu Ionescu <ilg@livius.net>
2011-02-23 11:19:33 +0000

    indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1706 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/cliUsart/src/TaskCli.cpp
+++ samples/cliUsart/src/TaskCli.cpp
@@ -16,15 +16,15 @@
  */
 
 TaskCli::TaskCli(const char *pName, OSDeviceCharacter& dev) :
-  OSTask(pName, m_stack, sizeof(m_stack )), m_dev(dev), m_cin( &m_dev),
-      m_cout( &m_dev), m_cli(m_line, sizeof(m_line ))
-  {
+  OSTask(pName, m_stack, sizeof(m_stack)), m_dev(dev), m_cin(&m_dev), m_cout(
+      &m_dev), m_cli(m_line, sizeof(m_line))
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    debug.putString("TaskCli()=");
-    debug.putPtr( this );
-    debug.putNewLine();
+  debug.putString("TaskCli()=");
+  debug.putPtr(this);
+  debug.putNewLine();
 #endif
-  }
+}
 
 // ---------------------------------------------------------------------------
 
@@ -37,71 +37,71 @@ static const char prompt[] = "> ";
  *
  */
 
-void TaskCli::taskMain(void)
-  {
-    if (os.isDebug() )
-      {
-        os.sched.lock();
-          {
-            clog << "TaskCli::taskMain("<< showbase << hex
-                << this <<
+void
+TaskCli::taskMain(void)
+{
+  if (os.isDebug())
+    {
+      os.sched.lock();
+        {
+          clog << "TaskCli::taskMain(" << showbase << hex << this <<
 #if false
-                ") SP="<< hex
-                << ( unsigned short ) SP <<
+              ") SP="<< hex
+              << ( unsigned short ) SP <<
 #endif
-                endl;
-          }
-        os.sched.unlock();
-      }
+              endl;
+        }
+      os.sched.unlock();
+    }
 
-    OSDeviceCharacter& dev = m_dev;
+  OSDeviceCharacter& dev = m_dev;
 
-    istream& cin = m_cin;
-    ostream& cout = m_cout;
+  istream& cin = m_cin;
+  ostream& cout = m_cout;
 
-    SimpleCli & cli = m_cli;
+  SimpleCli & cli = m_cli;
 
-    // task endless loop
-    for (;;)
-      {
-        dev.open(); // wait for dtr
+  // task endless loop
+  for (;;)
+    {
+      dev.open(); // wait for dtr
 
-        cout << endl << endl << greeting << endl;
+      cout << endl << endl << greeting << endl;
 
-        for (; dev.isConnected();)
-          {
-            cout << endl << prompt;
+      for (; dev.isConnected();)
+        {
+          cout << endl << prompt;
 
-            int c;
+          int c;
 
-c            = cli.readLine(cin, cout);
-            if (c == traits::eof() )
-              {
-                if (os.isDebug)
+          c = cli.readLine(cin, cout);
+          if (c == traits::eof())
+            {
+              if (os.isDebug)
                 clog << "disconnected" << endl;
 
-                break;
-              }
-            else if (c == OSReturn::OS_TIMEOUT)
-              {
-                if (os.isDebug)
+              break;
+            }
+          else if (c == OSReturn::OS_TIMEOUT)
+            {
+              if (os.isDebug)
                 clog << "timeout" << endl;
 
-                break;
-              }
-            else if (c < 0)
-              {
-                if (os.isDebug)
-                clog << "error -" << dec << ( int ) ( -c ) << endl;
+              break;
+            }
+          else if (c < 0)
+            {
+              if (os.isDebug)
+                clog << "error -" << dec << (int) (-c) << endl;
 
-                break;
-              }
-            lineProcess();
+              break;
+            }
+          lineProcess();
 
-          }
-        dev.close();
-      }
-  }
+        }
+      dev.close();
+    }
+}
 
 static const char str_checksum[] = "Checksum?";
 
@@ -125,96 +125,97 @@ static const char str_checksum[] = "Checksum?";
  *
  */
 
-int TaskCli::xorCheck(unsigned char *pc)
-  {
-    ostream & cout = m_cout;
+int
+TaskCli::xorCheck(unsigned char *pc)
+{
+  ostream & cout = m_cout;
 
-    unsigned char c;
-    unsigned char xor1, xor2;
-    unsigned char *q;
-    unsigned char *qq;
+  unsigned char c;
+  unsigned char xor1, xor2;
+  unsigned char *q;
+  unsigned char *qq;
 
-    *pc = ' ';
+  *pc = ' ';
 
-    for (xor1 = 0, q = pc + 1; (c = *q ) != '\0'; ++q)
-      {
-        if (c == '*')
-          break;
+  for (xor1 = 0, q = pc + 1; (c = *q) != '\0'; ++q)
+    {
+      if (c == '*')
+        break;
 
-        xor1 ^= c;
-      }
+      xor1 ^= c;
+    }
 
-    if (c == '\0')
-      {
-        // no xor received, return computed xor
-        cout << endl << ("*" );
-        cout.width( 2);
-        cout << hex << ( unsigned short ) xor1 ;
-        cout.flush();
-        return -2;
-      }
+  if (c == '\0')
+    {
+      // no xor received, return computed xor
+      cout << endl << ("*");
+      cout.width(2);
+      cout << hex << (unsigned short) xor1;
+      cout.flush();
+      return -2;
+    }
 
-    qq = q;
+  qq = q;
 
-    xor2 = 0;
-    q++;
-    c = *q;
-    if (c == '\0')
-      {
-        // syntax error
-        goto err;
-      }
+  xor2 = 0;
+  q++;
+  c = *q;
+  if (c == '\0')
+    {
+      // syntax error
+      goto err;
+    }
 
-    if ( '0' <= c && c <= '9')
-      xor2 = c - '0';
-    else if ( 'A' <= c && c <= 'F')
-      xor2 = c - 'A'+ 10;
-    else
-      {
-        // syntax error
-        goto err;
-      }
-    q++;
-    c = *q;
-    if (c == '\0')
-      {
-        // syntax error
-        goto err;
-      }
+  if ('0' <= c && c <= '9')
+    xor2 = c - '0';
+  else if ('A' <= c && c <= 'F')
+    xor2 = c - 'A' + 10;
+  else
+    {
+      // syntax error
+      goto err;
+    }
+  q++;
+  c = *q;
+  if (c == '\0')
+    {
+      // syntax error
+      goto err;
+    }
 
-    xor2 <<= 4;
+  xor2 <<= 4;
 
-    if ( '0' <= c && c <= '9')
-      xor2 |= c - '0';
-    else if ( 'A' <= c && c <= 'F')
-      xor2 |= c - 'A'+ 10;
-    else
-      {
-        // syntax error
-        goto err;
-      }
+  if ('0' <= c && c <= '9')
+    xor2 |= c - '0';
+  else if ('A' <= c && c <= 'F')
+    xor2 |= c - 'A' + 10;
+  else
+    {
+      // syntax error
+      goto err;
+    }
 
-    q++;
-    c = *q;
-    if (c != '\0')
-      {
-        // syntax error
-        goto err;
-      }
+  q++;
+  c = *q;
+  if (c != '\0')
+    {
+      // syntax error
+      goto err;
+    }
 
-    if (xor1 != xor2)
-      {
-        // crc error
-        cout << endl << str_checksum;
-        return -2; // return 'crc error'
-      }
-    //*qq++ = ' ';
-    *qq = '\0';
+  if (xor1 != xor2)
+    {
+      // crc error
+      cout << endl << str_checksum;
+      return -2; // return 'crc error'
+    }
+  //*qq++ = ' ';
+  *qq = '\0';
 
-    return 0; // return OK
+  return 0; // return OK
 
-    err: return -1; // return 'syntax error'
-  }
+  err: return -1; // return 'syntax error'
+}
 
 static const char str_help[] = "ri|re|rd|rc | st";
 static const char str_unknown[] = "Cmd?";
@@ -229,154 +230,155 @@ static const char str_unknown[] = "Cmd?";
  * 
  */
 
-void TaskCli::lineProcess()
-  {
-    unsigned char * pc;
-    pc = m_line;
+void
+TaskCli::lineProcess()
+{
+  unsigned char * pc;
+  pc = m_line;
 
-    SimpleCli & cli = m_cli;
-    ostream& cout = m_cout;
+  SimpleCli & cli = m_cli;
+  ostream& cout = m_cout;
 
-    unsigned char *p;
-    unsigned char c;
-    unsigned long l;
-    unsigned char v;
-    unsigned char i;
+  unsigned char *p;
+  unsigned char c;
+  unsigned long l;
+  unsigned char v;
+  unsigned char i;
 
-    int r;
+  int r;
 
-    os.WDTreset();
+  os.WDTreset();
 
-    if ( *pc == '$') // is command using checksum?
-      {
-        r = xorCheck(pc);
-        if (r == -2)
-          return;
-        else if (r == -1)
-          goto err;
-      }
+  if (*pc == '$') // is command using checksum?
+    {
+      r = xorCheck(pc);
+      if (r == -2)
+        return;
+      else if (r == -1)
+        goto err;
+    }
 
-cli    .parseReset(); // reset pointer to start of line
+  cli .parseReset(); // reset pointer to start of line
 
-    p = cli.parseNext();
-    if (p == 0)
+  p = cli.parseNext();
+  if (p == 0)
     return;
 
-    c = *p | 0x20;
-    if ( *p == '?')
-      {
-        cout << endl << str_help;
-      }
-    else if (c == 's')
-      {
-        ++p;
-        c = *p;
-        if (c == '\0')
+  c = *p | 0x20;
+  if (*p == '?')
+    {
+      cout << endl << str_help;
+    }
+  else if (c == 's')
+    {
+      ++p;
+      c = *p;
+      if (c == '\0')
         goto err;
 
-        c |= 0x20;
-        if (c == 't')
-          {
-            int cnt;
-            cnt = os.sched.getTasksCount();
+      c |= 0x20;
+      if (c == 't')
+        {
+          int cnt;
+          cnt = os.sched.getTasksCount();
 
-            for (int i = 0; i < cnt; ++i)
-              {
-                OSTask *pt;
-                pt = os.sched.getTask(i);
+          for (int i = 0; i < cnt; ++i)
+            {
+              OSTask *pt;
+              pt = os.sched.getTask(i);
 
-                cout << endl;
-                if (pt == this)
+              cout << endl;
+              if (pt == this)
                 cout << '*';
-                else
+              else
                 cout << ' ';
 
-                cout << *pt; // print task info
-              }
-          }
-        else
+              cout << *pt; // print task info
+            }
+        }
+      else
         goto err;
-      }
-    else if (c == 't')
-      {
-        // test commands
-        ++p;
-        c = *p;
-        if (c == '\0')
+    }
+  else if (c == 't')
+    {
+      // test commands
+      ++p;
+      c = *p;
+      if (c == '\0')
         goto err;
 
-        c |= 0x20;
-        if (c == '1')
-          {
-            if (os.isDebug() )
-            debug.putChar( 'U');
-          }
-        else if (c == '2')
-          {
-            if (os.isDebug() )
+      c |= 0x20;
+      if (c == '1')
+        {
+          if (os.isDebug())
+            debug.putChar('U');
+        }
+      else if (c == '2')
+        {
+          if (os.isDebug())
             clog << "u";
-          }
-        else
+        }
+      else
         goto err;
-      }
-    else if (c == 'r')
-      {
-        // flight recorder test commands
-        ++p;
-        c = *p;
-        if (c == '\0')
-        goto err;
-
-        c |= 0x20;
-        ++p;
-        if ( *p != '\0')
+    }
+  else if (c == 'r')
+    {
+      // flight recorder test commands
+      ++p;
+      c = *p;
+      if (c == '\0')
         goto err;
 
-        if (c == 'i')
-          {
-            if ( (p = cli.parseNext() ) != 0)
-            goto err;
-
-            cout << endl << "uid=00C80A0C";
-            cout << endl << "nmax=0001E9FF";
-            cout << endl << "nbeg=00005F7D";
-          }
-        else if (c == 'e')
-          {
-            if ( (p = cli.parseNext() ) == 0)
-            goto err;
-
-            cout << endl;
-          }
-        else if (c == 'd' || c == 'c' || c == 'o')
-          {
-            if ( (p = cli.parseNext() ) == 0)
-            goto err;
-
-            if (cli.parseHex(p, &l) < 0)
-            goto err;
-
-            if ( (p = cli.parseNext() ) != 0 && cli.parseHexNibble(p, &i) == 0)
-              {
-                v = i;
-              }
-            else
-              {
-                v = 0;
-              }
-
-            cout << endl
-            << "$B06F590E0001CF68638921FA30000028438A21F33000827111FA3801AEFF3200827122FA387FFF883100827112FA387FFFFF3200827121FA3801BA863100827101FB3801E0863120537211FA3C1EB6537311FA3C1CF2537411F93C1DB0537511FA3C0054FF00FF00*CD";
-          }
-        else
+      c |= 0x20;
+      ++p;
+      if (*p != '\0')
         goto err;
-      }
-    else
+
+      if (c == 'i')
+        {
+          if ((p = cli.parseNext()) != 0)
+            goto err;
+
+          cout << endl << "uid=00C80A0C";
+          cout << endl << "nmax=0001E9FF";
+          cout << endl << "nbeg=00005F7D";
+        }
+      else if (c == 'e')
+        {
+          if ((p = cli.parseNext()) == 0)
+            goto err;
+
+          cout << endl;
+        }
+      else if (c == 'd' || c == 'c' || c == 'o')
+        {
+          if ((p = cli.parseNext()) == 0)
+            goto err;
+
+          if (cli.parseHex(p, &l) < 0)
+            goto err;
+
+          if ((p = cli.parseNext()) != 0 && cli.parseHexNibble(p, &i) == 0)
+            {
+              v = i;
+            }
+          else
+            {
+              v = 0;
+            }
+
+          cout << endl
+              << "$B06F590E0001CF68638921FA30000028438A21F33000827111FA3801AEFF3200827122FA387FFF883100827112FA387FFFFF3200827121FA3801BA863100827101FB3801E0863120537211FA3C1EB6537311FA3C1CF2537411F93C1DB0537511FA3C0054FF00FF00*CD";
+        }
+      else
+        goto err;
+    }
+  else
     goto err;
 
-    return;
+  return;
 
-    err: cout << endl << str_unknown;
-    cout << endl << str_help;
-  }
+  err: cout << endl << str_unknown;
+  cout << endl << str_help;
+}
 
