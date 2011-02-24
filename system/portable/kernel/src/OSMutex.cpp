cb530ee9db14a52f0a844028eb693c5361f8a256
Liviu Ionescu <ilg@livius.net>
2011-02-24 00:10:41 +0000

    eventWait() in critical region; indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1720 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/src/OSMutex.cpp
+++ system/portable/kernel/src/OSMutex.cpp
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2007-2009 Liviu Ionescu.
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
  *
  *	This file is part of the uOS++ distribution.
  */
@@ -11,103 +11,113 @@
 #include "portable/kernel/include/OSMutex.h"
 
 OSMutex::OSMutex()
-  {
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("OSMutex()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("OSMutex()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-    m_isAcquired = false;
-    m_pOwnerTask = 0;
-    m_event = (OSEvent_t)this;
-    m_eventRet = OSEventWaitReturn::OS_NONE;
-  }
+  m_isAcquired = false;
+  m_pOwnerTask = 0;
+  m_event = (OSEvent_t) this;
+  m_eventRet = OSEventWaitReturn::OS_NONE;
+}
 
-int OSMutex::acquire(bool noBlock)
-  {
-    m_eventRet = OSEventWaitReturn::OS_NONE;
+int
+OSMutex::acquire(bool doNotBlock)
+{
+  m_eventRet = OSEventWaitReturn::OS_NONE;
 
-    bool isAcquired;
-    for (;;)
-      {
-        OSScheduler::criticalEnter();
-          {
-            isAcquired = m_isAcquired;
-            m_isAcquired = true;
-            if (!isAcquired)
+  bool isAcquired;
+  for (;;)
+    {
+      OSEventWaitReturn_t ret;
+      OSScheduler::criticalEnter();
+        {
+          isAcquired = m_isAcquired;
+          m_isAcquired = true;
+          if (!isAcquired)
+            {
+              // then acquire for the current task
               m_pOwnerTask = OSScheduler::getTaskCurrent();
-          }
-        OSScheduler::criticalExit();
+            }
+          else
+            {
+              if (!doNotBlock)
+                // if we are allowed, block
+                ret = OSScheduler::eventWait(m_event);
+            }
+        }
+      OSScheduler::criticalExit();
 
-        if (!isAcquired)
-          {
+      if (!isAcquired)
+        {
 #if defined(DEBUG) && defined(OS_DEBUG_OSMUTEX_ACQUIRE)
-            OSDeviceDebug::putString("OSMutex::acquire() OK");
-            OSDeviceDebug::putNewLine();
+          OSDeviceDebug::putString("OSMutex::acquire() OK");
+          OSDeviceDebug::putNewLine();
 #endif
-            return OK;
-          }
+          return OS_OK;
+        }
 
-        if (noBlock)
-          return ERROR_WOULD_BLOCK;
+      if (doNotBlock)
+        return OS_WOULD_BLOCK;
 
-        OSEventWaitReturn_t ret;
-        ret = OSScheduler::eventWait(m_event);
-        if (ret != (OSEventWaitReturn_t)m_event && ret
-            != OSEventWaitReturn::OS_ALL)
-          {
-            m_eventRet = ret;
+      if (ret != (OSEventWaitReturn_t) m_event && ret
+          != OSEventWaitReturn::OS_ALL)
+        {
+          m_eventRet = ret;
 #if defined(DEBUG) && defined(OS_DEBUG_OSMUTEX_ACQUIRE)
-            OSDeviceDebug::putString("OSMutex::acquire() event=");
-            OSDeviceDebug::putHex((unsigned short)ret);
-            OSDeviceDebug::putNewLine();
+          OSDeviceDebug::putString("OSMutex::acquire() event=");
+          OSDeviceDebug::putHex((unsigned short)ret);
+          OSDeviceDebug::putNewLine();
 #endif
-            return ERROR_EVENT;
-          }
-      }
-  }
+          return OS_OTHER_EVENT;
+        }
+    }
+}
 
-int OSMutex::release(OSTask * pTask)
-  {
-    if (pTask == 0)
-      pTask = OSScheduler::getTaskCurrent();
+int
+OSMutex::release(OSTask * pTask)
+{
+  if (pTask == 0)
+    pTask = OSScheduler::getTaskCurrent();
 
-    if (pTask != m_pOwnerTask)
-      {
+  if (pTask != m_pOwnerTask)
+    {
 #if defined(DEBUG) && defined(OS_DEBUG_OSMUTEX_RELEASE)
-        OSDeviceDebug::putString("OSMutex::release() not owner");
-        OSDeviceDebug::putNewLine();
+      OSDeviceDebug::putString("OSMutex::release() not owner");
+      OSDeviceDebug::putNewLine();
 #endif
-        return ERROR_NOT_OWNER;
-      }
+      return OS_NOT_OWNER;
+    }
 
-    bool isAcquired;
-    OSScheduler::criticalEnter();
-      {
-        isAcquired = m_isAcquired;
-        m_isAcquired = false;
-        m_pOwnerTask = 0;
-      }
-    OSScheduler::criticalExit();
+  bool isAcquired;
+  OSScheduler::criticalEnter();
+    {
+      isAcquired = m_isAcquired;
+      m_isAcquired = false;
+      m_pOwnerTask = 0;
+    }
+  OSScheduler::criticalExit();
 
-    if (!isAcquired)
-      {
+  if (!isAcquired)
+    {
 #if defined(DEBUG) && defined(OS_DEBUG_OSMUTEX_RELEASE)
-        OSDeviceDebug::putString("OSMutex::release() not acquired");
-        OSDeviceDebug::putNewLine();
+      OSDeviceDebug::putString("OSMutex::release() not acquired");
+      OSDeviceDebug::putNewLine();
 #endif
-        return ERROR_NOT_ACQUIRED;
-      }
-    // notify all tasks waiting for this mutex
-    OSScheduler::eventNotify(m_event, (OSEventWaitReturn_t)m_event);
-    // give other tasks a chance to acquire the mutex
-    OSScheduler::yield();
+      return OS_NOT_ACQUIRED;
+    }
+  // notify all tasks waiting for this mutex
+  OSScheduler::eventNotify(m_event, (OSEventWaitReturn_t) m_event);
+  // give other tasks a chance to acquire the mutex
+  OSScheduler::yield();
 
 #if defined(DEBUG) && defined(OS_DEBUG_OSMUTEX_RELEASE)
-    OSDeviceDebug::putString("OSMutex::release() OK");
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("OSMutex::release() OK");
+  OSDeviceDebug::putNewLine();
 #endif
-    return OK;
-  }
+  return OK;
+}
 
 #endif
