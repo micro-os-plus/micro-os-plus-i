b3388788ad4308cb4079808597babd72be528126
Liviu Ionescu <ilg@livius.net>
2011-02-24 12:50:00 +0000

    indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1738 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsart1.cpp
+++ system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsart1.cpp
@@ -31,7 +31,6 @@
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ          (OS_CFGLONG_OSCILLATOR_HZ / OS_CFGINT_CLOCK_PRESCALER / CFGINT_DEVICECHARACTERBUFFEREDUSART_PRESCALLER)
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_BAUD_CONSTANT     ((CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ / OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE)- 1UL)
 
-
 #if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)
 __attribute__((naked))
 #else
@@ -60,8 +59,8 @@ DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
     unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
     unsigned short txLWM, unsigned char *pRxBuf, unsigned short rxBufSize,
     unsigned short rxHWM, unsigned short rxLWM) :
-    DeviceCharacterBufferedUsartBase(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
-      rxHWM, rxLWM)
+  DeviceCharacterBufferedUsartBase(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf,
+      rxBufSize, rxHWM, rxLWM)
 {
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
   OSDeviceDebug::putString("DeviceCharacterBufferedUsart1()=");
@@ -76,7 +75,7 @@ DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
 DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
     unsigned char *pTxBuf, unsigned short txBufSize, unsigned char *pRxBuf,
     unsigned short rxBufSize) :
-    DeviceCharacterBufferedUsartBase(pTxBuf, txBufSize, pRxBuf, rxBufSize)
+  DeviceCharacterBufferedUsartBase(pTxBuf, txBufSize, pRxBuf, rxBufSize)
 {
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
   OSDeviceDebug::putString("DeviceCharacterBufferedUsart1()=");
@@ -92,22 +91,21 @@ DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
 int
 DeviceCharacterBufferedUsart1::implPortInit(void)
 {
-    this->setPortAddress(&AVR32_USART1);
-    gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_RX_PIN ,0);
-    gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_TX_PIN ,0);
-    usart_options_t usartConfig;
-    usartConfig.baudrate = OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE;
-    usartConfig.channelmode = USART_NORMAL_CHMODE;
-    usartConfig.charlength = 8;
-    usartConfig.paritytype = USART_NO_PARITY;
-    usartConfig.stopbits = USART_1_STOPBIT;
+  this->setPortAddress(&AVR32_USART1);
+  gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_RX_PIN, 0);
+  gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_TX_PIN, 0);
+  usart_options_t usartConfig;
+  usartConfig.baudrate = OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE;
+  usartConfig.channelmode = USART_NORMAL_CHMODE;
+  usartConfig.charlength = 8;
+  usartConfig.paritytype = USART_NO_PARITY;
+  usartConfig.stopbits = USART_1_STOPBIT;
 
-    usart_init_rs232(&AVR32_USART1, &usartConfig, OS_CFGLONG_OSCILLATOR_HZ);
+  usart_init_rs232(&AVR32_USART1, &usartConfig, OS_CFGLONG_OSCILLATOR_HZ);
 
-    INTC_register_interrupt(Usart1_contextHandler,AVR32_USART1_IRQ,0);
+  INTC_register_interrupt(Usart1_contextHandler, AVR32_USART1_IRQ, 0);
   return 0;
 }
-
 
 // ===== ISRs =================================================================
 
@@ -125,29 +123,7 @@ Usart1_contextHandler(void)
   OSScheduler::interruptExit();
 #endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)*/
 }
-#if false
-#if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)
-__attribute__((naked))
-#else
-__attribute__((interrupt))
-#endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)*/
-void
-Usart1RX_contextHandler(void);
 
-void
-Usart1RX_contextHandler(void)
-{
-#if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)
-  OSScheduler::interruptEnter();
-#endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)*/
-    {
-      ((DeviceCharacterBufferedUsart1 *) DeviceCharacterBufferedUsart1::ms_pThis)->interruptRxServiceRoutine();
-    }
-#if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)
-  OSScheduler::interruptExit();
-#endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)*/
-}
-#endif // false
 #endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSARTBASE) */
 
 #endif /* defined(OS_CONFIG_FAMILY_AVR32UC3) */
