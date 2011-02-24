7f5b30527077cb551a82822ce722aa9e1ba5f0a8
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-02-24 10:47:27 +0000

    single interrupt service routine for TX and RX; configuration for usart1
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1729 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsart1.cpp
+++ system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsart1.cpp
@@ -12,6 +12,12 @@
 
 #include "hal/arch/avr32/uc3/devices/character/include/DeviceCharacterBufferedUsart1.h"
 
+#include <avr32/io.h>
+#include "hal/arch/avr32/lib/include/compiler.h"
+#include "hal/arch/avr32/uc3/lib/include/intc.h"
+#include "hal/arch/avr32/uc3/lib/include/usart.h"
+#include "hal/arch/avr32/uc3/lib/include/gpio.h"
+
 #if defined(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_CONSTANT)
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_BAUD_CONSTANT     OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_CONSTANT
 #endif
@@ -24,6 +30,15 @@
 
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ          (OS_CFGLONG_OSCILLATOR_HZ / OS_CFGINT_CLOCK_PRESCALER / CFGINT_DEVICECHARACTERBUFFEREDUSART_PRESCALLER)
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_BAUD_CONSTANT     ((CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ / OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE)- 1UL)
+
+
+#if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)
+__attribute__((naked))
+#else
+__attribute__((interrupt))
+#endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)*/
+void
+Usart1_contextHandler(void);
 
 // ----- static data ----------------------------------------------------------
 
@@ -77,35 +92,40 @@ DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
 int
 DeviceCharacterBufferedUsart1::implPortInit(void)
 {
-  ; // TODO: implement it
+    this->setPortAddress(&AVR32_USART1);
+    gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_RX_PIN ,0);
+    gpio_enable_module_pin(OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_TX_PIN ,0);
+    usart_options_t usartConfig;
+    usartConfig.baudrate = OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE;
+    usartConfig.channelmode = USART_NORMAL_CHMODE;
+    usartConfig.charlength = 8;
+    usartConfig.paritytype = USART_NO_PARITY;
+    usartConfig.stopbits = USART_1_STOPBIT;
+
+    usart_init_rs232(&AVR32_USART1, &usartConfig, OS_CFGLONG_OSCILLATOR_HZ);
+
+    INTC_register_interrupt(Usart1_contextHandler,AVR32_USART1_IRQ,0);
   return 0;
 }
 
 
 // ===== ISRs =================================================================
 
-#if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)
-__attribute__((naked))
-#else
-__attribute__((interrupt))
-#endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)*/
-void
-Usart1TX_contextHandler(void);
 
 void
-Usart1TX_contextHandler(void)
+Usart1_contextHandler(void)
 {
 #if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)
   OSScheduler::interruptEnter();
 #endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)*/
     {
-      ((DeviceCharacterBufferedUsart1 *) DeviceCharacterBufferedUsart1::ms_pThis)->interruptTxServiceRoutine();
+      ((DeviceCharacterBufferedUsart1 *) DeviceCharacterBufferedUsart1::ms_pThis)->interruptServiceRoutine();
     }
 #if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)
   OSScheduler::interruptExit();
 #endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_TX_ISR)*/
 }
-
+#if false
 #if !defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)
 __attribute__((naked))
 #else
@@ -127,7 +147,7 @@ Usart1RX_contextHandler(void)
   OSScheduler::interruptExit();
 #endif /*!defined(OS_EXCLUDE_DEVICECHARACTERBUFFEREDUSART1_NAKED_RX_ISR)*/
 }
-
+#endif // false
 #endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSARTBASE) */
 
 #endif /* defined(OS_CONFIG_FAMILY_AVR32UC3) */
