3b8838b519853da3a7c346fe29c41c390c5179bf
Liviu Ionescu <ilg@livius.net>
2011-02-25 00:00:10 +0000

    updated to _CONFIG/_WRITE/_READ naming convention
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1761 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/boards/AVI/include/DeviceDebugI2C_A0A1_Defines.h
+++ system/hal/boards/AVI/include/DeviceDebugI2C_A0A1_Defines.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2007-2008 Liviu Ionescu.
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
  *
  *      This file is part of the uOS++ distribution.
  */
@@ -10,13 +10,13 @@
 #define OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT             (0)
 #define OS_CFGINT_DEBUG_I2C_EMU_INIT_DELAY_COUNT	(8)
 
-#define OS_CONFIG_DEBUG_SCL_PORT_INIT                   DDRA
-#define OS_CONFIG_DEBUG_SCL_PORT                        PORTA
+#define OS_CONFIG_DEBUG_SCL_PORT_CONFIG                 DDRA
+#define OS_CONFIG_DEBUG_SCL_PORT_WRITE                  PORTA
 #define OS_CONFIG_DEBUG_SCL_PORT_READ                   PINA
 #define OS_CONFIG_DEBUG_SCL_BIT                         0
 
-#define OS_CONFIG_DEBUG_SDA_PORT_INIT                   DDRA
-#define OS_CONFIG_DEBUG_SDA_PORT                        PORTA
+#define OS_CONFIG_DEBUG_SDA_PORT_CONFIG                 DDRA
+#define OS_CONFIG_DEBUG_SDA_PORT_WRITE                  PORTA
 #define OS_CONFIG_DEBUG_SDA_PORT_READ                   PINA
 #define OS_CONFIG_DEBUG_SDA_BIT                         1
 
