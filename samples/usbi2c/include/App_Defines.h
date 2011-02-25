ef42a3f7e1a5c23f9e52cba2b89366eb229e7fc9
Liviu Ionescu <ilg@livius.net>
2011-02-25 02:05:41 +0000

    cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1768 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/usbi2c/include/App_Defines.h
+++ samples/usbi2c/include/App_Defines.h
@@ -22,7 +22,10 @@
 
 // ----------------------------------------------------------------------------
 
-#if defined(OS_CONFIG_BOARD_A0739)
+#if defined(OS_CONFIG_BOARD_AVI_A07523)
+
+#elif defined(OS_CONFIG_BOARD_AVI_A0739)
+
 #define APP_CONFIG_IMUXEN_PORT_INIT     DDRC
 #define APP_CONFIG_IMUXEN_PORT          PORTC
 #define APP_CONFIG_IMUXEN_BIT           PORTC5
@@ -30,6 +33,11 @@
 #define APP_CONFIG_IMUX_PORT_INIT       DDRC
 #define APP_CONFIG_IMUX_PORT            PORTC
 #define APP_CONFIG_IMUX_BIT             PORTC4
-#endif /* defined(OS_CONFIG_BOARD_A0739) */
 
-#endif /*APP_DEFINES_H_ */
+#else
+#error "Missing OS_CONFIG_BOARD_* board definition"
+#endif
+
+// ----------------------------------------------------------------------------
+
+#endif /* APP_DEFINES_H_ */
