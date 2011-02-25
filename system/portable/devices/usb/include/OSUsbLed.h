e77996d8d7dbfe14a4c3a7aee7792b12f8992555
Liviu Ionescu <ilg@livius.net>
2011-02-25 00:04:48 +0000

    updated with new GPIO macros
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1762 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/usb/include/OSUsbLed.h
+++ system/portable/devices/usb/include/OSUsbLed.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2007-2008 Liviu Ionescu.
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
  *
  *      This file is part of the uOS++ distribution.
  */
@@ -7,69 +7,107 @@
 #ifndef OSUSBLED_H_
 #define OSUSBLED_H_
 
+#include "portable/kernel/include/OS.h"
+
 class OSUsbLed
-  {
+{
 public:
   OSUsbLed();
 
-  static void toggle(void);
-  static void on(void);
-  static void off(void);
-  static void init(void);
+  static void
+  init(void);
 
-  unsigned char bitNumber(void);
+  static void
+  on(void);
+
+  static void
+  off(void);
+
+  static void
+  toggle(void);
+
+  unsigned char
+  bitNumber(void);
 
 private:
-  };
+};
 
-inline OSUsbLed::OSUsbLed()
-  {
-  }
+inline
+OSUsbLed::OSUsbLed()
+{
+  // nothing to do, init() will do the magic
+}
 
-// TODO: move to hal
-inline void OSUsbLed::init(void)
-  {
-#if defined(OS_CONFIG_USB_LED_PORT_INIT) && defined(OS_CONFIG_USB_LED_BIT)
-    // init led port as output
-    OS_CONFIG_USB_LED_PORT_INIT |= _BV(OS_CONFIG_USB_LED_BIT);
-    // turn bit off
-    OS_CONFIG_USB_LED_PORT &= ~_BV(OS_CONFIG_USB_LED_BIT);
+inline void
+OSUsbLed::init(void)
+{
+#if defined(OS_CFGREG_OSUSBLED_LED_PORT_CONFIG) && defined(OS_CFGREG_OSUSBLED_LED_PORT_WRITE) && defined(OS_CFGPIN_OSUSBLED_LED_PIN)
+
+  // enable port
+  OS_GPIO_PIN_CONFIG_ENABLE(OS_CFGREG_OSUSBLED_LED_PORT_CONFIG, OS_CFGPIN_OSUSBLED_LED_PIN);
+  // init led port as output
+  OS_GPIO_PIN_CONFIG_OUTPUT(OS_CFGREG_OSUSBLED_LED_PORT_CONFIG, OS_CFGPIN_OSUSBLED_LED_PIN);
+  // turn led off
+#if defined(OS_CFGBOOL_OSUSBLED_LED_ISACTIVE_LOW)
+  OS_GPIO_PIN_HIGH(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
 #else
-#warning "no OS_CONFIG_USB_LED_PORT_INIT defined"
-#endif
-  }
+  OS_GPIO_PIN_LOW(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+#endif /* defined(APP_CONFIG_LED_ISACTIVE_LOW) */
 
-inline void OSUsbLed::toggle(void)
-  {
-#if defined(OS_CONFIG_USB_LED_PORT) && defined(OS_CONFIG_USB_LED_BIT)
-    // toggle led
-    OS_CONFIG_USB_LED_PORT ^= _BV(OS_CONFIG_USB_LED_BIT);
-#endif
-  }
-
-inline void OSUsbLed::on(void)
-  {
-#if defined(OS_CONFIG_USB_LED_PORT) && defined(OS_CONFIG_USB_LED_BIT)
-    // toggle led
-    OS_CONFIG_USB_LED_PORT |= _BV(OS_CONFIG_USB_LED_BIT);
-#endif
-  }
-
-inline void OSUsbLed::off(void)
-  {
-#if defined(OS_CONFIG_USB_LED_PORT) && defined(OS_CONFIG_USB_LED_BIT)
-    // toggle led
-    OS_CONFIG_USB_LED_PORT &= ~_BV(OS_CONFIG_USB_LED_BIT);
-#endif
-  }
-
-inline unsigned char OSUsbLed::bitNumber(void)
-  {
-#if defined(OS_CONFIG_USB_PORT_INIT) && defined(OS_CONFIG_USB_LED_BIT)
-    return OS_CONFIG_USB_LED_BIT;
 #else
-    return 0;
+#warning "Missing OS_CFGREG_OSUSBLED_LED_* definitions"
 #endif
-  }
+}
 
-#endif /*OSUSBLED_H_*/
+inline void
+OSUsbLed::toggle(void)
+{
+#if defined(OS_CFGREG_OSUSBLED_LED_PORT_WRITE) && defined(OS_CFGPIN_OSUSBLED_LED_PIN)
+
+  // toggle led
+  OS_GPIO_PIN_TOGGLE(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+
+#endif
+}
+
+inline void
+OSUsbLed::on(void)
+{
+#if defined(OS_CFGREG_OSUSBLED_LED_PORT_WRITE) && defined(OS_CFGPIN_OSUSBLED_LED_PIN)
+
+  // turn led on
+#if defined(OS_CFGBOOL_OSUSBLED_LED_ISACTIVE_LOW)
+  OS_GPIO_PIN_HIGH(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+#else
+  OS_GPIO_PIN_LOW(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+#endif /* defined(APP_CONFIG_LED_ISACTIVE_LOW) */
+
+#endif
+}
+
+inline void
+OSUsbLed::off(void)
+{
+#if defined(OS_CFGREG_OSUSBLED_LED_PORT_WRITE) && defined(OS_CFGPIN_OSUSBLED_LED_PIN)
+
+  // turn led off
+#if defined(OS_CFGBOOL_OSUSBLED_LED_ISACTIVE_LOW)
+  OS_GPIO_PIN_LOW(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+#else
+  OS_GPIO_PIN_HIGH(OS_CFGREG_OSUSBLED_LED_PORT_WRITE, OS_CFGPIN_OSUSBLED_LED_PIN);
+#endif /* defined(APP_CONFIG_LED_ISACTIVE_LOW) */
+
+#endif
+}
+
+inline unsigned char
+OSUsbLed::bitNumber(void)
+{
+#if  defined(OS_CFGPIN_OSUSBLED_LED_PIN)
+  return OS_CFGPIN_OSUSBLED_LED_PIN;
+#else
+  return 0;
+#endif
+}
+
+#endif /* OSUSBLED_H_ */
