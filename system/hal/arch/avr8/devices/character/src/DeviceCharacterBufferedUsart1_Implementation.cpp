2f30b9199fb343bd3ccdff251a55436a5730b88d
Liviu Ionescu <ilg@livius.net>
2011-03-01 23:22:21 +0000

    renamed to _Implementation
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1812 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr8/devices/character/src/DeviceCharacterBufferedUsart1_Implementation.cpp
+++ system/hal/arch/avr8/devices/character/src/DeviceCharacterBufferedUsart1_Implementation.cpp
@@ -25,53 +25,6 @@
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ          (OS_CFGLONG_OSCILLATOR_HZ / OS_CFGINT_CLOCK_PRESCALER / CFGINT_DEVICECHARACTERBUFFEREDUSART_PRESCALLER)
 #define CFGINT_DEVICECHARACTERBUFFEREDUSART_BAUD_CONSTANT     ((CFGINT_DEVICECHARACTERBUFFEREDUSART_CLOCK_HZ / OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_BAUD_RATE)- 1UL)
 
-// ----- static data ---------------------------------------------------------
-
-DeviceCharacterBufferedUsart1 * DeviceCharacterBufferedUsart1::ms_pThis;
-
-// ----- constructors --------------------------------------------------------
-
-DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(void)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsart1()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-}
-
-// set custom high/low water marks
-DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
-    unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
-    unsigned short txLWM, unsigned char *pRxBuf, unsigned short rxBufSize,
-    unsigned short rxHWM, unsigned short rxLWM) :
-  OSDeviceCharacterBuffered(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
-      rxHWM, rxLWM)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsart1()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-
-  ms_pThis = this;
-}
-
-// use default 3/4 high and 1/4 low water marks
-DeviceCharacterBufferedUsart1::DeviceCharacterBufferedUsart1(
-    unsigned char *pTxBuf, unsigned short txBufSize, unsigned char *pRxBuf,
-    unsigned short rxBufSize) :
-  OSDeviceCharacterBuffered(pTxBuf, txBufSize, pRxBuf, rxBufSize)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsart1()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-
-  ms_pThis = this;
-}
-
 // ----- inits ---------------------------------------------------------------
 
 int
