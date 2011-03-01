8491a5699ad2722a820641a41d95b1c70344726c
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-03-01 11:08:46 +0000

    DeviceCharacterBufferedUsart0.h added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1782 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/hal/arch/avr32/uc3/devices/character/include/DeviceCharacterBufferedUsart0.h
@@ -0,0 +1,34 @@
+/*
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef HAL_DEVICECHARACTERBUFFEREDUSART0_H_
+#define HAL_DEVICECHARACTERBUFFEREDUSART0_H_
+
+#include "hal/arch/avr32/uc3/devices/character/include/DeviceCharacterBufferedUsartBase.h"
+
+class DeviceCharacterBufferedUsart0 : public DeviceCharacterBufferedUsartBase
+{
+public:
+  DeviceCharacterBufferedUsart0();
+
+  DeviceCharacterBufferedUsart0(unsigned char *pRxBuf,
+      unsigned short rxBufSize, unsigned short rxHWM, unsigned short rxLWM,
+      unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
+      unsigned short txLWM);
+
+      DeviceCharacterBufferedUsart0(unsigned char *pRxBuf,
+          unsigned short rxBufSize, unsigned char *pTxBuf,
+          unsigned short txBufSize);
+
+public:
+  virtual int
+  implPortInit(void);
+
+  static DeviceCharacterBufferedUsart0 * ms_pThis;
+
+};
+
+#endif /* HAL_DEVICECHARACTERBUFFEREDUSART0_H_ */
