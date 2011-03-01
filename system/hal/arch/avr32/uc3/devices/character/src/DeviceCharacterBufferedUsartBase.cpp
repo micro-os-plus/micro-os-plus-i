22d17b669fa7df3c16b8813cc032a4b81abeb3ca
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-03-01 11:13:48 +0000

    not using framework functions
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1785 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
+++ system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
@@ -17,94 +17,96 @@
 // ----- constructors ---------------------------------------------------------
 
 DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(void)
-  {
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 // set custom high/low water marks
 DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
     unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
     unsigned short txLWM, unsigned char *pRxBuf, unsigned short rxBufSize,
     unsigned short rxHWM, unsigned short rxLWM) :
-OSDeviceCharacterBuffered(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
-    rxHWM, rxLWM)
-  {
+  OSDeviceCharacterBuffered(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
+      rxHWM, rxLWM)
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 // use default 3/4 high and 1/4 low water marks
 DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
     unsigned char *pTxBuf, unsigned short txBufSize, unsigned char *pRxBuf,
     unsigned short rxBufSize) :
-OSDeviceCharacterBuffered(pTxBuf, txBufSize, pRxBuf, rxBufSize)
-  {
+  OSDeviceCharacterBuffered(pTxBuf, txBufSize, pRxBuf, rxBufSize)
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 void
-DeviceCharacterBufferedUsartBase::setPortAddress( volatile avr32_usart_t* pPort)
-  {
-    m_pPort = pPort;
-  }
+DeviceCharacterBufferedUsartBase::setPortAddress(volatile avr32_usart_t* pPort)
+{
+  m_pPort = pPort;
+}
 
 void
 DeviceCharacterBufferedUsartBase::interruptServiceRoutine(void)
-  {
-    int rc;
-    rc = usart_tx_ready(m_pPort);
-    if(rc)
-      {
-        interruptTxServiceRoutine();
-      }
-    rc = usart_test_hit(m_pPort);
-    if(rc)
-      {
-        interruptRxServiceRoutine();
-      }
-  }
+{
+  // check if the USART Transmit Shift Register and the USART Transmit
+  //         Holding Register are free
+  if ((m_pPort->csr & AVR32_USART_CSR_TXRDY_MASK) != 0)
+    {
+      interruptTxServiceRoutine();
+    }
+  // check if the USART Receive Holding Register is full
+  if ((m_pPort->csr & AVR32_USART_CSR_RXRDY_MASK) != 0)
+    {
+      interruptRxServiceRoutine();
+    }
+}
 
 int
 DeviceCharacterBufferedUsartBase::implPortDisable(void)
-  {
-    usart_reset(m_pPort);
-    return 0;
-  }
+{
+  usart_reset(m_pPort);
+  return 0;
+}
 
 void
 DeviceCharacterBufferedUsartBase::implInterruptTxEnable(void)
-  {
-    usart_int_tx_enable(m_pPort);
-  }
+{
+  m_pPort->ier = AVR32_USART_IER_TXRDY_MASK;
+}
 
 void
 DeviceCharacterBufferedUsartBase::implInterruptTxDisable(void)
-  {
-    usart_int_tx_disable(m_pPort);
-  }
+{
+  m_pPort->idr = AVR32_USART_IER_TXRDY_MASK;
+}
 
 unsigned char
 DeviceCharacterBufferedUsartBase::implPortRead(void)
-  {
-   return usart_basic_read_char(m_pPort);
-  }
+{
+  return (unsigned char) (m_pPort->rhr & AVR32_USART_RHR_RXCHR_MASK)
+      >> AVR32_USART_RHR_RXCHR_OFFSET;
+}
 
 void
 DeviceCharacterBufferedUsartBase::implPortWrite(unsigned char b)
-  {
-    usart_basic_write_char(m_pPort,b);
-  }
+{
+  m_pPort->thr = (b << AVR32_USART_THR_TXCHR_OFFSET)
+      & AVR32_USART_THR_TXCHR_MASK;
+}
 
 #endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSARTBASE) */
 
