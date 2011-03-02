49dbb8957307584b618bd46417918dddc3e0ad7a
Liviu Ionescu <ilg@livius.net>
2011-03-02 09:07:20 +0000

    constructors removed, since they are already in the portable code; comments and cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1817 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
+++ system/hal/arch/avr32/uc3/devices/character/src/DeviceCharacterBufferedUsartBase.cpp
@@ -14,51 +14,16 @@
 
 #include "hal/arch/avr32/uc3/lib/include/usart.h"
 
-// ----- constructors ---------------------------------------------------------
-
-DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(void)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-}
-
-// set custom high/low water marks
-DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
-    unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
-    unsigned short txLWM, unsigned char *pRxBuf, unsigned short rxBufSize,
-    unsigned short rxHWM, unsigned short rxLWM) :
-  OSDeviceCharacterBuffered(pTxBuf, txBufSize, txHWM, txLWM, pRxBuf, rxBufSize,
-      rxHWM, rxLWM)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-}
-
-// use default 3/4 high and 1/4 low water marks
-DeviceCharacterBufferedUsartBase::DeviceCharacterBufferedUsartBase(
-    unsigned char *pTxBuf, unsigned short txBufSize, unsigned char *pRxBuf,
-    unsigned short rxBufSize) :
-  OSDeviceCharacterBuffered(pTxBuf, txBufSize, pRxBuf, rxBufSize)
-{
-#if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-  OSDeviceDebug::putString("DeviceCharacterBufferedUsartBase()=");
-  OSDeviceDebug::putPtr(this);
-  OSDeviceDebug::putNewLine();
-#endif
-}
-
+// The actual device implementation will call it to
+// store the specific port address.
 void
 DeviceCharacterBufferedUsartBase::setPortAddress(volatile avr32_usart_t* pPort)
 {
   m_pPort = pPort;
 }
 
+// The single interrupt code defined by AVR32, will determine the
+// interrupt source and call the appropriate portable routine
 void
 DeviceCharacterBufferedUsartBase::interruptServiceRoutine(void)
 {
@@ -82,18 +47,21 @@ DeviceCharacterBufferedUsartBase::implPortDisable(void)
   return 0;
 }
 
+// Trigger the transmit, so the TX queue will start to go out.
 void
 DeviceCharacterBufferedUsartBase::implInterruptTxEnable(void)
 {
   m_pPort->ier = AVR32_USART_IER_TXRDY_MASK;
 }
 
+// Disable transmit (when the queue is empty)
 void
 DeviceCharacterBufferedUsartBase::implInterruptTxDisable(void)
 {
   m_pPort->idr = AVR32_USART_IER_TXRDY_MASK;
 }
 
+// Read a byte from the AVR32 hardware
 unsigned char
 DeviceCharacterBufferedUsartBase::implPortRead(void)
 {
@@ -101,6 +69,7 @@ DeviceCharacterBufferedUsartBase::implPortRead(void)
       >> AVR32_USART_RHR_RXCHR_OFFSET;
 }
 
+// Write a byte to the AVR32 hardware
 void
 DeviceCharacterBufferedUsartBase::implPortWrite(unsigned char b)
 {
