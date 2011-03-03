4f653feaa3c86db9fe8092640bf7690e800f9927
Catalin Ioana <catalinio@gmail.com>
2011-03-03 15:39:05 +0000

    First commit of the project. Clone the sample-minimal, in order to start the new project.
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1825 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ samples/mutex/include/App_Defines.h
@@ -0,0 +1,10 @@
+/*
+ *	Copyright (C) 2007-2008 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#ifndef APP_DEFINES_H_
+#define APP_DEFINES_H_
+
+#endif /*APP_DEFINES_H_ */
---
+++ samples/mutex/src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ *	Copyright (C) 2007-2008 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/uOS.h"
+
+// Here include headers for other object required by tasks,
+// like local objects (system objects should be visible via uOS.h)
+
+// Here include headers for local tasks
+
+// ------------------------------------------------------------------------------
+
+/*
+ * This is the minimal application in the uOS++ environment. 
+ * 
+ * As expected, it does nothing, or, in scheduler terms, permanently runs
+ * the idle task.
+ */
+
+// ------------------------------------------------------------------------------
+
+// message to be displayed on the debugging device at startup
+#if defined(DEBUG)
+const char greeting[]= "AVI minimal v1.2";
+#endif
+
