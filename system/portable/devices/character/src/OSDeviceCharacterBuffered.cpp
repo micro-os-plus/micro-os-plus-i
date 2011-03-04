b1581caef591ac57629404ac67ab248e0bc47142
Liviu Ionescu <ilg@livius.net>
2011-03-04 23:02:27 +0000

    implAvailableRead() return value fixed; critical sections moved here form parent class, to minimize duration; indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1829 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/character/src/OSDeviceCharacterBuffered.cpp
+++ system/portable/devices/character/src/OSDeviceCharacterBuffered.cpp
@@ -15,213 +15,250 @@
 // ----- constructors ---------------------------------------------------------
 
 OSDeviceCharacterBuffered::OSDeviceCharacterBuffered(void)
-  {
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("OSDeviceCharacterBuffered()=");
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("OSDeviceCharacterBuffered()=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 OSDeviceCharacterBuffered::OSDeviceCharacterBuffered(unsigned char *pRxBuf,
     unsigned short rxBufSize, unsigned short rxHWM, unsigned short rxLWM,
     unsigned char *pTxBuf, unsigned short txBufSize, unsigned short txHWM,
     unsigned short txLWM) :
-m_rxBuf(pRxBuf, rxBufSize, rxHWM, rxLWM), m_txBuf(pTxBuf, txBufSize, txHWM,
-    txLWM)
-  {
+  m_rxBuf(pRxBuf, rxBufSize, rxHWM, rxLWM), m_txBuf(pTxBuf, txBufSize, txHWM,
+      txLWM)
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered()="));
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered()="));
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 OSDeviceCharacterBuffered::OSDeviceCharacterBuffered(unsigned char *pRxBuf,
     unsigned short rxBufSize, unsigned char *pTxBuf, unsigned short txBufSize) :
-m_rxBuf(pRxBuf, rxBufSize, rxBufSize * 3 / 4, rxBufSize / 4), m_txBuf(pTxBuf,
-    txBufSize, txBufSize * 3 / 4, txBufSize / 4)
-  {
+  m_rxBuf(pRxBuf, rxBufSize, rxBufSize * 3 / 4, rxBufSize / 4), m_txBuf(pTxBuf,
+      txBufSize, txBufSize * 3 / 4, txBufSize / 4)
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered()="));
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered()="));
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
-  }
+}
 
 void
-OSDeviceCharacterBuffered::init(unsigned char *pTxBuf, unsigned short txBufSize,
-    unsigned short txHWM, unsigned short txLWM, unsigned char *pRxBuf,
-    unsigned short rxBufSize, unsigned short rxHWM, unsigned short rxLWM)
-  {
+OSDeviceCharacterBuffered::init(unsigned char *pTxBuf,
+    unsigned short txBufSize, unsigned short txHWM, unsigned short txLWM,
+    unsigned char *pRxBuf, unsigned short rxBufSize, unsigned short rxHWM,
+    unsigned short rxLWM)
+{
 #if defined(OS_INCLUDE_DEVICECHARACTER_TYPE)
-    m_type = OS_DEVICECHARACTER_USART;
+  m_type = OS_DEVICECHARACTER_USART;
 #endif
 
-    m_txBuf.init(pTxBuf, txBufSize, txHWM, txLWM);
-    m_rxBuf.init(pRxBuf, rxBufSize, rxHWM, rxLWM);
-  }
+  m_txBuf.init(pTxBuf, txBufSize, txHWM, txLWM);
+  m_rxBuf.init(pRxBuf, rxBufSize, rxHWM, rxLWM);
+}
 
 // ----- open/close -----------------------------------------------------------
 
 int
 OSDeviceCharacterBuffered::implOpen(void)
-  {
-    OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered::implOpen()"));
-    OSDeviceDebug::putNewLine();
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered::implOpen()"));
+  OSDeviceDebug::putNewLine();
 
-    //assert(ms_pThis == this);
+  //assert(ms_pThis == this);
 
-    implPortInit();
-    return 0;
-  }
+  implPortInit();
+  return 0;
+}
 
 int
 OSDeviceCharacterBuffered::implClose(void)
-  {
-    OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered::implClose()"));
-    OSDeviceDebug::putNewLine();
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceCharacterBuffered::implClose()"));
+  OSDeviceDebug::putNewLine();
 
-    // TODO: probably should wait queue to drain and disable port
-    implPortDisable();
+  // TODO: probably should wait queue to drain and disable port
+  implPortDisable();
 
-    return 0;
-  }
+  return 0;
+}
 
 bool
 OSDeviceCharacterBuffered::implIsConnected() const
-  {
-    // TODO: implement connection mechanisms
-    return true;
-  }
+{
+  // TODO: implement connection mechanisms
+  return true;
+}
 
 int
 OSDeviceCharacterBuffered::implPortDisable(void)
-  {
-    return OSReturn::OS_OK;
-  }
+{
+  return OSReturn::OS_OK;
+}
 
 // ----- read -----------------------------------------------------------------
 
 bool
 OSDeviceCharacterBuffered::implCanRead()
-  {
-    return !m_rxBuf.isEmpty();
-  }
+{
+  bool f;
+  OSScheduler::criticalEnter();
+    {
+      !m_rxBuf.isEmpty();
+    }
+  OSScheduler::criticalExit();
+
+  return f;
+}
 
 int
 OSDeviceCharacterBuffered::implAvailableRead()
-  {
-    return !m_rxBuf.length();
-  }
+{
+  int r;
+  OSScheduler::criticalEnter();
+    {
+      r = m_rxBuf.length();
+    }
+  OSScheduler::criticalExit();
+
+  return r;
+}
 
 int
 OSDeviceCharacterBuffered::implReadByte(void)
-  {
-    return m_rxBuf.get();
-  }
+{
+  int r;
+  OSScheduler::criticalEnter();
+    {
+      r = m_rxBuf.get();
+    }
+  OSScheduler::criticalExit();
+
+  return r;
+}
 
 void
 OSDeviceCharacterBuffered::interruptRxServiceRoutine(void)
-  {
+{
 #if defined(OS_DEBUG_OSDEVICECHARACTERBUFFERED_ISR) && false
-    OSDeviceDebug::putChar('#');
-   // OSDeviceDebug::putPtr(this);
-   // OSDeviceDebug::putChar(' ');
-    //m_rxBuf.dump();
+  OSDeviceDebug::putChar('#');
+  // OSDeviceDebug::putPtr(this);
+  // OSDeviceDebug::putChar(' ');
+  //m_rxBuf.dump();
 #endif
 
-    unsigned char c;
+  unsigned char c;
 
-    c = implPortRead();
+  c = implPortRead();
 
-    if (!m_rxBuf.isFull())
-      {
-        //OSDeviceDebug::putChar(c);
-        m_rxBuf.put(c);
-      }
-    else
-      {
-        ; // character is lost, no place to store it in the receive queue
-        OSDeviceDebug::putChar('-');
-      }
+  if (!m_rxBuf.isFull())
+    {
+      //OSDeviceDebug::putChar(c);
+      m_rxBuf.put(c);
+    }
+  else
+    {
+      ; // character is lost, no place to store it in the receive queue
+      OSDeviceDebug::putChar('-');
+    }
 #if defined(OS_DEBUG_OSDEVICECHARACTERBUFFERED_ISR) && false
-    OSDeviceDebug::putHex((unsigned short)getReadEvent());
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putHex((unsigned short)getReadEvent());
+  OSDeviceDebug::putNewLine();
 #endif
 
-    OSScheduler::eventNotify(getReadEvent());
-  }
+  OSScheduler::eventNotify(getReadEvent());
+}
 
 // ----- write ---------------------------------------------------------------
 
 bool
 OSDeviceCharacterBuffered::implCanWrite(void)
-  {
-    //OSDeviceDebug::putString("OSDeviceCharacterBuffered::implCanWrite() ");
-    //OSDeviceDebug::putNewLine();
+{
+  //OSDeviceDebug::putString("OSDeviceCharacterBuffered::implCanWrite() ");
+  //OSDeviceDebug::putNewLine();
+  bool f;
+  OSScheduler::criticalEnter();
+    {
+      f = !m_txBuf.isFull();
+    }
+  OSScheduler::criticalExit();
 
-    return !m_txBuf.isFull();
-  }
+  return f;
+}
 
 int
 OSDeviceCharacterBuffered::implWriteByte(unsigned char b)
-  {
-    m_txBuf.put(b);
+{
+  OSScheduler::criticalEnter();
+    {
+      m_txBuf.put(b);
 
-    if (m_txBuf.isAboveHighWM())
-      {
-        implInterruptTxEnable(); // start transmission
-      }
+      if (m_txBuf.isAboveHighWM())
+        {
+          implInterruptTxEnable(); // start transmission
+        }
+    }
+  OSScheduler::criticalExit();
 
-    return b;
-  }
+  return b;
+}
 
 int
 OSDeviceCharacterBuffered::implFlush(void)
-  {
-    //OSDeviceDebug::putChar('f');
-    if (!m_txBuf.isEmpty())
-      {
-        implInterruptTxEnable();
-      }
-    return 0;
-  }
+{
+  //OSDeviceDebug::putChar('f');
+  OSScheduler::criticalEnter();
+    {
+      if (!m_txBuf.isEmpty())
+        {
+          implInterruptTxEnable();
+        }
+    }
+  OSScheduler::criticalExit();
+
+  return 0;
+}
 
 void
 OSDeviceCharacterBuffered::interruptTxServiceRoutine(void)
-  {
+{
 #if defined(OS_DEBUG_OSDEVICECHARACTERBUFFERED_ISR) && false
-    OSDeviceDebug::putChar('|');
-    OSDeviceDebug::putPtr(this);
-    OSDeviceDebug::putChar(' ');
-    m_txBuf.dump();
+  OSDeviceDebug::putChar('|');
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putChar(' ');
+  m_txBuf.dump();
 #endif
 
-    if (m_txBuf.isEmpty())
-      {
-        implInterruptTxDisable();
+  if (m_txBuf.isEmpty())
+    {
+      implInterruptTxDisable();
 #if defined(OS_DEBUG_OSDEVICECHARACTERBUFFERED_ISR)
-        OSDeviceDebug::putChar('<');
-        OSDeviceDebug::putNewLine();
+      OSDeviceDebug::putChar('<');
+      OSDeviceDebug::putNewLine();
 #endif
-      }
-    else
-      {
-        unsigned char c;
-        c = m_txBuf.get();
+    }
+  else
+    {
+      unsigned char c;
+      c = m_txBuf.get();
 #if defined(OS_DEBUG_OSDEVICECHARACTERBUFFERED_ISR) && false
-        OSDeviceDebug::putHex(c);
+      OSDeviceDebug::putHex(c);
 #endif
-        implPortWrite(c);
+      implPortWrite(c);
 
-        if (m_txBuf.isBelowLowWM())
-          {
-            OSScheduler::eventNotify(getWriteEvent());
-            //OSDeviceDebug::putChar('n');
-          }
-      }
-  }
+      if (m_txBuf.isBelowLowWM())
+        {
+          OSScheduler::eventNotify(getWriteEvent());
+          //OSDeviceDebug::putChar('n');
+        }
+    }
+}
 
 #endif /* OS_INCLUDE_OSDEVICECHARACTERBUFFERED */
 
