857900ad08047623ceee46f6de13c724ad194148
Liviu Ionescu <ilg@livius.net>
2011-03-09 23:21:16 +0000

    usart0 and usart1 as separate configs
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1865 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/cliUsart/src/main.cpp
+++ samples/cliUsart/src/main.cpp
@@ -6,20 +6,40 @@
 
 #include "portable/kernel/include/uOS.h"
 
-#include "portable/devices/character/include/DeviceCharacterBufferedUsart1.h"
 #include "portable/tasks/include/TaskBlink.h"
 #include "TaskCli.h"
 
 // ------------------------------------------------------------------------------
 
+#if defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSART0)
+
+#include "portable/devices/character/include/DeviceCharacterBufferedUsart0.h"
+
 // USART buffers
-unsigned char tx_store[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_TXBUF_SIZE];
-unsigned char rx_store[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_RXBUF_SIZE];
+unsigned char tx_store0[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART0_TXBUF_ARRAY_SIZE];
+unsigned char rx_store0[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART0_RXBUF_ARRAY_SIZE];
 
-DeviceCharacterBufferedUsart1 devUsart(tx_store, sizeof(tx_store), rx_store,
-    sizeof(rx_store));
+DeviceCharacterBufferedUsart0 devUsart0(tx_store0, sizeof(tx_store0), rx_store0,
+    sizeof(rx_store0));
 
-TaskCli taskCli("usart", devUsart);
+TaskCli taskCli0("usart0", devUsart0);
+
+#endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSART0) */
+
+#if defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSART1)
+
+#include "portable/devices/character/include/DeviceCharacterBufferedUsart1.h"
+
+// USART buffers
+unsigned char tx_store1[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_TXBUF_ARRAY_SIZE];
+unsigned char rx_store1[OS_CFGINT_DEVICECHARACTERBUFFEREDUSART1_RXBUF_ARRAY_SIZE];
+
+DeviceCharacterBufferedUsart1 devUsart1(tx_store1, sizeof(tx_store1), rx_store1,
+    sizeof(rx_store1));
+
+TaskCli taskCli1("usart1", devUsart1);
+
+#endif /* defined(OS_INCLUDE_DEVICECHARACTERBUFFEREDUSART1) */
 
 
 // ------------------------------------------------------------------------------
