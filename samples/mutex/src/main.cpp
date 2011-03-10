49be22b5a8b0000e63090ec71e4aaf6242f8ef00
Liviu Ionescu <ilg@livius.net>
2011-03-10 20:20:03 +0000

    mutex instance, not a function called mutex; all tasks activated; rand() moved to stress task; interrupt removed.
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1882 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/mutex/src/main.cpp
+++ samples/mutex/src/main.cpp
@@ -12,7 +12,7 @@
 // ---------------------------------------------------------------------------
 
 // the mutex which protects the shared region
-OSMutex mutex();
+OSMutex mutex;
 
 // tasks allocated on static storage
 
@@ -21,21 +21,18 @@ TaskBlink taskA("A", APP_CONFIG_LED1, APP_CFGINT_BLINK_TICKS);
 
 // the tasks, which will attempt to access the protected region
 TaskStress task0("0", 0);
-//TaskStress task1("1", 1);
-//TaskStress task2("2", 2);
-//TaskStress task3("3", 3);
-//TaskStress task4("4", 4);
-//TaskStress task5("5", 5);
-//TaskStress task6("6", 6);
-//TaskStress task7("7", 7);
-//TaskStress task8("8", 8);
-//TaskStress task9("9", 9);
+TaskStress task1("1", 1);
+TaskStress task2("2", 2);
+TaskStress task3("3", 3);
+TaskStress task4("4", 4);
+TaskStress task5("5", 5);
+TaskStress task6("6", 6);
+TaskStress task7("7", 7);
+TaskStress task8("8", 8);
+TaskStress task9("9", 9);
 
 // ---------------------------------------------------------------------------
 // global variables shared by all tasks
-
-// the random global variable
-unsigned int TaskStress::ms_rand;
 
 
 
@@ -48,25 +45,3 @@ int resourceAccessNum[APP_CONFIG_TASKS_NUM_MAX];
 
 // ---------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------
-
-#if defined(OS_INCLUDE_OSSAPPLICATIONIMPL_INTERRUPTTICK)
-
-// artificially make the system timer interrupt longer, so that we
-// increase the chance of having nested interrupts.
-
-void
-OSApplicationImpl::interruptTick(void)
-{
-#if defined(APP_INCLUDE_LENGTHEN_SYSTICK)
-#if defined(APP_INCLUDE_BUSY_WAIT)
-  OS::busyWaitMicros(1000000/OS_CFGINT_TICK_RATE_HZ*APP_BUSY_PROCENTAGE/100);
-#else
-  for (int i = APP_CFGINT_NOTIFIES; --i;)
-    os.sched.eventNotify(APP_EVENT_DUMMY);
-#endif
-#endif
-
-}
-
-#endif /* defined(OS_INCLUDE_OSSAPPLICATIONIMPL_INTERRUPTTICK) */
