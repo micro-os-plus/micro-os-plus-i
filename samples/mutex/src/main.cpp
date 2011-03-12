3fc71d386dafc0a75855056d3d06e4b950ae4ef1
Liviu Ionescu <ilg@livius.net>
2011-03-12 18:43:38 +0000

    configurable with or without OSMUTEX_WAITING_TASKS; test described; cleanups
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1894 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/mutex/src/main.cpp
+++ samples/mutex/src/main.cpp
@@ -7,19 +7,62 @@
 #include "portable/kernel/include/uOS.h"
 
 #include "TaskBlink.h"
+#include "TaskReport.h"
 #include "TaskStress.h"
 
 // ---------------------------------------------------------------------------
 
-// the mutex which protects the shared region
-OSMutex mutex;
+/*
+ * This project is intended to stress test the mutex synchronisation mechanism.
+ *
+ * The test method uses 10 instances of the stress task, all competing for the
+ * same mutex. When each one acquires the mutex, a task counter is incremented.
+ *
+ * A separate reporting task outputs on the debug interface the current
+ * counters.
+ *
+ * There are two implementations of the mutex, the extended one with a local
+ * round-robin notification mechanism, but more expensive, and one using
+ * the scheduler mechanism to notify all tasks, that favours tasks by
+ * registration order.
+ *
+ * The extended version should produce a more or less uniform distribution
+ * of the counters. The simple one will favour first registered tasks.
+ *
+ * For applications with lots of active clients waiting for the same mutex,
+ * it is recommended to use the extended version.
+ *
+ * For usual applications with only two clients for a mutex, the simple
+ * version is good enough.
+ *
+ * To activate the extended version, use OS_INCLUDE_OSMUTEX_WAITING_TASKS.
+ *
+ * For this test, an application definition can switch between standard
+ * and extended behaviours: APP_INCLUDE_OSMUTEX_WAITING_TASKS
+ */
 
-// tasks allocated on static storage
+// ---------------------------------------------------------------------------
 
-// the main task, used to output statistics
-TaskBlink taskA("A", APP_CONFIG_LED1, APP_CFGINT_BLINK_TICKS);
+// the mutex used to protect the shared variables
 
-// the tasks, which will attempt to access the protected region
+#if defined(OS_INCLUDE_OSMUTEX_WAITING_TASKS)
+  OSTask* waitingTasksArray[APP_CONFIG_RESOURCES_ARRAY_SIZE];
+#if defined(APP_INCLUDE_OSMUTEX_WAITING_TASKS)
+  OSMutex mutex(waitingTasksArray, sizeof(waitingTasksArray)/sizeof(waitingTasksArray[0]));
+#else
+  OSMutex mutex;
+#endif
+#else
+  OSMutex mutex;
+#endif /* defined(OS_INCLUDE_OSMUTEX_WAITING_TASKS) */
+
+// the blink task, used to show we are alive
+TaskBlink taskB("B", APP_CONFIG_LED1, APP_CFGINT_TASKBLINK_TICKS);
+
+// the reporting task, used to output statistics
+TaskReport taskR("R", APP_CFGINT_TASKREPORT_SECONDS);
+
+// the stress tasks, which will attempt to access the protected region
 TaskStress task0("0", 0);
 TaskStress task1("1", 1);
 TaskStress task2("2", 2);
@@ -32,16 +75,14 @@ TaskStress task8("8", 8);
 TaskStress task9("9", 9);
 
 // ---------------------------------------------------------------------------
-// global variables shared by all tasks
+// global variables shared by all tasks and protected by the mutex
 
-
-
-// the actual protected region (basically a global counter)
+// the global counter
 int resourceValue;
 
-// global array where each tasks increments its number of access of the resource
+// global array where each tasks increments its own counter
 // it is used for debugging and live statistics
-int resourceAccessNum[APP_CONFIG_TASKS_NUM_MAX];
+int resourceAccessNum[APP_CONFIG_RESOURCES_ARRAY_SIZE];
 
 // ---------------------------------------------------------------------------
 
