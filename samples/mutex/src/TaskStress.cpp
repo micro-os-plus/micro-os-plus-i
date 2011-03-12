1d283a3ce71101fc4aa8ebf610cc22c6240805d5
Liviu Ionescu <ilg@livius.net>
2011-03-12 18:45:16 +0000

    rand() with scheduler lock
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1896 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/mutex/src/TaskStress.cpp
+++ samples/mutex/src/TaskStress.cpp
@@ -15,7 +15,6 @@ extern int resourceAccessNum[];
 // the random global variable
 unsigned int TaskStress::ms_rand;
 
-
 /*
  * Task constructor.
  * Initialise system task object and store parameters in private members.
@@ -34,9 +33,9 @@ TaskStress::TaskStress(const char *pName, int taskId) :
 
 /*
  * Task main code.
- * Initialise led and toggle it using the rate.
+ * Acquire the mutex, increment counter and release the mutex.
  *
- * The toggle rate is done with busy wait, the loop being interrupted by yields.
+ * The task activity is simulated with some random sleep().
  */
 
 void
@@ -48,6 +47,8 @@ TaskStress::taskMain(void)
         {
           debug.putString("Task '");
           debug.putString(getName());
+          debug.putString("', id=");
+          debug.putDec((unsigned short)m_taskId);
           debug.putNewLine();
         }
       os.sched.unlock();
@@ -59,25 +60,23 @@ TaskStress::taskMain(void)
   for (;;)
     {
       // sleep for a random number of ticks
-      sleepTicks = (rand() % (m_maxSleepTicks - m_minSleepTicks)) + m_minSleepTicks;
+      sleepTicks = (rand() % (m_maxSleepTicks - m_minSleepTicks))
+          + m_minSleepTicks;
       os.sched.timerTicks.sleep(sleepTicks);
-
-      // obtain mutex while did not reach the maximum number of access
-      //if (resourceAccessNum[m_taskId] > m_maxAccessNum)
-      //  continue;
 
       // block task until the mutex is acquired
       mutex.acquire();
-      {
-        resourceValue++; //access the resource
-        resourceAccessNum[m_taskId]++; // increment the number of access
+        {
+          resourceValue++; //access the resource
+          resourceAccessNum[m_taskId]++; // increment the number of access
 
-        os.sched.timerTicks.sleep(rand()%200);
+          // simulate some activity done by the task
+          os.sched.timerTicks.sleep(rand() % m_maxActivityTicks);
 
-        // check if the task is the current owner of the mutex
-        if (this != mutex.getOwnerTask())
-          resourceAccessNum[m_taskId] = APP_CONFIG_ERROR_CODE;
-      }
+          // check if the task is the current owner of the mutex
+          if (this != mutex.getOwnerTask())
+            resourceAccessNum[m_taskId] = APP_CONFIG_ERROR_CODE;
+        }
       mutex.release();
     }
 }
@@ -85,10 +84,18 @@ TaskStress::taskMain(void)
 unsigned int
 TaskStress::rand(void)
 {
-  ms_rand = ms_rand * 214013L + 2531011L;
+  unsigned int ret;
+
+  os.sched.lock();
+    {
+      ms_rand = ms_rand * 214013L + 2531011L;
 #if (__SIZEOF_INT__ == 2)
-  return (ms_rand & 0x7fff);
+      ret = (ms_rand & 0x7fff);
 #else
-  return ((ms_rand >> 16) & 0x7fff);
+      ret = ((ms_rand >> 16) & 0x7fff);
 #endif
+    }
+  os.sched.unlock();
+
+  return ret;
 }
