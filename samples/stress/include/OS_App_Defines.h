d149f0fc6f42df931641208982b64cceb5e2074a
Liviu Ionescu <ilg@livius.net>
2011-03-14 22:23:18 +0000

    cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1921 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/stress/include/OS_App_Defines.h
+++ samples/stress/include/OS_App_Defines.h
@@ -8,17 +8,15 @@
 #define OS_APP_DEFINES_H_
 
 // override the board definitions, if needed
-#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
+//#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
 
 #define OS_CFGINT_TICK_RATE_HZ                          (1000)
-#define OS_CFGINT_TIMER_PRESCALLER                      (4)
-#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
+//#define OS_CFGINT_TIMER_PRESCALLER                      (4)
+//#define OS_CFGINT_TIMER_CLOCK_SELECT                    (3)
 
-#define OS_CFGINT_TASKS_TABLE_SIZE                      (12)
+#define OS_CFGINT_TASKS_TABLE_SIZE                      (13)
 
-//#define OS_INCLUDE_TIMERSECONDS                         (1)
-//#define OS_INCLUDE_OSTIMERSECONDS                       (1)
-//#define OS_INCLUDE_OSSCHEDULER_TIMERSECONDS             (1)
+#define OS_INCLUDE_OSTIMERSECONDS                       (1)
 
 //#define OS_EXCLUDE_PREEMPTION                           (1)
 //#define OS_EXCLUDE_OSTIMERTICKS_NAKED_ISR               (1)
@@ -75,6 +73,6 @@
 //#define OS_DEBUG_OSSCHEDULER_DUMPSTACK                  (1)
 //#define OS_DEBUG_CONSTRUCTORS_INIT                      (1)
 
-#endif /*DEBUG*/
+#endif /* defined(DEBUG) */
 
-#endif /*OS_APP_DEFINES_H_ */
+#endif /* OS_APP_DEFINES_H_ */
