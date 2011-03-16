12d774542e919c3a11c853cc4ab2a81821cc1b2f
Liviu Ionescu <ilg@livius.net>
2011-03-16 13:28:39 +0000

    versioning and greeting added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@1942 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/realTimeInterrupts/include/App_Defines.h
+++ samples/realTimeInterrupts/include/App_Defines.h
@@ -7,8 +7,20 @@
 #ifndef APP_DEFINES_H_
 #define APP_DEFINES_H_
 
+// ----------------------------------------------------------------------------
 
-extern void delayWithYield(schedTicks_t n);
+// Application versioning and greeting definitions
+// More definitions will be computer in uOS.h
+
+#define APP_CFGSTR_APPLICATION_NAME                     "realTimeInterrupts"
+#define APP_CFGSTR_APPLICATION_MANUFACTURER             "AVI"
+
+// Notice: do not use parenthesis! (the values will be stringified)
+#define APP_CFGINT_VERSION_MAJOR                        1
+#define APP_CFGINT_VERSION_MINOR                        2
+#define APP_CFGINT_VERSION_REVISION                     1622
+
+// ----------------------------------------------------------------------------
 
 #if defined(OS_CONFIG_BOARD_OLIMEX_STM32_H103)
 
@@ -82,5 +94,6 @@ extern void delayWithYield(schedTicks_t n);
 
 //#define APP_EXCLUDE_TASKBLINKREALTIME_TASKMAIN_LED      (1)
 
+// ----------------------------------------------------------------------------
 
 #endif /* APP_DEFINES_H_ */
