29425f25fda09b1c269c8d0d70c2a1acac3b6e46
Liviu Ionescu <ilg@livius.net>
2011-03-22 20:43:10 +0000

    indented; cosmetics
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2015 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/character/src/DeviceCharacterUsb.cpp
+++ system/portable/devices/character/src/DeviceCharacterUsb.cpp
@@ -12,57 +12,56 @@
 
 #include "portable/devices/character/include/DeviceCharacterUsb.h"
 
-// ----- constructors --------------------------------------------------------
+// ----- Constructors ---------------------------------------------------------
 
 DeviceCharacterUsb::DeviceCharacterUsb(int device)
-  {
+{
 #if defined(DEBUG) && defined(OS_DEBUG_CONSTRUCTORS)
-    OSDeviceDebug::putString("DeviceCharacterUsb(");
-    OSDeviceDebug::putDec((unsigned short)device);
-    OSDeviceDebug::putString(")=");
-    OSDeviceDebug::putPtr(this );
-    OSDeviceDebug::putNewLine();
+  OSDeviceDebug::putString("DeviceCharacterUsb(");
+  OSDeviceDebug::putDec((unsigned short) device);
+  OSDeviceDebug::putString(")=");
+  OSDeviceDebug::putPtr(this);
+  OSDeviceDebug::putNewLine();
 #endif
 
-    m_device = device;
-    if (m_device == 0)
-      {
-        m_tx_ep = TX_EP;
-        m_rx_ep = RX_EP;
-        m_int_ep = INT_EP;
-        g_usb0 = this;
-      }
-    else
-      {
+  m_device = device;
+  if (m_device == 0)
+    {
+      m_tx_ep = TX_EP;
+      m_rx_ep = RX_EP;
+      m_int_ep = INT_EP;
+      g_usb0 = this;
+    }
+  else
+    {
 #if defined(OS_INCLUDE_USB_CDC_DUAL_INTERFACE)
-        m_tx_ep = TXb_EP;
-        m_rx_ep = RXb_EP;
-        m_int_ep = INTb_EP;
-        g_usb1 = this;
-#endif
-      }
+      m_tx_ep = TXb_EP;
+      m_rx_ep = RXb_EP;
+      m_int_ep = INTb_EP;
+      g_usb1 = this;
+#endif /* defined(OS_INCLUDE_USB_CDC_DUAL_INTERFACE) */
+    }
 
 #if defined(OS_INCLUDE_DEVICECHARACTER_TYPE)
 
-    m_type = OS_DEVICECHARACTER_USB_CDC;
-#endif
-  }
+  m_type = OS_DEVICECHARACTER_USB_CDC;
+#endif /* defined(OS_INCLUDE_DEVICECHARACTER_TYPE) */
+}
 
 DeviceCharacterUsb::~DeviceCharacterUsb()
-  {
-    ;
-  }
+{
+  ;
+}
 
-#if true
-void DeviceCharacterUsb::setOpened(bool flag)
-  {
-    m_opened = flag;
-  }
-#endif
+void
+DeviceCharacterUsb::setOpened(bool flag)
+{
+  m_opened = flag;
+}
 
 DeviceCharacterUsb *g_usb0;
 #if defined(OS_INCLUDE_USB_CDC_DUAL_INTERFACE)
 DeviceCharacterUsb *g_usb1;
-#endif
+#endif /* defined(OS_INCLUDE_USB_CDC_DUAL_INTERFACE) */
 
 #endif /* OS_INCLUDE_DEVICECHARACTERUSB */
