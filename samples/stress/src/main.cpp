3a426eea0329b47e24d15eecc284850cfc959bbd
Liviu Ionescu <ilg@livius.net>
2011-04-09 18:50:03 +0000

    APP_CFGINT_SIMULTANEOUS_TASKS used to configure the number of simultaneous tasks
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2053 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/stress/src/main.cpp
+++ samples/stress/src/main.cpp
@@ -20,28 +20,52 @@
 
 TaskBlink taskA("A", APP_CONFIG_LED1, APP_CFGINT_BLINK_TICKS);
 
+#define APP_CFGINT_SIMULTANEOUS_TASKS 9
+
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 0
 TaskStress task0("0", 0, 50, 1, 150);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 1
 TaskStress task1("1", 0, 50, 1, 150);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 2
 TaskStress task2("2", 0, 50, 1, 150);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 3
 TaskStress task3("3", 30, 70, 1, 200);
-#if true
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 4
 TaskStress task4("4", 500, 2500, 1, 200);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 5
 TaskStress task5("5", 30, 70, 1, 100);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 6
 TaskStress task6("6", 10, 90, 1, 200);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 7
 TaskStress task7("7", 10, 90, 1, 400);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 8
 TaskStress task8("8", 10, 90, 1, 600);
+#if APP_CFGINT_SIMULTANEOUS_TASKS > 9
 TaskStress task9("9", 10, 90, 1, 800);
 #endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif /* APP_CFGINT_SIMULTANEOUS_TASKS > 0 */
+
 
 #if defined(APP_INCLUDE_TASKBLINKREALTIME)
 TaskBlinkRealTime taskRT("RT", APP_CFGINT_TASKBLINKREALTIME_LEDBIT,
     APP_CFGINT_TASKBLINKREALTIME_TICKS);
+
 bool g_flagNotify;
 #endif
+
 
 TaskReportStacks taskR("R", APP_CFGINT_DUMP_INTERVAL_SECONDS,
     APP_CFGINT_DUMP_MAX_INTERVAL_SECONDS,
     APP_CFGINT_DUMP_INCREASE_RATE_PROCENTS);
+
 
 // ---------------------------------------------------------------------------
 
