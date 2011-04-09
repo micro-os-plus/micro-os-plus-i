0938a5bc6343f336d2fe019b989ff374dd05dded
Liviu Ionescu <ilg@livius.net>
2011-04-09 18:42:08 +0000

    configurable critical section with nesting counter instead of stack; global context switching variables as static members; indented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2050 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/src/OSScheduler.cpp
+++ system/portable/kernel/src/OSScheduler.cpp
@@ -10,25 +10,24 @@
 
 #include "portable/kernel/include/OS.h"
 
-// ----- Static Data ----------------------------------------------------------
-
-// Global pointer, used in contextSave/Restore.
-// It points to the address of the m_pStack field of the current running task.
-// This approach is used in order to make code faster.
-OSStack_t** g_ppCurrentStack;
-
 // ----- OSSchedulerLock static variables -------------------------------------
 
-#if !defined(OS_EXCLUDE_STACK_USAGE)
-bool volatile OSSchedulerLock::ms_isLocked;
-#else /* defined(OS_EXCLUDE_STACK_USAGE) */
-unsigned char volatile OSSchedulerLock::ms_lockDepth;
-#endif /* !defined(OS_EXCLUDE_STACK_USAGE) */
+unsigned char volatile OSSchedulerLock::ms_nestingLevel;
+
+// ----- OSCriticalSection static variables -----------------------------------
+
+// Used in contextSave/Restore.
+OSStack_t volatile OSCriticalSection::ms_nestingLevel;
 
 // ----- OSScheduler static variables -----------------------------------------
 
+// Used in contextSave/Restore.
+// It points to the address of the m_pStack field of the current running task.
+// This approach is used in order to make code faster.
+volatile OSStack_t** volatile OSScheduler::ms_ppCurrentStack;
+
 OSTask* OSScheduler::ms_pTaskIdle;
-OSTask* OSScheduler::ms_pTaskRunning;
+OSTask* volatile OSScheduler::ms_pTaskRunning;
 
 bool OSScheduler::ms_isRunning;
 bool OSScheduler::ms_isPreemptive;
@@ -87,6 +86,45 @@ OSCriticalSection::OSCriticalSection()
 }
 #endif
 
+#if defined(OS_EXCLUDE_OSCRITICALSECTION_USE_STACK)
+
+// When we cannot use the stack, we no longer need to inline, so here are
+// the usual routines to enter()/exit() critical sections.
+void
+OSCriticalSection::enter(void)
+{
+#if defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS)
+  register OSStack_t tmp;
+
+  tmp = OSCPUImpl::getInterruptsMask();
+  tmp |= (OS_CFGINT_OSCRITICALSECTION_MASK);
+  OSCPUImpl::setInterruptsMask(tmp);
+#else /* !defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS) */
+  OSCPUImpl::interruptsDisable();
+#endif /* defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS) */
+
+  ++ms_nestingLevel;
+}
+
+void
+OSCriticalSection::exit(void)
+{
+  if ((ms_nestingLevel > 0) && (--ms_nestingLevel == 0))
+    {
+#if defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS)
+      register OSStack_t tmp;
+
+      tmp = OSCPUImpl::getInterruptsMask();
+      tmp &= ~(OS_CFGINT_OSCRITICALSECTION_MASK);
+      OSCPUImpl::setInterruptsMask(tmp);
+#else /* !defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS) */
+      OSCPUImpl::interruptsEnable();
+#endif /* defined(OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS) */
+    }
+}
+
+#endif /* defined(OS_EXCLUDE_OSCRITICALSECTION_USE_STACK) */
+
 // ============================================================================
 
 // Runs before all constructors
@@ -109,6 +147,8 @@ OSScheduler::earlyInit(void)
 #if defined(OS_INCLUDE_OSTASK_SLEEP)
   ms_allowDeepSleep = true;
 #endif
+
+  OSCriticalSection::ms_nestingLevel = 0;
 
   // pIdleTask = 0;      // will be set by IdleTask constructor
 
@@ -136,7 +176,8 @@ void
 OSScheduler::start(void)
 {
   // Insert all tasks in the ready list; equal priority tasks
-  // will have the same order as they were defined
+  // will have the same order as they were defined.
+  // Interrupts should be disabled here, no need for critical sections.
   int i;
   for (i = 0; i < ms_tasksCount; ++i)
     {
@@ -165,7 +206,8 @@ OSScheduler::start(void)
       // Get the task with the highest priority.
       ms_pTaskRunning = OSActiveTasks::getTop();
       // Prepare the global value with the pointer to the m_pStack.
-      g_ppCurrentStack = &ms_pTaskRunning->m_pStack;
+      OSScheduler::ms_ppCurrentStack
+          = (volatile OSStack_t**) &ms_pTaskRunning->m_pStack;
 
 #if defined(DEBUG) && defined(OS_DEBUG_OSSCHEDULER_START)
         {
@@ -252,14 +294,14 @@ OSScheduler::eventNotify(OSEvent_t event, OSEventWaitReturn_t ret)
 
 #if defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY)
 
-// Instead of always starting the notification from the first registered task,
-// the round-robin notification mechanism remembers the first notified task
-// and next time starts from the next one.
+  // Instead of always starting the notification from the first registered task,
+  // the round-robin notification mechanism remembers the first notified task
+  // and next time starts from the next one.
 
   int notifyIndex;
   int firstNotified = -1;
   notifyIndex = ms_notifyIndex;
-  
+
 #endif /* defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY) */
 
   for (i = 0; i < ms_tasksCount; ++i)
@@ -278,7 +320,7 @@ OSScheduler::eventNotify(OSEvent_t event, OSEventWaitReturn_t ret)
 #if defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY)
           // Remember the first notified task index
           if (firstNotified == -1 && r != 0)
-            firstNotified = notifyIndex;
+          firstNotified = notifyIndex;
 #endif /* defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY) */
 
           cnt += r;
@@ -287,7 +329,7 @@ OSScheduler::eventNotify(OSEvent_t event, OSEventWaitReturn_t ret)
 #if defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY)
       notifyIndex++;
       if (notifyIndex >= ms_tasksCount)
-        notifyIndex = 0;
+      notifyIndex = 0;
 #endif /* defined(OS_INCLUDE_OSSCHEDULER_ROUND_ROBIN_NOTIFY) */
     }
 
@@ -295,7 +337,7 @@ OSScheduler::eventNotify(OSEvent_t event, OSEventWaitReturn_t ret)
   OSCriticalSection::enter();
     {
       if (firstNotified > 0)
-        ms_notifyIndex = firstNotified;
+      ms_notifyIndex = firstNotified;
 
       ms_notifyIndex++;
       if (ms_notifyIndex >= ms_tasksCount)
@@ -329,7 +371,7 @@ OSScheduler::isContextSwitchRequired()
 
 // Perform the context switch operations.
 // At exit the static variable ms_pTaskRunning points to the new
-// running task and the global variable g_ppCurrentStack to the
+// running task and the global variable OSScheduler::ms_ppCurrentStack to the
 // tasks context to be restored.
 
 void
@@ -362,8 +404,8 @@ OSScheduler::performContextSwitch()
 
           // Select the running task from the top of the list
           ms_pTaskRunning = OSActiveTasks::getTop();
-          // Prepare the global value with the pointer to the m_pStack.
-          g_ppCurrentStack = &ms_pTaskRunning->m_pStack;
+          // Prepare the global variable with the pointer to the m_pStack.
+          ms_ppCurrentStack = (volatile OSStack_t**) &ms_pTaskRunning->m_pStack;
         }
       OSCriticalSection::exit();
     }
@@ -440,7 +482,7 @@ OSScheduler::taskRegister(OSTask *pTask)
   return id;
 }
 
-// Called from timer interrupt
+// Called from the timer interrupt
 // Warning: interrupts are enabled!
 
 void
@@ -509,37 +551,43 @@ OSActiveTasks::OSActiveTasks()
   ms_count = 0;
 }
 
-// Insert tasks in order of decreasing priority
+// Insert tasks in order of decreasing priority.
+// Must be called in a critical section.
 void
 OSActiveTasks::insert(OSTask *pTask)
 {
   int i;
+
+  // Check if already in
   i = find(pTask);
   if (i != -1)
     {
-      return; // already in
+      return; // already in, we're done
     }
 
   unsigned int prio;
   prio = pTask->m_staticPriority;
   for (i = 0; i < ms_count; ++i)
     {
-      // for identical priority, insert at the end
-      if (prio > ms_array[i] ->m_staticPriority)
+      // If tasks with identical priority exist, insert at the end
+      // in other words, insert before task with higher priority.
+      if (prio > ms_array[i]->m_staticPriority)
         break;
     }
 
   if (i < ms_count)
-  // i is the place where to insert
     {
+      // i is the place where to insert
+
       int j;
-      // shift right to make space
+      // shift array right to make space
       for (j = ms_count; j > i; j--)
         {
           ms_array[j] = ms_array[j - 1];
         }
     }
-  // i might also be == ms_count
+
+  // but i might also be == ms_count, i.e. we insert at the end
 
   ms_array[i] = pTask;
   ms_count++;
@@ -553,6 +601,7 @@ OSActiveTasks::insert(OSTask *pTask)
 #endif
 }
 
+// Must be called in a critical section.
 void
 OSActiveTasks::remove(OSTask * pTask)
 {
@@ -560,13 +609,15 @@ OSActiveTasks::remove(OSTask * pTask)
     return; // do not remove the idle task
 
   int i;
+
+  // Try to find the task in the active list
   i = find(pTask);
   if (i == -1)
     {
       return; // task not found, nothing to remove
     }
 
-  // move the list one step to the left
+  // Remove the task by copying the list one step to the left
   for (; i < ms_count - 1; ++i)
     {
       ms_array[i] = ms_array[i + 1];
