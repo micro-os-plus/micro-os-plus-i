ee72b883d5acd35e579174a35c856c18d02ba714
Liviu Ionescu <ilg@livius.net>
2011-04-19 12:24:46 +0000

    under construction code
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2107 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/usb/include/OSUsbDeviceDescriptorsConfig.h
+++ system/portable/devices/usb/include/OSUsbDeviceDescriptorsConfig.h
@@ -15,4 +15,30 @@
 
 // ----------------------------------------------------------------------------
 
+#if false
+
+#if defined(OS_CONFIG_ARCH_AVR8)
+
+#if defined(OS_CONFIG_FAMILY_AT90USB)
+#include "hal/arch/avr8/at90usb/devices/usb/include/OSUsbDeviceDescriptorsConfig.h"
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+#elif defined(OS_CONFIG_ARCH_AVR32)
+
+#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+#include "hal/arch/avr32/uc3/devices/usb/include/OSUsbDeviceDescriptorsConfig.h"
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+#else
+#error "Missing OS_CONFIG_ARCH_* definition"
+#endif
+
+#endif
+
+// ----------------------------------------------------------------------------
+
 #endif /* OSUSBDEVICEDESCRIPTORSCONFIG_H_ */
