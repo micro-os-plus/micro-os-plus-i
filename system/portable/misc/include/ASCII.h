c38c574d6209f6a41e2cdde608acee46f6050c18
Liviu Ionescu <ilg@livius.net>
2011-04-21 22:46:58 +0000

    BELL added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2119 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/misc/include/ASCII.h
+++ system/portable/misc/include/ASCII.h
@@ -14,6 +14,8 @@ public:
   static const unsigned char STX = 0x02;
   static const unsigned char ETX = 0x03;
 
+  static const unsigned char BELL = 0x07;
+
   static const unsigned char LF = 0x0A;
   static const unsigned char CR = 0x0D;
 
