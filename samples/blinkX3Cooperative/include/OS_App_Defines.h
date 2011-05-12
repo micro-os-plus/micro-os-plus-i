c5796bbe72c9847072dbb2b834af7628e4fd84ca
Liviu Ionescu <ilg@livius.net>
2011-05-12 20:58:01 +0000

    task renamed as thread
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2152 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3Cooperative/include/OS_App_Defines.h
+++ samples/blinkX3Cooperative/include/OS_App_Defines.h
@@ -7,7 +7,7 @@
 #ifndef OS_APP_DEFINES_H_
 #define OS_APP_DEFINES_H_
 
-// ----------------------------------------------------------------------------
+// ----- Portable Definitions -------------------------------------------------
 
 // override the board definitions, if needed
 //#define OS_CFGLONG_OSCILLATOR_HZ                        (16000000UL)
@@ -26,7 +26,17 @@
 // After this works properly, proceed with naked interrupts.
 //#define OS_EXCLUDE_OSTIMERTICKS_NAKED_ISR               (1)
 
-// ----------------------------------------------------------------------------
+// ----- Board Definitions ----------------------------------------------------
+
+#if defined(OS_CONFIG_BOARD_ATMEL_EVK1104)
+
+//#define OS_EXCLUDE_STACK_USAGE                          (1)
+
+#elif defined(OS_CONFIG_BOARD_ATMEL_STK525)
+
+#endif /* OS_CONFIG_BOARD_* */
+
+// ----- Debug Definitions ----------------------------------------------------
 
 #if defined(DEBUG)
 
