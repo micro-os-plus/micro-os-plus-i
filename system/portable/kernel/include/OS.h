bd20142b1d61f4d41bbb124ab338694c12296cd3
Liviu Ionescu <ilg@livius.net>
2011-05-17 14:39:09 +0000

    OSReturn definitions moved to separate file
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2172 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/include/OS.h
+++ system/portable/kernel/include/OS.h
@@ -9,33 +9,7 @@
 
 #include "portable/kernel/include/OS_Defines.h"
 
-// ----------------------------------------------------------------------------
-
-typedef int OSReturn_t;
-
-class OSReturn
-{
-public:
-  static const OSReturn_t OS_OK = 0;
-  static const OSReturn_t OS_TIMEOUT = -1;
-  static const OSReturn_t OS_DISCONNECTED = -2;
-  static const OSReturn_t OS_WOULD_BLOCK = -3;
-  static const OSReturn_t OS_NOT_OWNER = -4;
-  static const OSReturn_t OS_NOT_ACQUIRED = -5;
-  static const OSReturn_t OS_ALREADY_OPENED = -6;
-  static const OSReturn_t OS_NOT_OPENED = -7;
-  static const OSReturn_t OS_TOO_LONG = -8;
-  static const OSReturn_t OS_ILLEGAL_CHARACTER = -9;
-  static const OSReturn_t OS_BAD_CHECKSUM = -10;
-  static const OSReturn_t OS_NOT_INITIALIZED = -11;
-  static const OSReturn_t OS_BAD_PARAMETER = -12;
-  static const OSReturn_t OS_BUFFER_FULL = -13;
-  static const OSReturn_t OS_IMMEDIATELY = -14;
-  static const OSReturn_t OS_ITEM_NOT_FOUND = -15;
-  static const OSReturn_t OS_NOT_ENOUGH_SPACE = -16;
-  static const OSReturn_t OS_BAD_COMMAND = -17;
-
-};
+#include "portable/kernel/include/OSReturn.h"
 
 // ----------------------------------------------------------------------------
 
