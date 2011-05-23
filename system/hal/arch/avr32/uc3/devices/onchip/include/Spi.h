f0617b75526ae96e562cff07631cde8dd1f18e23
Liviu Ionescu <ilg@livius.net>
2011-05-23 08:03:02 +0000

    preliminary SPI, in avr32::uc3 namespace
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2195 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/hal/arch/avr32/uc3/devices/onchip/include/Spi.h
@@ -0,0 +1,31 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef AVR32_UC3_SPI_H_
+#define AVR32_UC3_SPI_H_
+
+namespace avr32
+{
+  namespace uc3
+  {
+
+    class Spi
+    {
+    public:
+      enum ModuleId_t
+      {
+        zero = 0, one = 1
+      };
+
+      Spi(ModuleId_t id);
+
+    private:
+      void* m_peripheralAddress;
+    };
+  }
+}
+
+#endif /* AVR32_UC3_SPI_H_ */
---
+++ system/hal/arch/avr32/uc3/devices/onchip/src/Spi.cpp
@@ -0,0 +1,36 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+
+#if defined(OS_INCLUDE_AVR32_UC3_SPI)
+
+#include "hal/arch/avr32/uc3/devices/onchip/include/Spi.h"
+
+namespace avr32
+{
+  namespace uc3
+  {
+
+    Spi::Spi(ModuleId_t id)
+    {
+      if (id == zero)
+        {
+          m_peripheralAddress = (void*) 0xFFFF2400;
+        }
+      else if (id == one)
+        {
+          m_peripheralAddress = (void*) 0xFFFF2800;
+        }
+    }
+  }
+}
+
+#endif /* defined(OS_INCLUDE_AVR32_UC3_SPI) */
+
+#endif /* defined(OS_CONFIG_FAMILY_AVR32UC3) */
