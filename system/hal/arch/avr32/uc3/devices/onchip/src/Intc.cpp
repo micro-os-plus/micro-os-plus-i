6b106db3920488a96e8bbb94f2e20da2dc03029a
Liviu Ionescu <ilg@livius.net>
2011-05-26 19:07:35 +0000

    registerInterruptHandler() implemented with a framework call
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2227 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/hal/arch/avr32/uc3/devices/onchip/src/Intc.cpp
@@ -0,0 +1,38 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+
+#if defined(OS_INCLUDE_AVR32_UC3_INTC)
+
+#include "hal/arch/avr32/uc3/devices/onchip/include/Intc.h"
+#include "hal/arch/avr32/uc3/lib/include/intc.h"
+
+namespace avr32
+{
+  namespace uc3
+  {
+    Intc::Intc()
+    {
+      OSDeviceDebug::putConstructor("avr32::uc3::Intc", this);
+    }
+
+    void
+    Intc::registerInterruptHandler(intc::InterruptHandler_t handler,
+        intc::InterruptIndex_t index, intc::Priority_t priority)
+    {
+      // TODO: implement locally
+      // Until then, call the framework
+      INTC_register_interrupt(handler, index, priority);
+    }
+  }
+}
+
+#endif /* defined(OS_INCLUDE_AVR32_UC3_INTC) */
+
+#endif /* defined(OS_CONFIG_FAMILY_AVR32UC3) */
