c5477ce1836a22a77e566b9de6250f0f1bc1b868
Liviu Ionescu <ilg@livius.net>
2011-05-29 22:07:29 +0000

    this will be the new MCI hardware driver
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2259 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/hal/arch/avr32/uc3/devices/onchip/include/Mci.h
@@ -0,0 +1,30 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef AVR32_UC3_MCI_H_
+#define AVR32_UC3_MCI_H_
+
+#include "portable/kernel/include/OS.h"
+
+#include "hal/arch/avr32/uc3/devices/onchip/include/Mci.h"
+
+namespace avr32
+{
+  namespace uc3
+  {
+    namespace mci
+    {
+      // TODO: add definitions
+    }
+
+    class Mci
+    {
+    public:
+      Mci();
+    };
+  }
+}
+#endif /* AVR32_UC3_MCI_H_ */
