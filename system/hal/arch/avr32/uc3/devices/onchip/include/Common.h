dc72d40cee6d6ac59f3a8652446cd8aa2f71c1ab
Liviu Ionescu <ilg@livius.net>
2011-06-01 18:27:02 +0000

    PeripheralAddressMap added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2293 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/onchip/include/Common.h
+++ system/hal/arch/avr32/uc3/devices/onchip/include/Common.h
@@ -17,6 +17,31 @@ namespace avr32
     typedef uint32_t const volatile regReadOnly_t;
     typedef uint32_t regWriteOnly_t;
     typedef uint32_t regNotAllocated_t;
+
+    class PeripheralAddressMap
+    {
+    public:
+      static const uint32_t DMACA = 0xFF100000;
+      static const uint32_t USB = 0xFFFE0000;
+      static const uint32_t MCI = 0xFFFE4000;
+      static const uint32_t PDCA = 0xFFFF0000;
+      static const uint32_t INTC = 0xFFFF0800;
+      static const uint32_t PM = 0xFFFF0C00;
+      static const uint32_t RTC = 0xFFFF0D00;
+      static const uint32_t WDT = 0xFFFF0D30;
+      static const uint32_t GPIO = 0xFFFF1000;
+      static const uint32_t USART0 = 0xFFFF1400;
+      static const uint32_t USART1 = 0xFFFF1800;
+      static const uint32_t USART2 = 0xFFFF1C00;
+      static const uint32_t USART3 = 0xFFFF2000;
+      static const uint32_t SPI0 = 0xFFFF2400;
+      static const uint32_t SPI1 = 0xFFFF2800;
+      static const uint32_t TWIM0 = 0xFFFF2C00;
+      static const uint32_t TWIM1 = 0xFFFF3000;
+      static const uint32_t TC0 = 0xFFFF3800;
+      static const uint32_t ADC = 0xFFFF3C00;
+      static const uint32_t TC1 = 0xFFFF4400;
+    };
   }
 }
 
