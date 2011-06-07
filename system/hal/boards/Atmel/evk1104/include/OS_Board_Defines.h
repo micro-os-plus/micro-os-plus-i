7f3fc16096f05118b7066cfda84a09e953cefe4b
Bogdan Brinza <brinzab@yahoo.com>
2011-06-07 15:03:52 +0000

    GPIO implementation.
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2297 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/boards/Atmel/evk1104/include/OS_Board_Defines.h
+++ system/hal/boards/Atmel/evk1104/include/OS_Board_Defines.h
@@ -9,6 +9,7 @@
 
 #define OS_CONFIG_ARCH_AVR32					1
 #define OS_CONFIG_FAMILY_AVR32UC3				1
+#define OS_INCLUDE_AVR32_UC3_GPIO                               1
 #define OS_CONFIG_VARIANT_AVR32UC3A3				1
 #if !defined(OS_CONFIG_BOARD_ATMEL_EVK1104)
 #define OS_CONFIG_BOARD_ATMEL_EVK1104				1
