738acac5e777b0b31f32782eec2cdce7fb393cf9
Liviu Ionescu <ilg@livius.net>
2011-06-10 11:10:27 +0000

    code reorganised with separate parent implementation class; critical sections code implemented locally; types updated to use standarn types
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2325 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/devices/debug/include/DeviceDebugI2cImpl.h
@@ -0,0 +1,371 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef DEVICEDEBUGI2CIMPL_H_
+#define DEVICEDEBUGI2CIMPL_H_
+
+#include "portable/kernel/include/OS_Defines.h"
+
+class DeviceDebugI2cImpl
+{
+public:
+  static void
+  implEarlyInit(void);
+
+  // Ouput one byte on the debug interface.
+  static void
+  implPutByte(unsigned char c);
+
+  // Ouput the given buffer on the debug interface.
+  static int
+  implPutBytes(const char* s, unsigned int n);
+
+  // Return true if the debug interface can output now.
+  static bool
+  implCanTransmit(void);
+
+  // Return true if the debug interface is connected with the MCU.
+  static bool
+  implDevicePresent(void);
+
+  // Watchdog reset.
+  static void
+  implWDReset(void);
+
+private:
+
+  static const int I2C_WRITE = 0x0;
+  static const int I2C_READ = 0x1;
+
+  // Implement I2C master by bitbanging.
+  static void
+  masterInit(void);
+
+  static void
+  masterSclInit(void);
+  static void
+  masterSclHigh(void);
+  static void
+  masterSclLow(void);
+  static bool
+  masterSclIsLow(void);
+
+  static void
+  masterSdaInit(void);
+  static void
+  masterSdaHigh(void);
+  static void
+  masterSdaLow(void);
+  static bool
+  masterSdaIsLow(void);
+
+  static void
+  masterSclSynchronize(void);
+  static void
+  masterSdaSynchronize(void);
+
+  static void
+  masterStart(void);
+  static void
+  masterDelay(int n) __attribute__((noinline));
+
+  static void
+  masterPutAddress(unsigned char addr, unsigned char mode);
+  static void
+  masterPutByte(unsigned char c);
+  //static unsigned char masterGetByte(void);
+  static void
+  masterPutAck(bool b);
+  static bool
+  masterGetAck(void);
+  static void
+  masterStop(void);
+  static void
+  masterCleanup(void);
+};
+
+#if defined(OS_CONFIG_BOARD_ATMEL_STK525)
+#include "hal/boards/Atmel/stk525/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_ATMEL_EVK1104)
+#include "hal/boards/Atmel/evk1104/include/DeviceDebugI2C_Board_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_METRILOG_M512)
+#include "hal/boards/Metrilog/m512/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_METRILOG_M514)
+#include "hal/boards/Metrilog/m514/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_AVI_A0739)
+#include "hal/boards/AVI/a0739/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_AVI_A07523)
+#include "hal/boards/AVI/a07523/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_AVI_A07527)
+#include "hal/boards/AVI/a07527/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_AVI_A07524)
+#include "hal/boards/AVI/a07524/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_AVI_A07521)
+#include "hal/boards/AVI/a07521/include/DeviceDebugI2C_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_OLIMEX_STM32_H103)
+#include "hal/boards/Olimex/stm32_h103/include/DeviceDebugI2C_Board_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_TEMPLATE)
+#include "hal/boards/TEMPLATE_MANUFACTURER/TEMPLATE_BOARD/include/DeviceDebugI2C_Board_Defines.h"
+
+#elif defined(OS_CONFIG_BOARD_CUSTOM)
+#include "custom/include/DeviceDebugI2C_Board_Defines.h"
+
+#else
+#error "Missing OS_CONFIG_BOARD_* definition"
+#endif
+
+#if !defined(OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT)
+#define OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT (0)
+#endif
+
+//#if !defined(OS_EXCLUDE_OSDEVICEDEBUG_IMPLEMENTATION)
+
+// ----- Public methods -------------------------------------------------------
+
+inline bool
+DeviceDebugI2cImpl::implCanTransmit(void)
+{
+  return true;
+}
+
+inline bool
+DeviceDebugI2cImpl::implDevicePresent(void)
+{
+  return !masterSdaIsLow();
+}
+
+inline void
+DeviceDebugI2cImpl::implWDReset(void)
+{
+#if false
+  IWDG_ReloadCounter();
+#endif
+}
+
+inline void
+DeviceDebugI2cImpl::implEarlyInit(void)
+{
+#if true
+    {
+      unsigned int _i, _j;
+      for (_i = 0; _i < OS_CFGINT_DEBUG_I2C_EMU_INIT_DELAY_COUNT; ++_i)
+        for (_j = 0; _j < OS_CFGINT_BUSYWAIT_CALIBRATION; ++_j)
+          implWDReset();
+    }
+#endif
+
+  masterInit();
+
+  masterSclInit();
+  masterSdaInit();
+}
+
+inline void
+DeviceDebugI2cImpl::implPutByte(unsigned char c)
+{
+  for (;;) // loop until we succeed to transmit
+
+    {
+      masterStart();
+      masterPutAddress(OS_CFGINT8_DEBUG_I2C_DEBUGGER_ADDR, I2C_WRITE);
+      if (!masterGetAck())
+        {
+          masterCleanup();
+          goto retry;
+        }
+
+      masterPutByte(c);
+      if (!masterGetAck())
+        {
+          masterCleanup();
+          goto retry;
+        }
+
+      masterStop();
+      break;
+
+      retry: masterDelay(OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT * 10);
+    }
+}
+
+inline int
+DeviceDebugI2cImpl::implPutBytes(const char* s, unsigned int n)
+{
+  unsigned int i;
+#if defined(OS_INCLUDE_DEVICEDEBUGI2C_SINGLE_BYTE)
+  for (i = 0; i < n; ++i)
+  implPutByte(s[i]);
+#else
+  for (i = 0; i < n;)
+    { // loop until we succeed to transmit
+      masterStart();
+      masterPutAddress(OS_CFGINT8_DEBUG_I2C_DEBUGGER_ADDR, I2C_WRITE);
+      if (!masterGetAck())
+        {
+          masterCleanup();
+          goto retry;
+        }
+
+      for (; i < n; ++i)
+        {
+          masterPutByte(s[i]);
+          if (!masterGetAck())
+            {
+              masterCleanup();
+              goto retry;
+            }
+        }
+      masterStop();
+      break;
+
+      retry: masterDelay(OS_CFGINT_DEBUG_I2C_EMU_DELAY_COUNT * 10);
+    }
+#endif
+  return n;
+}
+
+// ---------------------------------------------------------------------------
+// Implementation of the bitbanged i2c master.
+
+inline void
+DeviceDebugI2cImpl::masterInit(void)
+{
+  ;
+}
+
+inline void
+DeviceDebugI2cImpl::masterSclInit(void)
+{
+  // Set the pin value to 0; when the pin direction will be configured
+  // to output, this will set the level to low.
+  OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_SCL_PORT_WRITE, OS_CONFIG_DEBUG_SCL_BIT);
+
+  // Set the pin direction to input, i.e. allow the
+  // pull-ups to raise the level to high.
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSclHigh(void)
+{
+  // The pull-up will do the job.
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSclLow(void)
+{
+  // Configure direction to output, the value was already set to 0.
+  OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SCL_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline bool
+DeviceDebugI2cImpl::masterSclIsLow(void)
+{
+  return OS_GPIO_PIN_ISLOW(OS_CONFIG_DEBUG_SCL_PORT_READ,
+      OS_CONFIG_DEBUG_SCL_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSdaInit(void)
+{
+  // Set the pin value to 0; when the pin direction will be configured
+  // to output, this will set the level to low.
+  OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_SDA_PORT_WRITE, OS_CONFIG_DEBUG_SDA_BIT);
+
+  // Set the pin direction to input, i.e. allow the
+  // pull-ups to raise the level to high.
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSdaHigh(void)
+{
+  // The pull-up will do the job.
+  OS_GPIO_PIN_CONFIG_INPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSdaLow(void)
+{
+  // Configure direction to output, the value was already set to 0.
+  OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_SDA_PORT_CONFIG,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline bool
+DeviceDebugI2cImpl::masterSdaIsLow(void)
+{
+  return OS_GPIO_PIN_ISLOW(OS_CONFIG_DEBUG_SDA_PORT_READ,
+      OS_CONFIG_DEBUG_SDA_BIT);
+}
+
+inline void
+DeviceDebugI2cImpl::masterSclSynchronize(void)
+{
+  while (masterSclIsLow())
+    {
+      implWDReset();
+    }
+}
+
+inline void
+DeviceDebugI2cImpl::masterSdaSynchronize(void)
+{
+  while (masterSdaIsLow())
+    {
+      implWDReset();
+    }
+}
+
+//#endif /* OS_EXCLUDE_OSDEVICEDEBUG_IMPLEMENTATION */
+
+#if false
+#if defined(OS_CONFIG_ARCH_AVR8)
+#include "hal/arch/avr8/devices/debug/include/DeviceDebugI2CEmuMaster_Inlines.h"
+
+#elif defined(OS_CONFIG_ARCH_ARM_CORTEX_M3)
+#if defined(OS_CONFIG_FAMILY_STM32F10X)
+#include "hal/arch/arm_cortex_m3/stm32f10x/devices/debug/include/DeviceDebugI2CEmuMaster_Family_Inlines.h"
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+#elif defined(OS_CONFIG_ARCH_AVR32)
+//#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+//#include "hal/arch/avr32/uc3/devices/debug/include/DeviceDebugI2CEmuMaster_Family_Inlines.h"
+//#else
+//#error "Missing OS_CONFIG_FAMILY_* definition"
+//#endif
+
+#elif defined(OS_CONFIG_ARCH_TEMPLATE)
+#if defined(OS_CONFIG_FAMILY_TEMPLATE)
+#include "hal/arch/TEMPLATE_ARCH/TEMPLATE_FAMILY/devices/debug/include/DeviceDebugI2CEmuMaster_Family_Inlines.h"
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+#else
+#error "Missing OS_CONFIG_ARCH_* definition"
+#endif
+#endif /* false */
+
+#endif /* DEVICEDEBUGI2CIMPL_H_ */
--- system/portable/devices/debug/include/OSDeviceDebug.h
+++ system/portable/devices/debug/include/OSDeviceDebug.h
@@ -7,16 +7,33 @@
 #ifndef OSDEVICEDEBUG_H_
 #define OSDEVICEDEBUG_H_
 
-#include "portable/kernel/include/OS_Defines.h"
+#include "portable/kernel/include/OS.h"
 
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_STREAMBUF)
 #include "portable/stdlib/include/streambuf"
+#endif /* defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_STREAMBUF) */
+
+#if defined(OS_CONFIG_DEBUG_DEVICE_I2C)
+#include "portable/devices/debug/include/DeviceDebugI2cImpl.h"
+#endif /* defined(OS_CONFIG_DEBUG_DEVICE_I2C) */
+
+// ============================================================================
+
+// This class is used for displaying the trace messages.
+// The actual device output is passed to an implementation helper.
+
+class OSDeviceDebug :
+
+#if defined(OS_CONFIG_DEBUG_DEVICE_I2C)
+    public DeviceDebugI2cImpl
+#elif defined(OS_CONFIG_DEBUG_DEVICE_USART)
+  public OSDeviceDebugUsartImpl
+#else
+#error "Missing OS_CONFIG_DEBUG_DEVICE_* definition"
 #endif
 
-// The parent class of all debug classes used for displaying trace messages.
-class OSDeviceDebug
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_STREAMBUF)
-: public std::streambuf
+    , public std::streambuf
 #endif
 
 {
@@ -28,7 +45,7 @@ public:
 #endif
   // Output, on debug interface,  one character.
   static void
-  putChar(unsigned char c);
+  putChar(uchar_t c);
   // Output, on debug interface, CR+LF.
   static void
   putNewLine(void);
@@ -39,7 +56,7 @@ public:
   static void
   putConstructor(const char* pc, const void* p);
   static void
-  putConstructorWithIndex(const char* pc, unsigned short i, const void* p);
+  putConstructorWithIndex(const char* pc, uint16_t i, const void* p);
   static void
   putDestructor(const char* pc, const void* p);
 
@@ -57,31 +74,34 @@ public:
   // Output, on debug interface, the hex value of a byte;
   // i.e. 2 hexadecimal characters.
   static void
-  putHex(unsigned char c);
+  putHex(uint8_t c);
+
   // Output, on debug interface, the hex value of a 2-byte word;
   // i.e. 4 hexadecimal characters.
   static void
-  putHex(unsigned short w);
+  putHex(uint16_t w);
+
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_PUTHEX_LONG)
   // Output, on debug interface, the hex value of a long;
   // i.e. 8 hexadecimal characters.
   static void
-  putHex(unsigned long l);
+  putHex(uint32_t l);
 #endif
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_PUTHEX_INT)
   // Output, on debug interface, the hex value of an integer;
   // i.e. 8 hexadecimal characters.
   static void
-  putHex(unsigned int l);
+  putHex(uint_t l);
 #endif
   // Output, on debug interface, the decimal value of a 2-byte word,
   // <n> last digits will not be output, max 5 decimal digits.
   static void
-  putDec(unsigned short w, unsigned short n = 0);
+  putDec(uint16_t w, uint16_t n = 0);
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_PUTDEC_LONG)
   // Output, on debug interface, the decimal value of a short
   // <n> last digits will not be output, max 10 decimal digits.
-  static void putDec(unsigned long l, unsigned short n = 0);
+  static void
+  putDec(uint32_t l, uint16_t n = 0);
 #endif
   // Output, on debug interface, the address p, in hexadecimal.
   static void
@@ -95,14 +115,15 @@ public:
   static void
   __assert(const char* func, const char* file, int lineno, const char* sexp);
 
-#if defined(DEBUG)
-  // Watchdog reset; implementation specific.
-  static void
-  implWDReset(void);
-#endif
   // Initialise debug interface and print greeting and build strings.
   static void
   earlyInit();
+
+  static OSStack_t
+  criticalEnter(void);
+
+  static void
+  criticalExit(OSStack_t mask);
 
 private:
 
@@ -113,27 +134,11 @@ private:
 #endif
   // Send one byte to debug interface.
   static int
-  commonPutByte(unsigned char c);
+  commonPutByte(uchar_t c);
   // Send the given buffer to debug interface.
   static int
   commonPutBytes(const char* pc, unsigned int n);
 
-  // implementation code, defined in separated files
-  // Initialise debug interface.
-  static void
-  implEarlyInit(void);
-  // Ouput one byte on the debug interface.
-  static void
-  implPutByte(unsigned char c);
-  // Ouput the given buffer on the debug interface.
-  static int
-  implPutBytes(const char* s, unsigned int n);
-  // Return true if the debug interface can output now.
-  static bool
-  implCanTransmit(void);
-  // Return true if the debug interface is connected with the MCU.
-  static bool
-  implDevicePresent(void);
 #endif
 
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_STREAMBUF)
@@ -150,13 +155,24 @@ private:
 
 #if defined(DEBUG)
 
-#if defined(OS_CONFIG_DEBUG_DEVICE_I2C)
-#include "portable/devices/debug/include/DeviceDebugI2C_Inlines.h"
-#elif defined(OS_CONFIG_DEBUG_DEVICE_USART)
-#include "portable/devices/debug/include/DeviceDebugUsart_Inlines.h"
-#else
-#error "Missing OS_CONFIG_DEBUG_DEVICE_* definition"
-#endif
+// ----------------------------------------------------------------------------
+
+inline OSStack_t
+OSDeviceDebug::criticalEnter(void)
+{
+  register OSStack_t tmp;
+
+  tmp = OSCPUImpl::getInterruptsMask();
+  OSCPUImpl::interruptsDisable();
+
+  return tmp;
+}
+
+inline void
+OSDeviceDebug::criticalExit(OSStack_t mask)
+{
+  OSCPUImpl::setInterruptsMask(mask);
+}
 
 #if !defined(OS_DEBUG_CONSTRUCTORS)
 
@@ -166,7 +182,7 @@ OSDeviceDebug::putConstructor(const char* pc __attribute__((unused)), const void
   }
 
 inline void
-OSDeviceDebug::putConstructorWithIndex(const char* pc __attribute__((unused)), unsigned short i __attribute__((unused)), const void* p __attribute__((unused)))
+OSDeviceDebug::putConstructorWithIndex(const char* pc __attribute__((unused)), uint16_t i __attribute__((unused)), const void* p __attribute__((unused)))
   {
   }
 
@@ -182,34 +198,34 @@ OSDeviceDebug::putDestructor(const char* pc __attribute__((unused)), const void*
 // define empty functions for Release
 inline void
 OSDeviceDebug::putChar(unsigned char __attribute__((unused)) c)
-{
-}
+  {
+  }
 
 inline void
 OSDeviceDebug::putNewLine(void)
-{
-}
+  {
+  }
 
 inline void
 OSDeviceDebug::putString(const char __attribute__((unused)) * pc)
-{
-}
+  {
+  }
 
 inline void
 OSDeviceDebug::putConstructor(const char* pc __attribute__((unused)), const void* p __attribute__((unused)))
-{
-}
+  {
+  }
 
 inline void
-OSDeviceDebug::putConstructorWithIndex(const char* pc __attribute__((unused)), unsigned short i __attribute__((unused)),
-    const void* p  __attribute__((unused)))
-{
-}
+OSDeviceDebug::putConstructorWithIndex(const char* pc __attribute__((unused)), uint16_t i __attribute__((unused)),
+    const void* p __attribute__((unused)))
+  {
+  }
 
 inline void
 OSDeviceDebug::putDestructor(const char* pc __attribute__((unused)), const void* p __attribute__((unused)))
-{
-}
+  {
+  }
 
 #if defined(OS_INCLUDE_SEPARATE_PROGMEM)
 
@@ -223,7 +239,7 @@ OSDeviceDebug::putConstructor_P(const char* __attribute__((unused)) PROGMEM pc, 
   }
 
 inline void
-OSDeviceDebug::putConstructorWithIndex_P(const char* __attribute__((unused)) PROGMEM pc, unsigned short i __attribute__((unused)), const void* __attribute__((unused)) p)
+OSDeviceDebug::putConstructorWithIndex_P(const char* __attribute__((unused)) PROGMEM pc, uint16_t i __attribute__((unused)), const void* __attribute__((unused)) p)
   {
   }
 
@@ -236,19 +252,19 @@ OSDeviceDebug::putDestructor_P(const char* __attribute__((unused)) PROGMEM pc, c
 
 inline void
 OSDeviceDebug::putHex(unsigned char __attribute__((unused)) c)
-{
-}
+  {
+  }
 
 inline void
 OSDeviceDebug::putHex(unsigned short __attribute__((unused)) w)
-{
-}
+  {
+  }
 
 inline void
 OSDeviceDebug::putDec(unsigned short __attribute__((unused)) w,
     unsigned short __attribute__((unused)) n)
-{
-}
+  {
+  }
 
 #if defined(DEBUG) && defined(OS_INCLUDE_OSDEVICEDEBUG_PUTDEC_LONG)
 inline void OSDeviceDebug::putDec(unsigned long __attribute__((unused)) l, unsigned short
@@ -259,17 +275,17 @@ inline void OSDeviceDebug::putDec(unsigned long __attribute__((unused)) l, unsig
 
 inline void
 OSDeviceDebug::putPC(const char* PROGMEM __attribute__((unused)) pc)
-{
-  pc = pc;
-}
+  {
+    pc = pc;
+  }
 
 inline void
 OSDeviceDebug::__assert(const char __attribute__((unused)) *func,
     const char __attribute__((unused)) *xfile,
     int __attribute__((unused)) lineno,
     const char __attribute__((unused)) *sexp)
-{
-}
+  {
+  }
 
 #endif
 
