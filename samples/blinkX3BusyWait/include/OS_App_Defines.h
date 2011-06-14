6b5f3b08ff30e40b195abeba0436adae9987d78d
Liviu Ionescu <ilg@livius.net>
2011-06-14 17:09:21 +0000

    OS_INCLUDE_OSCRITICALSECTION_USE_THREAD_STACK added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2361 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/blinkX3BusyWait/include/OS_App_Defines.h
+++ samples/blinkX3BusyWait/include/OS_App_Defines.h
@@ -9,7 +9,7 @@
 
 // ----- Portable Definitions -------------------------------------------------
 
-#define OS_CFGINT_THREADS_ARRAY_SIZE                      (3)
+#define OS_CFGINT_THREADS_ARRAY_SIZE                    (3)
 
 #define OS_INCLUDE_OS_BUSYWAITMILLIS                    (1)
 
@@ -24,9 +24,11 @@
 
 #if defined(OS_CONFIG_BOARD_ATMEL_EVK1104)
 
-#define OS_EXCLUDE_OSCRITICALSECTION_USE_SYSTEM_STACK          (1)
+#define OS_EXCLUDE_OSCRITICALSECTION_USE_SYSTEM_STACK   (1)
+#define OS_INCLUDE_OSCRITICALSECTION_USE_THREAD_STACK   (1)
 #define OS_INCLUDE_OSCRITICALSECTION_MASK_INTERRUPTS    (1)
 //#define OS_INCLUDE_OSREALTIMECRITICALSECTION_MASK_INTERRUPTS (1)
+#define OS_INCLUDE_OSSCHEDULER_YIELD_MASK_INTERRUPTS  (1)
 
 #elif defined(OS_CONFIG_BOARD_ATMEL_STK525)
 
@@ -38,16 +40,18 @@
 
 #define OS_DEBUG_OSTIMERTICKS_ISR_MARK_SECONDS          (1)
 
-//#define OS_DEBUG_OSSCHEDULER_STACKINITIALIZE            (1)
+#define OS_DEBUG_CONSTRUCTORS_INIT                      (1)
 #define OS_DEBUG_CONSTRUCTORS                           (1)
+#if false
+#define OS_DEBUG_OSSCHEDULER_STACKINITIALIZE            (1)
 #define OS_DEBUG_OSSCHEDULER_START                      (1)
-//#define OS_DEBUG_OSSCHEDULER_CONTEXTSWITCH              (1)
-//#define OS_DEBUG_OSSCHEDULER_THREADREGISTER               (1)
-//#define OS_DEBUG_OSREADYLIST_INSERT                     (1)
-//#define OS_DEBUG_OSSCHEDULER_DUMPSTACK                  (1)
-//#define OS_DEBUG_CONSTRUCTORS_INIT                      (1)
+#define OS_DEBUG_OSSCHEDULER_CONTEXTSWITCH              (1)
+#define OS_DEBUG_OSSCHEDULER_THREADREGISTER             (1)
+#define OS_DEBUG_OSREADYLIST_INSERT                     (1)
+#define OS_DEBUG_OSSCHEDULER_DUMPSTACK                  (1)
+#endif
 
-#endif /*DEBUG*/
+#endif /* defined(DEBUG) */
 
 // ----------------------------------------------------------------------------
 
