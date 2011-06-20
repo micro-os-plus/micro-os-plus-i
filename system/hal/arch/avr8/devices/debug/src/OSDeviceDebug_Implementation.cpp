6af4b8a99a3e352202f4138757ab052d0cdf2e96
Liviu Ionescu <ilg@livius.net>
2011-06-20 21:11:56 +0000

    _P constructor/destructor functions added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2393 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr8/devices/debug/src/OSDeviceDebug_Implementation.cpp
+++ system/hal/arch/avr8/devices/debug/src/OSDeviceDebug_Implementation.cpp
@@ -11,33 +11,86 @@
 
 #include "portable/kernel/include/OS.h"
 
-void OSDeviceDebug::putString_P(const char*  PROGMEM pc)
-  {
-    if (pc == 0)
-      return;
+void
+OSDeviceDebug::putString_P(const char* PROGMEM pc)
+{
+  if (pc == 0)
+    return;
 
-    OSCriticalSection::enter();
-      {
-        unsigned char buff[10];
-        unsigned short i;
+  OSCriticalSection::enter();
+    {
+      unsigned char buff[10];
+      unsigned short i;
 
-        i = 0;
-        for (; (buff[i++] = pgm_read_byte(pc++)) != '\0';)
-          {
-            if (i == sizeof(buff))
-              {
-                commonPutBytes((const char*)buff, i);
-                i = 0;
-              }            
-          }
-        
-        if (i > 1)
-          {
-            commonPutBytes((const char*)buff, i);
-          }
-      }
-    OSCriticalSection::exit();
-  }
+      i = 0;
+      for (; (buff[i++] = pgm_read_byte(pc++)) != '\0';)
+        {
+          if (i == sizeof(buff))
+            {
+              commonPutBytes((const char*) buff, i);
+              i = 0;
+            }
+        }
+
+      if (i > 1)
+        {
+          commonPutBytes((const char*) buff, i);
+        }
+    }
+  OSCriticalSection::exit();
+}
+
+// display constructor name and object address
+void
+OSDeviceDebug::putConstructor_P(const char* PROGMEM pc, const void* p)
+{
+  register OSStack_t mask;
+
+  mask = criticalEnter();
+    {
+      putString_P(pc);
+      putString("() @");
+      putPtr(p);
+      putNewLine();
+    }
+  criticalExit(mask);
+}
+
+// display constructor name and object address
+void
+OSDeviceDebug::putConstructorWithIndex_P(const char* PROGMEM pc, uint16_t i,
+    const void* p)
+{
+  register OSStack_t mask;
+
+  mask = criticalEnter();
+    {
+      putString_P(pc);
+      putChar('(');
+      putDec(i);
+      putString(") @");
+      putPtr(p);
+      putNewLine();
+    }
+  criticalExit(mask);
+}
+
+// display destructor name and object address
+void
+OSDeviceDebug::putDestructor_P(const char* PROGMEM pc, const void* p)
+{
+  register OSStack_t mask;
+
+  mask = criticalEnter();
+    {
+      putChar('~');
+      putString_P(pc);
+      putString("() @");
+      putPtr(p);
+      putNewLine();
+    }
+  criticalExit(mask);
+}
 
 #endif /* defined(DEBUG) */
 #endif /* defined(OS_CONFIG_ARCH_AVR8) */
