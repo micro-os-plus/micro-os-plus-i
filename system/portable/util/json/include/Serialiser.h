a0b99165a9ddfcc68b8219bcbf5ff18cd680d2af
Liviu Ionescu <ilg@livius.net>
2011-06-23 20:16:25 +0000

    simple json serialiser added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2417 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/util/json/include/Serialiser.h
@@ -0,0 +1,52 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef UTIL_JSON_SERIALISER_H_
+#define UTIL_JSON_SERIALISER_H_
+
+#include "portable/kernel/include/OS.h"
+#include "portable/stdlib/include/ostream"
+
+namespace util
+{
+  namespace json
+  {
+    class Serialiser
+    {
+    public:
+      // ----- Constants ------------------------------------------------------
+      static const uchar_t SEPARATOR = ',';
+      static const uchar_t ARRAY_BEGIN = '[';
+      static const uchar_t ARRAY_END = ']';
+      static const uchar_t OBJECT_BEGIN = '{';
+      static const uchar_t OBJECT_END = '}';
+
+      // ----- Constructors and destructors -----------------------------------
+      Serialiser(std::ostream& out);
+      ~Serialiser();
+
+      // ---- Object methods --------------------------------------------------
+      void
+      putChar(uchar_t c);
+
+      void
+      putVariableName(const char* str);
+      void
+      putNumber(uint_t n);
+      void
+      putNumber(ulong_t n);
+      void
+      putNumberArray(uint8_t* pb, std::size_t len);
+      void
+      putQuotedString(const char* str);
+
+    private:
+      std::ostream& m_out;
+    };
+  }
+}
+
+#endif /* UTIL_JSON_SERIALISER_H_ */
