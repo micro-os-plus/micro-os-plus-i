0b0307ff73a96d0584bce182011f1ffff0b1cd8b
Liviu Ionescu <ilg@livius.net>
2011-06-23 08:03:10 +0000

    renamed Crc16x1021 and cosmetised
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2410 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/util/crc/include/Crc16x1021.h
@@ -0,0 +1,88 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef UTIL_CRC_CRC16X1021_H_
+#define UTIL_CRC_CRC16X1021_H_
+
+#include "portable/kernel/include/OS.h"
+
+namespace util
+{
+  namespace crc
+  {
+    // Polynomial: x^16 + x^12 + x^5 + 1 (0x1021)
+    // Initial value: 0x0
+
+    // Used in SD/MMC, XMODEM
+
+    class Crc16x1021
+    {
+    public:
+      Crc16x1021();
+      ~Crc16x1021();
+
+      static const uint16_t INITIAL_VALUE = 0x0000;
+      static const uint16_t FINAL_VALUE = 0x0000;
+
+      void
+      setInitialValue(void);
+      void
+      computePolynomial(uint8_t b);
+      void
+      computePolynomial(uint8_t* pb, std::size_t n);
+      bool
+      isFinalValueCorrect(void);
+
+      // ---- Static methods --------------------------------------------------
+      static void
+      computePolynomial(uint16_t& crc, uint8_t b);
+      static void
+      computePolynomial(uint16_t& crc, uint8_t* pb, std::size_t n);
+      static bool
+      isFinalValueCorrect(uint16_t crc);
+
+    private:
+      uint16_t m_crc;
+    };
+
+    // ----- Static inline methods --------------------------------------------
+
+    inline bool
+    Crc16x1021::isFinalValueCorrect(uint16_t crc)
+    {
+      return (crc == FINAL_VALUE);
+    }
+
+    // ----- Object inline methods --------------------------------------------
+
+    void
+    Crc16x1021::setInitialValue(void)
+    {
+      m_crc = INITIAL_VALUE;
+    }
+
+    void
+    Crc16x1021::computePolynomial(uint8_t b)
+    {
+      computePolynomial(m_crc, b);
+    }
+
+    void
+    Crc16x1021::computePolynomial(uint8_t* pb, std::size_t n)
+    {
+      computePolynomial(m_crc, pb, n);
+    }
+
+    bool
+    Crc16x1021::isFinalValueCorrect(void)
+    {
+      return isFinalValueCorrect(m_crc);
+    }
+
+  }
+}
+
+#endif /* UTIL_CRC_CRC16X1021_H_ */
---
+++ system/portable/util/crc/src/Crc16x1021.cpp
@@ -0,0 +1,61 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_INCLUDE_UTIL_CRC_CRC16X1021)
+
+#include "portable/kernel/include/OS.h"
+
+#include "portable/util/crc/include/Crc16x1021.h"
+
+namespace util
+{
+  namespace crc
+  {
+    Crc16x1021::Crc16x1021()
+    {
+      OSDeviceDebug::putConstructor_P(PSTR("Crc16x1021"), this);
+    }
+
+    Crc16x1021::~Crc16x1021()
+    {
+      OSDeviceDebug::putDestructor_P(PSTR("Crc16x1021"), this);
+    }
+
+    // ----- Static methods -----------------------------------------------
+
+    void
+    Crc16x1021::computePolynomial(uint16_t& crc, uint8_t b)
+    {
+      uint16_t acc;
+
+      acc = crc ^ ((uint16_t) b << 8);
+      uint_t i;
+      for (i = 0; i < 8; i++)
+        {
+          if (acc & 0x8000)
+            acc = (acc << 1) ^ 0x1021;
+          else
+            acc <<= 1;
+        }
+      // Update reference to return the newly computed value
+      crc = acc;
+    }
+
+    void
+    Crc16x1021::computePolynomial(uint16_t& crc, uint8_t* pb, std::size_t n)
+    {
+      std::size_t i;
+      for (i = 0; i < n; ++i)
+        {
+          computePolynomial(crc, *pb++);
+        }
+    }
+  }
+}
+
+#endif /* defined(OS_INCLUDE_UTIL_CRC_CRC16X1021) */
