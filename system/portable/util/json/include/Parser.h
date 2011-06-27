a50d449ec0f7fb21444dafab36fe42a3bc11f858
Liviu Ionescu <ilg@livius.net>
2011-06-27 22:02:25 +0000

    preliminary json parser
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2428 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/util/json/include/Parser.h
@@ -0,0 +1,75 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef UTIL_JSON_PARSER_H_
+#define UTIL_JSON_PARSER_H_
+
+#include "portable/kernel/include/OS.h"
+
+namespace util
+{
+  namespace json
+  {
+    class Parser
+    {
+    public:
+      Parser(const uchar_t* pBuf, std::size_t len);
+      ~Parser();
+
+      void
+      setInput(const uchar_t* pBuf, std::size_t len);
+
+      OSReturn_t
+      skipCharacter(uchar_t c);
+
+      OSReturn_t
+      parseVariableName(uchar_t* pName, std::size_t siz);
+
+      OSReturn_t
+      parseNumber(uint32_t& num);
+
+      OSReturn_t
+      parseNumber(int32_t& num);
+
+      uchar_t
+      skipSpaces(void);
+
+      bool
+      hasMoreContent(void);
+
+      uchar_t
+      getCurrentChar(void);
+
+      void
+      advanceCurrent(void);
+
+    private:
+      const uchar_t* m_pBuf;
+      std::size_t m_len;
+
+      const uchar_t* m_pCrt;
+      std::size_t m_remLen;
+
+    };
+
+    inline bool
+    Parser::hasMoreContent(void)
+    {
+      return (m_remLen > 0);
+    }
+
+#if true
+    inline uchar_t
+    Parser::getCurrentChar(void)
+    {
+      return *m_pCrt;
+    }
+#endif
+  }
+
+}
+
+#endif /* UTIL_JSON_PARSER_H_ */
---
+++ system/portable/util/json/src/Parser.cpp
@@ -0,0 +1,213 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_INCLUDE_UTIL_JSON_PARSER)
+
+#include "portable/kernel/include/OS.h"
+
+#include "portable/util/json/include/Parser.h"
+#include "portable/util/json/include/Serialiser.h"
+
+namespace util
+{
+  namespace json
+  {
+    // ----- Constructor ------------------------------------------------------
+
+    Parser::Parser(const uchar_t* pBuf, std::size_t len)
+    {
+      OSDeviceDebug::putConstructor_P(PSTR("util::json::Parser"), this);
+
+      setInput(pBuf, len);
+    }
+
+    // ----- Destructor -------------------------------------------------------
+
+    Parser::~Parser()
+    {
+      OSDeviceDebug::putDestructor_P(PSTR("util::json::Parser"), this);
+    }
+
+    // ----- Object methods----------------------------------------------------
+
+    void
+    Parser::setInput(const uchar_t* pBuf, std::size_t len)
+    {
+      m_pBuf = pBuf;
+      m_len = len;
+
+      m_pCrt = pBuf;
+      m_remLen = len;
+    }
+
+#if false
+    uchar_t
+    Parser::getCurrentChar(void)
+    {
+      OSDeviceDebug::putChar(*m_pCrt);
+      return *m_pCrt;
+    }
+#endif
+
+    void
+    Parser::advanceCurrent(void)
+    {
+      --m_remLen;
+      ++m_pCrt;
+    }
+
+    uchar_t
+    Parser::skipSpaces(void)
+    {
+      for (; hasMoreContent(); advanceCurrent())
+        {
+          uchar_t ch;
+          ch = getCurrentChar();
+          if (ch != ' ' && ch != '\t')
+            return ch;
+        }
+
+      return '\0';
+    }
+
+    OSReturn_t
+    Parser::skipCharacter(uchar_t c)
+    {
+      uchar_t ch;
+
+      ch = skipSpaces();
+
+      if (m_remLen > 0)
+        {
+          if (ch == c)
+            {
+              advanceCurrent();
+
+              return OSReturn::OS_OK;
+            }
+        }
+
+      return OSReturn::OS_NOT_FOUND;
+    }
+
+    OSReturn_t
+    Parser::parseVariableName(uchar_t* pName, std::size_t siz)
+    {
+      OSReturn_t r;
+
+      r = skipCharacter('"');
+      if (r != OSReturn::OS_OK)
+        return r;
+
+      bool isTooLarge;
+      isTooLarge = false;
+
+      for (; hasMoreContent(); advanceCurrent())
+        {
+          uchar_t ch;
+          ch = getCurrentChar();
+
+          if (ch == '"')
+            {
+              // Skip over terminating "
+              advanceCurrent();
+              break;
+            }
+
+          if (siz > 1)
+            {
+              *pName++ = ch;
+              *pName = '\0';
+              --siz;
+            }
+          else
+            {
+              isTooLarge = true;
+            }
+        }
+
+      r = skipCharacter(':');
+      if (r != OSReturn::OS_OK)
+        return r;
+
+      if (isTooLarge)
+        return OSReturn::OS_NOT_ENOUGH_SPACE;
+
+      return OSReturn::OS_OK;
+    }
+
+    OSReturn_t
+    Parser::parseNumber(uint32_t& num)
+    {
+      uint32_t n;
+      n = 0;
+
+      uchar_t ch;
+      ch = skipSpaces();
+
+      for (; hasMoreContent(); advanceCurrent())
+        {
+          uchar_t ch;
+          ch = getCurrentChar();
+
+          if ('0' <= ch && ch <= '9')
+            {
+              n = n * 10 + (ch - '0');
+            }
+          else
+            break;
+        }
+      num = n;
+
+      return OSReturn::OS_OK;
+    }
+
+    OSReturn_t
+    Parser::parseNumber(int32_t& num)
+    {
+      int32_t n;
+      n = 0;
+
+      bool isNegative;
+      isNegative = false;
+
+      uchar_t ch;
+      ch = skipSpaces();
+      if (ch == '-')
+        {
+          isNegative = true;
+          advanceCurrent();
+        }
+
+      for (; hasMoreContent(); advanceCurrent())
+        {
+          uchar_t ch;
+          ch = getCurrentChar();
+
+          if ('0' <= ch && ch <= '9')
+            {
+              n = n * 10 + (ch - '0');
+            }
+          else
+            break;
+        }
+
+      if (isNegative)
+        num = -n;
+      else
+        num = n;
+
+      return OSReturn::OS_OK;
+    }
+
+  // --------------------------------------------------------------------------
+  }
+}
+
+#endif /* defined(OS_INCLUDE_UTIL_JSON_PARSER) */
+
