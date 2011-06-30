9ba8022446fe49aa25aef27556f980a56df9a3a0
Alexandru Burcea <alexandru.burcea@aft.ro>
2011-06-30 22:15:40 +0000

    SetGpsSchedule() prototype added;  Pin definition for connect with GPS module
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2443 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- samples/gps/include/GpsManager.h
+++ samples/gps/include/GpsManager.h
@@ -11,6 +11,25 @@
 
 #include "portable/misc/include/GpsPosition.h"
 
+#define GPS_POWER_SW_PORT               GPIOX
+#define GPS_POWER_SW_PIN                AVR32_PIN_PX44
+
+#define GPS_RST_PORT                    GPIOX
+#define GPS_RST_PIN                     AVR32_PIN_PX15
+
+#define GPS_PPS_PORT                    GPIOA
+#define GPS_PPS_PIN                     AVR32_PIN_PA12
+
+#define GPS_WK_UP_PORT                  GPIOX
+#define GPS_WK_UP_PIN                   AVR32_PIN_PX42
+
+#define GPS_START_UP_NR_MSG             (6)
+
+#define GPS_START_UP_READ_TIMEOUT       (1000)
+#define GPS_MSG_READ_TIMEOUT            (250)
+
+#define OS_CFGINT_GPSMANAGER_RXBUF_ARRAY_SIZE   (100)
+#define OS_CFGINT_GPSMANAGER_TXBUF_ARRAY_SIZE   (100)
 
 class GpsManager : public OSThread
 {
@@ -25,17 +44,45 @@ public:
   virtual void
   threadMain(void);
 
-  OSReturn_t requestGpsPosition(void);
+  OSReturn_t
+  RequestGpsPosition(void);
+  OSReturn_t
+  SetGpsSchedule(int address, int id, int absoluteDateTime, int onDurationSeconds,
+      int offDurationSeconds);
+
+//  OSReturn_t
+//  getPosition(gpsPosition_t* pPos);
+
+
+  bool
+  getOnStatus(void);
 
   OSReturn_t getPosition(GpsPosition_t& pPos);
 
+
 private:
   // members
-  OSStack_t m_stack[(OSThread::STACK_MINIMAL_SIZE + 400) / sizeof(OSStack_t)];
+  OSStack_t m_stack[(OSThread::STACK_MINIMAL_SIZE + 600) / sizeof(OSStack_t)];
   OSDeviceCharacter& m_dev;
 
   OSEventFlags m_eventFlags;
   GpsPosition m_gpsPosition;
+
+  Parser m_parser;
+  unsigned char m_token[20]; // parsed token
+
+  bool m_isOn;
+
+  void
+  setOnStatus(bool value);
+
+  OSReturn_t
+  sleepEnter(void);
+  OSReturn_t
+  sleepExit(void);
+
+  void
+  init(void);
 
 };
 
