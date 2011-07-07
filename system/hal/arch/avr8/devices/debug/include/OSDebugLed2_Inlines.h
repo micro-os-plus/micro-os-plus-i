3d37d47f7bce22fe8f71a464d8dd0883010def8d
Liviu Ionescu <ilg@livius.net>
2011-07-07 21:51:17 +0000

    _INIT -> _CONFIG, _WRITE
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2466 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr8/devices/debug/include/OSDebugLed2_Inlines.h
+++ system/hal/arch/avr8/devices/debug/include/OSDebugLed2_Inlines.h
@@ -12,7 +12,7 @@
 inline void OSDebugLed2::init(void)
   {
     // init led port as output
-    OS_GPIO_PIN_CONFIG(OS_CONFIG_DEBUG_LED2_PORT_INIT, OS_CONFIG_DEBUG_LED2_BIT, 1);
+    OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_LED2_PORT_CONFIG, OS_CONFIG_DEBUG_LED2_BIT);
   }
 
 inline void OSDebugLed2::toggle(void)
@@ -20,7 +20,7 @@ inline void OSDebugLed2::toggle(void)
     OSCriticalSection::enter();
       {
         // toggle led
-        OS_GPIO_PIN_TOGGLE(OS_CONFIG_DEBUG_LED2_PORT, OS_CONFIG_DEBUG_LED2_BIT);
+        OS_GPIO_PIN_TOGGLE(OS_CONFIG_DEBUG_LED2_PORT_WRITE, OS_CONFIG_DEBUG_LED2_BIT);
       }
     OSCriticalSection::exit();
   }
@@ -30,7 +30,7 @@ inline void OSDebugLed2::on(void)
     OSCriticalSection::enter();
       {
         // toggle led
-        OS_GPIO_PIN_HIGH(OS_CONFIG_DEBUG_LED2_PORT, OS_CONFIG_DEBUG_LED2_BIT);
+        OS_GPIO_PIN_HIGH(OS_CONFIG_DEBUG_LED2_PORT_WRITE, OS_CONFIG_DEBUG_LED2_BIT);
       }
     OSCriticalSection::exit();
   }
@@ -40,7 +40,7 @@ inline void OSDebugLed2::off(void)
     OSCriticalSection::enter();
       {
         // toggle led
-        OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_LED2_PORT, OS_CONFIG_DEBUG_LED2_BIT);
+        OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_LED2_PORT_WRITE, OS_CONFIG_DEBUG_LED2_BIT);
       }
     OSCriticalSection::exit();
   }
