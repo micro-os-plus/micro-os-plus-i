435f56ea07087aa6f02afd99e1a66dafaf7339a6
Liviu Ionescu <ilg@livius.net>
2011-07-09 18:18:07 +0000

    debug prints when entering sleep mode; indent
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@2499 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/src/OSThreadIdle.cpp
+++ system/portable/kernel/src/OSThreadIdle.cpp
@@ -14,68 +14,78 @@
 
 OSThreadIdle::OSThreadIdle() :
   OSThread("IDLE", m_stack, sizeof(m_stack), OSThread::IDLE_PRIORITY)
-  {
-    OSDeviceDebug::putConstructor_P(PSTR("OSThreadIdle"), this);
-  }
+{
+  OSDeviceDebug::putConstructor_P(PSTR("OSThreadIdle"), this);
+}
 
-void OSThreadIdle::threadMain(void)
-  {
+void
+OSThreadIdle::threadMain(void)
+{
 #if defined(DEBUG)
-    // TODO : eventually define and use lockEnter()/lockExit()
-    OSCriticalSection::enter();
-      {
-        OSDeviceDebug::putString_P(PSTR("OSThreadIdle::threadMain()"));
-        OSDeviceDebug::putNewLine();
-      }
-    OSCriticalSection::exit();
+  // TODO : eventually define and use lockEnter()/lockExit()
+  OSCriticalSection::enter();
+    {
+      OSDeviceDebug::putString_P(PSTR("OSThreadIdle::threadMain()"));
+      OSDeviceDebug::putNewLine();
+    }
+  OSCriticalSection::exit();
 #endif
 
-    for (;;)
-      {
+  for (;;)
+    {
 
 #if defined(OS_INCLUDE_OSTHREAD_SLEEP)
-        if (!enterSleep())
-          {
-            OSScheduler::ledActiveOff(); // turn activity led off
-            OSCPU::idle();
-          }
+      if (!enterSleep())
+        {
+          OSScheduler::ledActiveOff(); // turn activity led off
+          OSCPU::idle();
+        }
 #else
-        OSScheduler::ledActiveOff(); // turn activity led off
-        OSCPU::idle();
+      OSScheduler::ledActiveOff(); // turn activity led off
+      OSCPU::idle();
 #endif
-        // required for cooperative and after non-scheduled interrupts
-        OSScheduler::yield();
-      }
-  }
+      // required for cooperative and after non-scheduled interrupts
+      OSScheduler::yield();
+    }
+}
 
 #if defined(OS_INCLUDE_OSTHREAD_SLEEP)
-bool OSThreadIdle::enterSleep(void)
-  {
-    for (int i = 0; i < OSScheduler::getThreadsCount(); ++i)
-      {
-        // if any thread requests to prevent deep sleep, return
-        if (!OSScheduler::getThread(i)->isDeepSleepAllowed())
+bool
+OSThreadIdle::enterSleep(void)
+{
+  for (int i = 0; i < OSScheduler::getThreadsCount(); ++i)
+    {
+      // if any thread requests to prevent deep sleep, return
+      if (!OSScheduler::getThread(i)->isDeepSleepAllowed())
         return false;
-      }
+    }
 
-    // if there are threads scheduled on the ticks timer, return
-    if (OSScheduler::timerTicks.getCount()> 0)
+  // if there are threads scheduled on the ticks timer, return
+  if (OSScheduler::timerTicks.getCount() > 0)
     return false;
 
-    // finally enter (deep) sleep
-    if (OSScheduler::isAllowDeepSleep())
-      {
-        OSScheduler::ledActiveOff(); // turn activity led off
-        OSCPU::deepSleep();
-      }
-    else
-      {
-        OSScheduler::ledActiveOff(); // turn activity led off
-        OSCPU::sleep();
-      }
+  // finally enter (deep) sleep
+  if (OSScheduler::isAllowDeepSleep())
+    {
+#if defined(DEBUG) && defined(OS_DEBUG_OSTHREADIDLE_ENTERSLEEP)
+      OSDeviceDebug::putChar('S');
+#endif /* defined(DEBUG) && defined(OS_DEBUG_OSTHREADIDLE_ENTERSLEEP) */
 
-    return true;
-  }
+      OSScheduler::ledActiveOff(); // turn activity led off
+      OSCPU::deepSleep();
+    }
+  else
+    {
+#if defined(DEBUG) && defined(OS_DEBUG_OSTHREADIDLE_ENTERSLEEP)
+      OSDeviceDebug::putChar('s');
+#endif /* defined(DEBUG) && defined(OS_DEBUG_OSTHREADIDLE_ENTERSLEEP) */
+
+      OSScheduler::ledActiveOff(); // turn activity led off
+      OSCPU::sleep();
+    }
+
+  return true;
+}
 #endif
 
 #ifndef OS_INCLUDE_CUSTOM_IDLE_THREAD
