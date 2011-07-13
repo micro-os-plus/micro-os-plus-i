7e98799f96866d2b72a9fe9a9ca29f09493c9048
Liviu Ionescu <ilg@livius.net>
2011-07-13 11:19:35 +0000

    Registers instead of PortRegisters; read/write inline methods added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7186 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/onchip/include/Mci.h
+++ system/hal/arch/avr32/uc3/devices/onchip/include/Mci.h
@@ -21,7 +21,7 @@ namespace avr32
 
       // ----- Port memory mapped registers -----------------------------------
 
-      class PortRegisters
+      class Registers
       {
       public:
         // --------------------------------------------------------------------
@@ -42,10 +42,7 @@ namespace avr32
         regReadWrite_t blkr; //0x0018
         regReadWrite_t cstor; //0x001C
 
-        regReadOnly_t rspr; //0x0020
-        regReadOnly_t rspr1; //0x0024
-        regReadOnly_t rspr2; //0x0028
-        regReadOnly_t rspr3; //0x002C
+        regReadOnly_t rspr[4]; //0x0020-0x002C
 
         regReadOnly_t rdr; //0x0030
         regWriteOnly_t tdr; //0x0034
@@ -65,7 +62,276 @@ namespace avr32
         regNotAllocated_t dummyEC[4]; //0x00EC-0x00F8
 
         regReadOnly_t version; //0x00FC
+
+        // ----- Methods ------------------------------------------------------
+
+        void
+        writeControl(uint8_t mask);
+
+        void
+        writeMode(uint32_t value);
+
+        uint32_t
+        readMode(void);
+
+        void
+        writeDataTimeout(uint8_t value);
+
+        uint8_t
+        readDataTimeout(void);
+
+        void
+        writeSdCard(uint8_t value);
+
+        uint8_t
+        readSdCard(void);
+
+        void
+        writeArgument(uint32_t value);
+
+        uint32_t
+        readArgument(void);
+
+        void
+        writeCommand(uint32_t value);
+
+        void
+        writeBlock(uint32_t value);
+
+        uint32_t
+        readBlock(void);
+
+        void
+        writeCompletionSignalTimeout(uint8_t value);
+
+        uint8_t
+        readCompletionSignalTimeout(void);
+
+        uint32_t
+        readResponse(uint_t index);
+
+        uint32_t
+        readReceiveData(void);
+
+        void
+        writeTransmitData(uint32_t value);
+
+        uint32_t
+        readStatus(void);
+
+        // Writing 1 to a bit will se IMRt; writing 0 will be ignored
+        void
+        writeInterruptEnable(uint32_t value);
+
+        // Writing 1 to a bit will clear IMR; writing 0 will be ignored
+        void
+        writeInterruptDisable(uint32_t value);
+
+        uint32_t
+        readInterruptMask(void);
+
+        void
+        writeDmaConfiguration(uint16_t value);
+
+        uint16_t
+        readDmaConfiguration(void);
+
+        void
+        writeConfiguration(uint16_t value);
+
+        uint16_t
+        readConfiguration(void);
+
+        void
+        writeWriteProtectMode(uint32_t value);
+
+        uint32_t
+        readWriteProtectMode(void);
+
+        uint32_t
+        readWriteProtectStatus(void);
+
+        uint32_t
+        readVersion(void);
+
+      };
+
+      inline void
+      Registers::writeControl(uint8_t mask)
+      {
+        this->cr = mask;
       }
+
+      inline void
+      Registers::writeMode(uint32_t value)
+      {
+        this->mr = value;
+      }
+
+      inline uint32_t
+      Registers::readMode(void)
+      {
+        return this->mr;
+      }
+
+      inline void
+      Registers::writeDataTimeout(uint8_t value)
+      {
+        this->dtor = value;
+      }
+
+      inline uint8_t
+      Registers::readDataTimeout(void)
+      {
+        return this->dtor;
+      }
+
+      inline void
+      Registers::writeSdCard(uint8_t value)
+      {
+        this->sdcr = value;
+      }
+
+      inline uint8_t
+      Registers::readSdCard(void)
+      {
+        return this->sdcr;
+      }
+
+      inline void
+      Registers::writeArgument(uint32_t value)
+      {
+        this->argr = value;
+      }
+
+      inline uint32_t
+      Registers::readArgument(void)
+      {
+        return this->argr;
+      }
+
+      inline void
+      Registers::writeCommand(uint32_t value)
+      {
+        this->cmdr = value;
+      }
+
+      inline void
+      Registers::writeBlock(uint32_t value)
+      {
+        this->cmdr = value;
+      }
+
+      inline uint32_t
+      Registers::readBlock(void)
+      {
+        return this->blkr;
+      }
+
+      inline void
+      Registers::writeCompletionSignalTimeout(uint8_t value)
+      {
+        this->cstor = value;
+      }
+
+      inline uint8_t
+      Registers::readCompletionSignalTimeout(void)
+      {
+        return this->cstor;
+      }
+
+      inline uint32_t
+      Registers::readResponse(uint_t index)
+      {
+        return this->rspr[index];
+      }
+
+      inline uint32_t
+      Registers::readReceiveData(void)
+      {
+        return this->rdr;
+      }
+
+      inline void
+      Registers::writeTransmitData(uint32_t value)
+      {
+        this->tdr = value;
+      }
+
+      inline uint32_t
+      Registers::readStatus(void)
+      {
+        return this->sr;
+      }
+
+      // Writing 1 to a bit will se IMRt; writing 0 will be ignored
+      inline void
+      Registers::writeInterruptEnable(uint32_t value)
+      {
+        this->ier = value;
+      }
+
+      // Writing 1 to a bit will clear IMR; writing 0 will be ignored
+      inline void
+      Registers::writeInterruptDisable(uint32_t value)
+      {
+        this->idr = value;
+      }
+
+      inline uint32_t
+      Registers::readInterruptMask(void)
+      {
+        return this->imr;
+      }
+
+      inline void
+      Registers::writeDmaConfiguration(uint16_t value)
+      {
+        this->dma = value;
+      }
+
+      inline uint16_t
+      Registers::readDmaConfiguration(void)
+      {
+        return this->dma;
+      }
+
+      inline void
+      Registers::writeConfiguration(uint16_t value)
+      {
+        this->cfg = value;
+      }
+
+      inline uint16_t
+      Registers::readConfiguration(void)
+      {
+        return this->cfg;
+      }
+
+      inline void
+      Registers::writeWriteProtectMode(uint32_t value)
+      {
+        this->wpmr = value;
+      }
+
+      inline uint32_t
+      Registers::readWriteProtectMode(void)
+      {
+        return this->wpmr;
+      }
+
+      inline uint32_t
+      Registers::readWriteProtectStatus(void)
+      {
+        return this->wpsr;
+      }
+
+      inline uint32_t
+      Registers::readVersion(void)
+      {
+        return this->version;
+      }
+
     }
 
     class Mci
@@ -74,8 +340,7 @@ namespace avr32
       Mci();
 
     public:
-      volatile mci::PortRegisters& portRegisters;
-
+      volatile mci::Registers& registers;
 
     };
   }
