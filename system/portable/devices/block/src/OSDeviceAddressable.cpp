25a8e4091ee44481a479d197a2e93e9ac87559be
Liviu Ionescu <ilg@livius.net>
2011-07-14 19:54:09 +0000

    templates for methods, instead of abstract methods
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7200 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/devices/block/src/OSDeviceAddressable.cpp
+++ system/portable/devices/block/src/OSDeviceAddressable.cpp
@@ -22,4 +22,79 @@ OSDeviceAddressable::~OSDeviceAddressable()
   OSDeviceDebug::putDestructor_P(PSTR("OSDeviceAddressable"), this);
 }
 
+OSReturn_t
+OSDeviceAddressable::open(void)
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::open()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
+OSDeviceAddressable::Offset_t
+OSDeviceAddressable::getDeviceSize(void)
+{
+  return 0; // No device
+}
+
+OSReturn_t
+OSDeviceAddressable::readBytes(OSDeviceAddressable::Offset_t offset __attribute__((unused)),
+    uint8_t* pBuf __attribute__((unused)), OSDeviceAddressable::Count_t count __attribute__((unused)))
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::readBytes()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
+OSReturn_t
+OSDeviceAddressable::writeBytes(OSDeviceAddressable::Offset_t offset __attribute__((unused)),
+    uint8_t* pBuf __attribute__((unused)), OSDeviceAddressable::Count_t count __attribute__((unused)))
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::writeBytes()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
+OSDeviceAddressable::Alignnment_t
+OSDeviceAddressable::getWriteAlignment(void)
+{
+  return 512; // Default alignment size, from MMC specs
+}
+
+OSReturn_t
+OSDeviceAddressable::close(void)
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::close()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
+OSReturn_t
+OSDeviceAddressable::erase(OSDeviceAddressable::Offset_t offset __attribute__((unused)),
+    OSDeviceAddressable::Count_t count __attribute__((unused)))
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::erase()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
+OSDeviceAddressable::Alignnment_t
+OSDeviceAddressable::getEraseAlignment(void)
+{
+  return 512; // Default alignment size, from MMC specs
+}
+
+OSReturn_t
+OSDeviceAddressable::eraseEntireDevice(void)
+{
+  OSDeviceDebug::putString_P(PSTR("OSDeviceAddressable::eraseEntireDevice()"));
+  OSDeviceDebug::putNewLine();
+
+  return OSReturn::OS_NOT_IMPLEMENTED;
+}
+
 #endif /* defined(OS_INCLUDE_OSDEVICEADDRESSABLE) */
