9bc7a6e3b443a90ffef4d3058fc3f635ec6e253a
Liviu Ionescu <ilg@livius.net>
2011-07-29 22:01:50 +0000

    OSDebugLed1 properly implemented
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7280 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/debug/include/OSDebugLed1_Inlines.h
+++ system/hal/arch/avr32/uc3/devices/debug/include/OSDebugLed1_Inlines.h
@@ -14,42 +14,55 @@ OSDebugLed1::init(void)
 {
   // init led port as output
   OS_GPIO_PIN_CONFIG_ENABLE(OS_CONFIG_DEBUG_LED1_PORT_CONFIG, OS_CONFIG_DEBUG_LED1_BIT);
+#if OS_CONFIG_DEBUG_LED1_ISACTIVE_LOW
+  OS_GPIO_PIN_HIGH(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#else
+  OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#endif
   OS_GPIO_PIN_CONFIG_OUTPUT(OS_CONFIG_DEBUG_LED1_PORT_CONFIG, OS_CONFIG_DEBUG_LED1_BIT);
 }
 
 inline void
 OSDebugLed1::toggle(void)
 {
-  OSCriticalSection::enter();
+  //OSCriticalSection::enter();
     {
       // toggle led
       OS_GPIO_PIN_TOGGLE(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
     }
-  OSCriticalSection::exit();
+  //OSCriticalSection::exit();
 }
 
 inline
 void
 OSDebugLed1::on(void)
 {
-  OSCriticalSection::enter();
+  //OSCriticalSection::enter();
     {
-      // toggle led
+      // turn led on
+#if OS_CONFIG_DEBUG_LED1_ISACTIVE_LOW
+      OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#else
       OS_GPIO_PIN_HIGH(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#endif
     }
-  OSCriticalSection::exit();
+  //OSCriticalSection::exit();
 }
 
 inline
 void
 OSDebugLed1::off(void)
 {
-  OSCriticalSection::enter();
+  //OSCriticalSection::enter();
     {
-      // toggle led
+      // turn led off
+#if OS_CONFIG_DEBUG_LED1_ISACTIVE_LOW
+      OS_GPIO_PIN_HIGH(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#else
       OS_GPIO_PIN_LOW(OS_CONFIG_DEBUG_LED1_PORT_WRITE, OS_CONFIG_DEBUG_LED1_BIT);
+#endif
     }
-  OSCriticalSection::exit();
+  //OSCriticalSection::exit();
 }
 
 inline unsigned char
