570e48c5d06208f7232ceef072b3c6a1eb41fd4c
Liviu Ionescu <ilg@livius.net>
2011-07-31 18:18:42 +0000

    4.3.7305 (minor increased to 3, cpuSleep critical sections added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7306 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/kernel/include/OS_Defines.h
+++ system/portable/kernel/include/OS_Defines.h
@@ -16,8 +16,8 @@
 
 // Notice: do not use parenthesis! (the values will be stringified)
 #define OS_CFGINT_VERSION_MAJOR         4
-#define OS_CFGINT_VERSION_MINOR         2
-#define OS_CFGINT_VERSION_REVISION      7288
+#define OS_CFGINT_VERSION_MINOR         3
+#define OS_CFGINT_VERSION_REVISION      7305
 
 #define OS_CFGSTR_STRINGIFY(x)          OS_CFGSTR_SHARP(x)
 #define OS_CFGSTR_SHARP(x)              #x
