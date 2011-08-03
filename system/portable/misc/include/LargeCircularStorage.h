17a80c120cb18c580a841468b1b45f9da383fac4
Liviu Ionescu <ilg@livius.net>
2011-08-03 21:41:50 +0000

    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7340 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/misc/include/LargeCircularStorage.h
+++ system/portable/misc/include/LargeCircularStorage.h
@@ -11,15 +11,53 @@
 
 // ----------------------------------------------------------------------------
 
-class LargeCircularStorage
+class LargeCircularStorageWriter
 {
 public:
-  LargeCircularStorage(OSDeviceBlock& dev);
-  ~LargeCircularStorage();
+#if defined(OS_INCLUDE_LARGECIRCULARSTORAGE_LARGE_SESSIONID)
+  typedef uint64_t SessionUniqueId_t;
+#else
+  typedef uint32_t SessionUniqueId_t;
+#endif
+
+  typedef uint32_t BlockUniqueId_t;
+
+  LargeCircularStorageWriter(OSDeviceBlock& device);
+  ~LargeCircularStorageWriter();
+
+  OSReturn_t
+  createSession(SessionUniqueId_t sessionId);
+
+  // Fill in the
+  OSReturn_t
+  writeBlock(uint8_t* pBuf);
+
+  // Return the amount of space the application should reserve
+  // at the beginning of each written block
+  uint_t
+  getReservedHeaderSize(void);
+
+  OSReturn_t
+  closeSession(void);
 
 private:
-  OSDeviceBlock& m_dev;
+  OSDeviceBlock& m_device;
+
+  // An application supplied unique id for identifying sessions
+  SessionUniqueId_t m_sessionUniqueId;
+  // A monotone increasing unique Id
+  BlockUniqueId_t m_blockUniqueId;
+
+  OSDeviceBlock::BlockNumber_t m_sessionFirstBlockNumber;
+  OSDeviceBlock::BlockNumber_t m_currentBlockNumber;
 };
+
+inline uint_t
+LargeCircularStorageWriter::getReservedHeaderSize(void)
+{
+  return sizeof(SessionUniqueId_t) + sizeof(OSDeviceBlock::BlockNumber_t)
+      + sizeof(BlockUniqueId_t);
+}
 
 // ----------------------------------------------------------------------------
 
--- system/portable/misc/src/LargeCircularStorage.cpp
+++ system/portable/misc/src/LargeCircularStorage.cpp
@@ -12,15 +12,112 @@
 
 // ----------------------------------------------------------------------------
 
-LargeCircularStorage::LargeCircularStorage(OSDeviceBlock& dev) :
-  m_dev(dev)
+LargeCircularStorageWriter::LargeCircularStorageWriter(OSDeviceBlock& device) :
+  m_device(device)
 {
-  debug.putConstructor_P(PSTR("GpsCoordinate"), this);
+  debug.putConstructor_P(PSTR("LargeCircularStorage"), this);
 }
 
-LargeCircularStorage::~LargeCircularStorage()
+LargeCircularStorageWriter::~LargeCircularStorageWriter()
 {
-  debug.putDestructor_P(PSTR("GpsCoordinate"), this);
+  debug.putDestructor_P(PSTR("LargeCircularStorage"), this);
+}
+
+OSReturn_t
+LargeCircularStorageWriter::createSession(SessionUniqueId_t sessionUniqueId)
+{
+  if (sessionUniqueId == 0)
+    {
+      return OSReturn::OS_BAD_PARAMETER;
+
+    }
+  m_sessionUniqueId = sessionUniqueId;
+
+  // TODO: search for the next free block;
+  // For the moment just start from the beginning
+  // and with a dummy block unique id
+
+  m_sessionFirstBlockNumber = 0;
+  m_currentBlockNumber = 0;
+
+  m_blockUniqueId = 0x12345678;
+
+  return OSReturn::OS_OK;
+}
+
+OSReturn_t
+LargeCircularStorageWriter::closeSession(void)
+{
+  m_sessionUniqueId = 0;
+
+  return OSReturn::OS_OK;
+}
+
+OSReturn_t
+LargeCircularStorageWriter::writeBlock(uint8_t* pBuf)
+{
+  if (m_sessionUniqueId == 0)
+    {
+      return OSReturn::OS_NOT_INITIALIZED;
+    }
+
+  // Fill-in the block header, currently having three fields
+  //    sessionUniqueId
+  //    sessionFirstBlockNumber
+  //    blockUniqueId
+
+  uint8_t* p;
+  p = (uint8_t*) pBuf + getReservedHeaderSize() - 1;
+
+  uint8_t b;
+  uint_t i;
+
+  BlockUniqueId_t blockUniqueId;
+  blockUniqueId = m_blockUniqueId;
+
+  for (i = 0; i < sizeof(BlockUniqueId_t); i++)
+    {
+      b = (uint8_t) blockUniqueId;
+      *p-- = b;
+
+      blockUniqueId >>= 8;
+    }
+
+  OSDeviceBlock::BlockNumber_t sessionFirstBlockNumber;
+  sessionFirstBlockNumber = m_sessionFirstBlockNumber;
+
+  for (i = 0; i < sizeof(OSDeviceBlock::BlockNumber_t); i++)
+    {
+      b = (uint8_t) sessionFirstBlockNumber;
+      *p-- = b;
+
+      sessionFirstBlockNumber >>= 8;
+    }
+
+  SessionUniqueId_t sessionUniqueId;
+  sessionUniqueId = m_sessionUniqueId;
+
+  for (i = 0; i < sizeof(SessionUniqueId_t); i++)
+    {
+      b = (uint8_t) sessionUniqueId;
+      *p-- = b;
+
+      sessionUniqueId >>= 8;
+    }
+
+  OSReturn_t ret;
+  // Write one block the output device
+  ret = m_device.writeBlocks(m_currentBlockNumber, pBuf, 1);
+
+  // Increment block number; if reaching device size, roll over
+  ++m_currentBlockNumber;
+  if (m_currentBlockNumber >= m_device.getDeviceSize())
+    m_currentBlockNumber = 0;
+
+  // Increase the monotone increasing unique block number
+  ++m_blockUniqueId;
+
+  return ret;
 }
 
 // ----------------------------------------------------------------------------
