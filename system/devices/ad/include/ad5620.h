643ac67aaf2ad56d8dbb6c1f37acbb4c72a0d7c5
Liviu Ionescu <ilg@livius.net>
2011-08-05 20:18:39 +0000

    ad5620 driver added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7349 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/devices/ad/include/ad5620.h
@@ -0,0 +1,129 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef DEVICE_AD_AD5620_H_
+#define DEVICE_AD_AD5620_H_
+
+#include "portable/kernel/include/OS.h"
+
+#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+
+#include "hal/arch/avr32/uc3/devices/onchip/include/Usart.h"
+#include "portable/devices/generic/include/ChipSelectActiveLow.h"
+
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+namespace device
+{
+  namespace ad
+  {
+    namespace ad5620
+    {
+      // ----- Type definitions -----------------------------------------------
+
+      typedef ChipSelectActiveLow ChipSelectActiveLow_t;
+
+      typedef uint16_t Value_t;
+
+      class Value
+      {
+      public:
+        const static Value_t VALIDATION_MASK = 0x0FFF;
+        const static uint_t SHIFT_BITS = 2;
+
+        const static Value_t MIN = 0;
+        const static Value_t MAX = VALIDATION_MASK;
+        const static Value_t MIDDLE = (MIN + ((MAX - MIN) / 2));
+      };
+
+      typedef uint16_t PowerDownMode_t;
+
+      class PowerDownMode
+      {
+      public:
+        const static uint_t SHIFT_BITS = 14;
+
+        const static PowerDownMode_t NORMAL_OPERATION = (0x0 << SHIFT_BITS);
+        const static PowerDownMode_t _1K_TO_GND = (0x1 << SHIFT_BITS);
+        const static PowerDownMode_t _100K_TO_GND = (0x2 << SHIFT_BITS);
+        const static PowerDownMode_t THREE_STATE = (0x3 << SHIFT_BITS);
+
+        const static PowerDownMode_t MASK = (0x3 << SHIFT_BITS);
+      };
+
+      typedef uint32_t ClockFrequencyHz_t;
+
+      class ClockFrequencyHz
+      {
+      public:
+        const static ClockFrequencyHz_t MAX = 30000000L; // 30 MHz
+      };
+
+#if defined(OS_CONFIG_FAMILY_AVR32UC3)
+
+      typedef avr32::uc3::Gpio Gpio_t;
+      typedef avr32::uc3::usart::SpiMaster SpiMaster_t;
+
+#else
+#error "Missing OS_CONFIG_FAMILY_* definition"
+#endif
+
+    }
+
+    class Ad5620
+    {
+      // ----------------------------------------------------------------------
+
+    public:
+
+      // ----- Constructors & Destructors -------------------------------------
+
+      Ad5620(ad5620::ChipSelectActiveLow_t& cs, ad5620::SpiMaster_t& spi);
+      ~Ad5620();
+
+    public:
+
+      // ----- Public methods -------------------------------------------------
+
+      void
+      powerUp(void);
+
+      void
+      powerDown(ad5620::PowerDownMode_t mode);
+
+      void
+      configureClockFrequencyHz(ad5620::ClockFrequencyHz_t clockFrequency);
+
+      void
+      writeValue(ad5620::Value_t value);
+
+      ad5620::Value_t
+      getPreviouslyWrittenValue(void);
+
+      // Also return new value
+      ad5620::Value_t
+      incrementValue(void);
+
+      // Also return new value
+      ad5620::Value_t
+      decrementValue(void);
+
+      // ----- Private members ------------------------------------------------
+
+      ad5620::ChipSelectActiveLow_t& m_cs;
+      ad5620::SpiMaster_t& m_spi;
+
+      ad5620::Value_t m_shadowValue;
+
+      // ----------------------------------------------------------------------
+
+    };
+  }
+}
+
+#endif /* DEVICE_AD_AD5620_H_ */
---
+++ system/devices/ad/src/ad5620.cpp
@@ -0,0 +1,132 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_INCLUDE_DEVICE_AD_AD5620)
+
+#include "devices/ad/include/ad5620.h"
+
+namespace device
+{
+  namespace ad
+  {
+
+    // ----- Constructors & Destructors ---------------------------------------
+
+    Ad5620::Ad5620(ad5620::ChipSelectActiveLow_t& cs, ad5620::SpiMaster_t& spi) :
+      m_cs(cs), m_spi(spi)
+    {
+      OSDeviceDebug::putConstructor("device::ad::Ad5620", this);
+
+      // In the manual there is a reference that some models start at
+      // mid-value, but common ones start at zero.
+      // Just set it explicitly to be sure.
+#if defined(OS_CFGBOOL_DEVICE_AD_AD5620_START_WITH_MIDDLE_VALUE)
+      m_shadowValue = ad5620::Value::MIDDLE;
+#else
+      m_shadowValue = ad5620::Value::MIN;
+#endif /* defined(OS_CFGBOOL_DEVICE_AD_AD5620_START_WITH_MIDDLE_VALUE) */
+    }
+
+    Ad5620::~Ad5620()
+    {
+      OSDeviceDebug::putDestructor("device::ad::Ad5620", this);
+    }
+
+    // ----- Public methods ---------------------------------------------------
+
+    void
+    Ad5620::powerUp(void)
+    {
+      m_cs.powerUp();
+      m_spi.powerUp();
+
+      // Start with the maximum clock allowed by the device
+      m_spi.configureClockFrequencyHz(ad5620::ClockFrequencyHz::MAX);
+
+      // SPI Bus Protocol Mode 1 (CPOL=0, CPHA=0)
+      m_spi.configurePolarityAndPhase(0, 0);
+
+      // Start with the previous value (MIDDLE value at start-up)
+      writeValue(m_shadowValue);
+    }
+
+    void
+    Ad5620::powerDown(ad5620::PowerDownMode_t mode)
+    {
+      // The chip has two bits in the value register to
+      // configure the behaviour of the output line in power down mode
+      m_cs.assert();
+        {
+          ad5620::Value_t tmp;
+          tmp = (m_shadowValue << ad5620::Value::SHIFT_BITS) | (mode
+              & ad5620::PowerDownMode::MASK);
+
+          m_spi.writeWaitReadByte(tmp >> 8);
+          m_spi.writeWaitReadByte(tmp & 0xFF);
+        }
+      m_cs.deassert();
+
+      m_spi.powerDown();
+      m_cs.powerDown();
+    }
+
+    void
+    Ad5620::configureClockFrequencyHz(
+        ad5620::ClockFrequencyHz_t clockFrequencyHz)
+    {
+      m_spi.configureClockFrequencyHz(clockFrequencyHz);
+    }
+
+    void
+    Ad5620::writeValue(ad5620::Value_t value)
+    {
+      // Validate input value
+      value = (value & ad5620::Value::VALIDATION_MASK);
+
+      m_cs.assert();
+        {
+          ad5620::Value_t tmp;
+          tmp = (value << ad5620::Value::SHIFT_BITS)
+              | ad5620::PowerDownMode::NORMAL_OPERATION;
+
+          m_spi.writeWaitReadByte(tmp >> 8);
+          m_spi.writeWaitReadByte(tmp & 0xFF);
+        }
+      m_cs.deassert();
+
+      // Store value for further usage
+      m_shadowValue = value;
+    }
+
+    // Return the new value
+    ad5620::Value_t
+    Ad5620::incrementValue(void)
+    {
+      if (m_shadowValue < ad5620::Value::MAX)
+        writeValue(m_shadowValue + 1);
+
+      return m_shadowValue;
+    }
+
+    // Return the new value
+    ad5620::Value_t
+    Ad5620::decrementValue(void)
+    {
+      if (m_shadowValue > 0)
+        writeValue(m_shadowValue - 1);
+
+      return m_shadowValue;
+    }
+
+  // --------------------------------------------------------------------------
+
+  }
+}
+
+#endif /* defined(OS_INCLUDE_DEVICE_AD_AD5620) */
+
