1d94189446a02b1d2f19c94f536a1aa3ddadfe91
Liviu Ionescu <ilg@livius.net>
2011-08-05 20:22:23 +0000

    abstract class added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7352 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/devices/generic/include/ChipSelect.h
@@ -0,0 +1,50 @@
+/*
+ *      Copyright (C) 2011 Liviu Ionescu.
+ *
+ *      This file is part of the uOS++ distribution.
+ */
+
+#ifndef CHIPSELECT_H_
+#define CHIPSELECT_H_
+
+#include "portable/kernel/include/OS.h"
+
+// Interface for Chip Select implementations
+// Use it as a reference for actual implementations.
+
+class ChipSelect
+{
+  // --------------------------------------------------------------------------
+
+public:
+
+  // ----- Constructors & Destructors -----------------------------------------
+
+  ChipSelect(Gpio& gpio);
+  ~ChipSelect();
+
+  // ----- Public methods -----------------------------------------------------
+
+  virtual void
+  powerUp(void) = 0;
+
+  virtual void
+  powerDown(void) = 0;
+
+  virtual void
+  assert(void) = 0;
+
+  virtual void
+  deassert(void) = 0;
+
+private:
+
+  // ----- Private members ----------------------------------------------------
+
+  Gpio& m_gpio;
+
+  // --------------------------------------------------------------------------
+
+};
+
+#endif /* CHIPSELECT_H_ */
