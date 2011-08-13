abcbc488c3d2f00b9f6fa968c586aedd18265182
Liviu Ionescu <ilg@livius.net>
2011-08-13 19:09:49 +0000

    first functional version, still with some Atmel member names (to be fixed)
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7424 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/hal/arch/avr32/uc3/devices/onchip/include/Twim.h
+++ system/hal/arch/avr32/uc3/devices/onchip/include/Twim.h
@@ -12,6 +12,7 @@
 #include "hal/arch/avr32/uc3/devices/onchip/include/Twim_Definitions.h"
 #include "hal/arch/avr32/uc3/devices/onchip/include/Gpio.h"
 #include "hal/arch/avr32/uc3/devices/onchip/include/Intc.h"
+#include "hal/arch/avr32/uc3/devices/onchip/include/Pm.h"
 
 namespace avr32
 {
@@ -24,6 +25,9 @@ namespace avr32
       // ----- Type definitions -----------------------------------------------
 
       typedef uint32_t BusSpeedKbps_t;
+
+      // NOTE: The slave address is the 7 bit address, right aligned,
+      // NOT the 8 bit right aligned with the R/W bit appended.
       typedef uint16_t Address_t;
 
       class BusSpeed
@@ -58,43 +62,49 @@ namespace avr32
       void
       enable(void);
 
+      bool
+      isEnabled(void);
+
       void
       disable();
 
       void
       configureSmbus(uint32_t value);
 
+      // Better do not call it while a transfer is in progress
       OSReturn_t
       configureBusSpeedKbps(BusSpeedKbps_t speed);
 
       // This is the main transfer method, doing a write followed by a read.
       OSReturn_t
-      writeByteArrayReadByteArray(Address_t addr, uint8_t* outgoingBytes,
-          size_t outgoingBytesLength, uint8_t* incomingBytes,
-          size_t incomingBytesSize, size_t* pIncomingBytesLength);
+      writeByteArrayReadByteArray(Address_t slaveAddress,
+          uint8_t* pOutgoingBytes, size_t outgoingBytesLength,
+          uint8_t* pIncomingBytes, size_t incomingBytesSize,
+          size_t* pIncomingBytesLength);
 
       // Sends only the address; if the device is not there, a NACK will
       // be returned
       OSReturn_t
-      probeDevice(Address_t addr);
+      probeDevice(Address_t slaveAddress);
 
       // Write a single byte
       OSReturn_t
-      writeByte(Address_t addr, uint8_t b);
+      writeByte(Address_t slaveAddress, uint8_t byte);
 
       // Write a byte array
       OSReturn_t
-      writeByteArray(Address_t addr, uint8_t* outgoingBytes,
+      writeByteArray(Address_t slaveAddress, uint8_t* pOutgoingBytes,
           size_t outgoingBytesLength);
 
       // Write a single byte and read a byte array
       OSReturn_t
-      writeByteReadByteArray(Address_t addr, uint8_t b, uint8_t* incomingBytes,
-          size_t incomingBytesSize, size_t* pIncomingBytesLength);
+      writeByteReadByteArray(Address_t slaveAddress, uint8_t byte,
+          uint8_t* pIncomingBytes, size_t incomingBytesSize,
+          size_t* pIncomingBytesLength);
 
       // Read into a byte array
       OSReturn_t
-      readByteArray(Address_t addr, uint8_t* incomingBytes,
+      readByteArray(Address_t slaveAddress, uint8_t* pIncomingBytes,
           size_t incomingBytesSize, size_t* pIncomingBytesLength);
 
       // ---
@@ -124,6 +134,12 @@ namespace avr32
       void
       clearStatus(void);
 
+      bool
+      isIdle(void);
+
+      static bool
+      isAddressTenBit(Address_t address);
+
     public:
 
       // ----- Public members -------------------------------------------------
@@ -137,6 +153,29 @@ namespace avr32
       twim::ModuleId_t m_module;
       avr32::uc3::gpio::PinPeripheralFunction_t* m_pGpioConfigurationArray;
 
+      // --
+      volatile uint32_t twim_it_mask;
+      enum twim_transfer_status
+      {
+        TWI_SUCCESS = 0, //!< \brief TWI Transaction Success
+        TWI_INVALID_ARGUMENT = -1, //!< \brief Invalid Argument Passed
+        TWI_ARBITRATION_LOST = -2, //!< \brief Bus Arbitration Lost
+        TWI_NO_CHIP_FOUND = -3, //!< \brief Slave Not Found
+        TWI_RECEIVE_NACK = -4, //!< \brief Data No Acknowledgement Received
+        TWI_SEND_NACK = -5, //!< \brief Data No Acknowledgement Send
+        TWI_INVALID_CLOCK_DIV = -6
+      //!< \brief Invalid Clock Divider Value
+      };
+      typedef enum twim_transfer_status twim_transfer_status_t;
+
+      volatile twim_transfer_status_t transfer_status;
+      volatile bool twim_next; // =false
+      volatile uint8_t *twim_rx_data; // = NULL;
+      volatile uint32_t twim_rx_nb_bytes; // = 0;
+      const volatile uint8_t *twim_tx_data; // = NULL;
+      volatile uint32_t twim_tx_nb_bytes; // = 0;
+      volatile uint32_t twim_rx_ef_bytes; // = 0;
+
       // ----------------------------------------------------------------------
     };
 
@@ -145,14 +184,13 @@ namespace avr32
     inline uint32_t
     Twim::getInputClockFrequencyHz(void)
     {
-      return OS_CFGLONG_PBA_FREQUENCY_HZ;
-      //return OS_CFGLONG_PBB_FREQUENCY_HZ;
+      return Pm::getPbaClockFrequencyHz();
     }
 
     inline void
     Twim::softwareReset(void)
     {
-      // Reset interface. The interface must be enabled
+      // Reset interface. The interface must enabled for this to work
       moduleRegisters.writeControl(AVR32_TWIM_CR_SWRST_MASK);
     }
 
@@ -170,6 +208,18 @@ namespace avr32
       moduleRegisters.writeControl(AVR32_TWIM_CR_MEN_MASK);
     }
 
+    inline bool
+    Twim::isEnabled(void)
+    {
+      return ((moduleRegisters.readStatus() & AVR32_TWIM_SR_MENB_MASK) != 0);
+    }
+
+    inline bool
+    Twim::isIdle(void)
+    {
+      return ((moduleRegisters.readStatus() & AVR32_TWIM_SR_IDLE_MASK) != 0);
+    }
+
     inline void
     Twim::disableAllInterrupts(void)
     {
@@ -182,6 +232,12 @@ namespace avr32
       moduleRegisters.writeStatusClear(~0);
     }
 
+    inline bool
+    Twim::isAddressTenBit(Address_t address)
+    {
+      return (address > 0xFF);
+    }
+
     inline void
     Twim::setGpioConfigurationArray(
         avr32::uc3::gpio::PinPeripheralFunction_t* pGpioConfigurationArray)
@@ -190,39 +246,39 @@ namespace avr32
     }
 
     inline OSReturn_t
-    Twim::probeDevice(Address_t addr)
+    Twim::probeDevice(Address_t slaveAddress)
     {
-      return writeByteArrayReadByteArray(addr, NULL, 0, NULL, 0, NULL);
+      return writeByteArrayReadByteArray(slaveAddress, NULL, 0, NULL, 0, NULL);
     }
 
     inline OSReturn_t
-    Twim::writeByte(Address_t addr, uint8_t b)
+    Twim::writeByte(Address_t slaveAddress, uint8_t byte)
     {
-      return writeByteArrayReadByteArray(addr, &b, 1, NULL, 0, NULL);
+      return writeByteArrayReadByteArray(slaveAddress, &byte, 1, NULL, 0, NULL);
     }
 
     inline OSReturn_t
-    Twim::writeByteArray(Address_t addr, uint8_t* outgoingBytes,
+    Twim::writeByteArray(Address_t slaveAddress, uint8_t* pOutgoingBytes,
         size_t outgoingBytesLength)
     {
-      return writeByteArrayReadByteArray(addr, outgoingBytes,
+      return writeByteArrayReadByteArray(slaveAddress, pOutgoingBytes,
           outgoingBytesLength, NULL, 0, NULL);
     }
 
     inline OSReturn_t
-    Twim::writeByteReadByteArray(Address_t addr, uint8_t b,
-        uint8_t* incomingBytes, size_t incomingBytesSize,
+    Twim::writeByteReadByteArray(Address_t slaveAddress, uint8_t byte,
+        uint8_t* pIncomingBytes, size_t incomingBytesSize,
         size_t* pIncomingBytesLength)
     {
-      return writeByteArrayReadByteArray(addr, &b, 1, incomingBytes,
-          incomingBytesSize, pIncomingBytesLength);
+      return writeByteArrayReadByteArray(slaveAddress, &byte, 1,
+          pIncomingBytes, incomingBytesSize, pIncomingBytesLength);
     }
 
     inline OSReturn_t
-    Twim::readByteArray(Address_t addr, uint8_t* incomingBytes,
+    Twim::readByteArray(Address_t slaveAddress, uint8_t* pIncomingBytes,
         size_t incomingBytesSize, size_t* pIncomingBytesLength)
     {
-      return writeByteArrayReadByteArray(addr, NULL, 0, incomingBytes,
+      return writeByteArrayReadByteArray(slaveAddress, NULL, 0, pIncomingBytes,
           incomingBytesSize, pIncomingBytesLength);
     }
 
--- system/hal/arch/avr32/uc3/devices/onchip/src/Twim.cpp
+++ system/hal/arch/avr32/uc3/devices/onchip/src/Twim.cpp
@@ -12,6 +12,9 @@
 
 #include "hal/arch/avr32/uc3/devices/onchip/include/Twim.h"
 
+//#define OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE  (1)
+//#define OS_DEBUG_AVR32_UC3_TWIM_WRITEBYTEARRAYREADBYTEARRAY (1)
+
 namespace avr32
 {
   namespace uc3
@@ -25,6 +28,9 @@ namespace avr32
                   + (module * twim::ModuleRegisters::MEMORY_OFFSET)))
     {
       OSDeviceDebug::putConstructorWithIndex("avr32::uc3::Twim", module, this);
+      OSDeviceDebug::putString("TWIM Version=");
+      OSDeviceDebug::putHex(moduleRegisters.readVersion());
+      OSDeviceDebug::putNewLine();
 
       m_module = module;
     }
@@ -83,6 +89,7 @@ namespace avr32
       OSCriticalSection::enter();
         {
           disableAllInterrupts();
+          enable();
           softwareReset();
         }
       OSCriticalSection::exit();
@@ -144,21 +151,322 @@ namespace avr32
     }
 
     OSReturn_t
-    Twim::writeByteArrayReadByteArray(Address_t addr, uint8_t* outgoingBytes,
-        size_t outgoingBytesLength, uint8_t* incomingBytes,
-        size_t incomingBytesSize, size_t* pIncomingBytesLength)
+    Twim::writeByteArrayReadByteArray(Address_t slaveAddress,
+        uint8_t* pOutgoingBytes, size_t outgoingBytesLength,
+        uint8_t* pIncomingBytes, size_t incomingBytesSize,
+        size_t* pIncomingBytesLength)
     {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_WRITEBYTEARRAYREADBYTEARRAY)
       OSDeviceDebug::putString("I2C addr=");
-      OSDeviceDebug::putHex(addr);
+      OSDeviceDebug::putHex(slaveAddress);
       OSDeviceDebug::putString(", wl=");
       OSDeviceDebug::putDec(outgoingBytesLength);
       OSDeviceDebug::putString(", rl=");
       OSDeviceDebug::putDec(incomingBytesSize);
       OSDeviceDebug::putNewLine();
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_WRITEBYTEARRAYREADBYTEARRAY) */
+
+      // Avoid null pointer references
+      if (pOutgoingBytes == NULL)
+        outgoingBytesLength = 0;
+
+      if (pIncomingBytes == NULL)
+        incomingBytesSize = 0;
+
+      if (!isEnabled())
+        return OSReturn::OS_NOT_ENABLED;
+
+      bool isTenBit;
+      isTenBit = isAddressTenBit(slaveAddress);
+
+      // Reset the TWIM module to clear the THR register
+      enable();
+      softwareReset();
+      disable();
+
+      // Disable the TWIM interrupts
+      disableAllInterrupts();
+      clearStatus();
+
+      OSCriticalSection::enter();
+
+      // point to outgoing buffer
+      twim_tx_data = pOutgoingBytes;
+
+      // set the number of bytes to transmit
+      twim_tx_nb_bytes = outgoingBytesLength;
+
+      // point to incoming buffer
+      twim_rx_data = pIncomingBytes;
+
+      // set the number of bytes to receive
+      twim_rx_nb_bytes = incomingBytesSize;
+
+      // Initialize bus transfer status
+      transfer_status = TWI_SUCCESS;
+
+      twim_rx_ef_bytes = 0;
+
+      if (incomingBytesSize == 0)
+        {
+          // Write only call
+
+          // Set next transfer to false
+          twim_next = false;
+
+          // set the command to start the write transfer
+          moduleRegisters.writeCommand(
+              (slaveAddress << AVR32_TWIM_CMDR_SADR_OFFSET)
+                  | ((outgoingBytesLength & 0xFF)
+                      << AVR32_TWIM_CMDR_NBYTES_OFFSET)
+                  | (AVR32_TWIM_CMDR_VALID_MASK) | (AVR32_TWIM_CMDR_START_MASK)
+                  | (AVR32_TWIM_CMDR_STOP_MASK) | ((isTenBit ? 1 : 0)
+                  << AVR32_TWIM_CMDR_TENBIT_OFFSET) | (0
+                  << AVR32_TWIM_CMDR_READ_OFFSET));
+
+          moduleRegisters.writeNextCommand(0);
+
+          //OSDeviceDebug::putString(" 3 ");
+
+          if (outgoingBytesLength != 0)
+            {
+              // enable TXRDY interrupts
+              twim_it_mask = AVR32_TWIM_IER_TXRDY_MASK;
+            }
+        }
+      else
+        {
+          if (outgoingBytesLength == 0)
+            {
+              // Read only call
+
+              // Set next transfer to false
+              twim_next = false;
+
+              // set the command to start the read transfer
+              moduleRegisters.writeCommand(
+                  (slaveAddress << AVR32_TWIM_CMDR_SADR_OFFSET)
+                      | ((incomingBytesSize & 0xFF)
+                          << AVR32_TWIM_CMDR_NBYTES_OFFSET)
+                      | (AVR32_TWIM_CMDR_VALID_MASK)
+                      | (AVR32_TWIM_CMDR_START_MASK)
+                      | (AVR32_TWIM_CMDR_STOP_MASK) | ((isTenBit ? 1 : 0)
+                      << AVR32_TWIM_CMDR_TENBIT_OFFSET)
+                      | (AVR32_TWIM_CMDR_READ_MASK));
+
+              moduleRegisters.writeNextCommand(0);
+
+              //OSDeviceDebug::putString(" 4 ");
+
+              // mask RXRDY interrupts
+              twim_it_mask = AVR32_TWIM_IER_RXRDY_MASK;
+            }
+          else
+            {
+              // Write followed by read call
+
+              // set the command to start the write transfer
+              moduleRegisters.writeCommand(
+                  (slaveAddress << AVR32_TWIM_CMDR_SADR_OFFSET)
+                      | ((outgoingBytesLength & 0xFF)
+                          << AVR32_TWIM_CMDR_NBYTES_OFFSET)
+                      | (AVR32_TWIM_CMDR_VALID_MASK)
+                      | (AVR32_TWIM_CMDR_START_MASK) | (0
+                      << AVR32_TWIM_CMDR_STOP_OFFSET) | ((isTenBit ? 1 : 0)
+                      << AVR32_TWIM_CMDR_TENBIT_OFFSET) | (0
+                      << AVR32_TWIM_CMDR_READ_OFFSET));
+
+              // set the command to start the read transfer
+              moduleRegisters.writeNextCommand(
+                  (slaveAddress << AVR32_TWIM_CMDR_SADR_OFFSET)
+                      | ((incomingBytesSize & 0xFF)
+                          << AVR32_TWIM_CMDR_NBYTES_OFFSET)
+                      | (AVR32_TWIM_CMDR_VALID_MASK)
+                      | (AVR32_TWIM_CMDR_START_MASK)
+                      | (AVR32_TWIM_CMDR_STOP_MASK) | ((isTenBit ? 1 : 0)
+                      << AVR32_TWIM_CMDR_TENBIT_OFFSET)
+                      | (AVR32_TWIM_CMDR_READ_MASK));
+
+              // mask NACK and TXRDY and RXRDY interrupts
+              twim_it_mask = AVR32_TWIM_IER_TXRDY_MASK
+                  | AVR32_TWIM_IER_RXRDY_MASK;
+
+              //OSDeviceDebug::putString(" 6 ");
+              //OSDeviceDebug::putHex(moduleRegisters.readNextCommand());
+
+            }
+        }
+      //OSDeviceDebug::putString(" 7 ");
+
+      // Enable NACK anc CCOMP too
+      twim_it_mask |= AVR32_TWIM_IER_ANAK_MASK | AVR32_TWIM_IER_DNAK_MASK
+          | AVR32_TWIM_IER_CCOMP_MASK;
+      // update IMR through IER
+      moduleRegisters.writeInterruptEnable(twim_it_mask);
+
+      //OSDeviceDebug::putString(" 8 ");
+      enable();
+
+      OSCriticalSection::exit();
+
+#if true
+      //OSDeviceDebug::putString(" wb ");
+
+      // send data
+      while ((transfer_status == TWI_SUCCESS) && !isIdle())
+        {
+          OSDeviceDebug::putChar('w');
+          OSScheduler::yield();
+        }
+
+      //OSDeviceDebug::putString(" we ");
+#endif
+
+      if (pIncomingBytesLength != NULL)
+        *pIncomingBytesLength = twim_rx_ef_bytes;
+
+      if (transfer_status == TWI_RECEIVE_NACK)
+        {
+          return OSReturn::OS_NACK;
+        }
+
+      if (transfer_status != TWI_SUCCESS) // TWI_ARBITRATION_LOST)
+        {
+          return OSReturn::OS_ERROR;
+        }
 
       return OSReturn::OS_OK;
     }
 
+    // ----- Interrupt service routine ----------------------------------------
+
+    void
+    Twim::interruptServiceRoutine(void)
+    {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+      OSDeviceDebug::putChar('#');
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+      // get masked status register value
+      twim::Status_t status;
+      status = moduleRegisters.readStatus();
+
+      uint16_t imr;
+      imr = moduleRegisters.readInterruptMask();
+
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+      OSDeviceDebug::putHex(status);
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+      // Is this a NACK?
+      if (((status & (AVR32_TWIM_SR_ANAK_MASK | AVR32_TWIM_SR_DNAK_MASK
+          | AVR32_TWIM_SR_ARBLST_MASK)) != 0) && ((imr
+          & (AVR32_TWIM_IER_ANAK_MASK | AVR32_TWIM_IER_DNAK_MASK)) != 0))
+        {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putString(" nak ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+          // if we get a nak, clear the valid bit in cmdr,
+          // otherwise the command will be resent.
+
+          transfer_status = (status & (AVR32_TWIM_IER_ANAK_MASK
+              | AVR32_TWIM_IER_DNAK_MASK)) ? TWI_RECEIVE_NACK
+              : TWI_ARBITRATION_LOST;
+
+          moduleRegisters.writeCommand(
+              moduleRegisters.readCommand() & ~AVR32_TWIM_CMDR_VALID_MASK);
+
+          clearStatus();
+          disableAllInterrupts();
+        }
+      else if ((status & AVR32_TWIM_SR_CCOMP_MASK) != 0 && ((imr
+          & AVR32_TWIM_IER_CCOMP_MASK) != 0))
+        {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putString(" cmd ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+          moduleRegisters.writeStatusClear(AVR32_TWIM_SR_CCOMP_MASK);
+        }
+      // Is this a RXRDY?
+      else if ((status & AVR32_TWIM_SR_RXRDY_MASK) != 0 && ((imr
+          & AVR32_TWIM_IER_RXRDY_MASK) != 0))
+        {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putString(" rx ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+          uchar_t ch;
+
+          ch = moduleRegisters.readReceiveHolding();
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putHex(ch);
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+          // get data from Receive Holding Register
+          *twim_rx_data = ch;
+          twim_rx_data++;
+
+          twim_rx_ef_bytes++;
+
+          // decrease received bytes number
+          twim_rx_nb_bytes--;
+
+          // receive complete
+          if (twim_rx_nb_bytes == 0)
+            {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+              OSDeviceDebug::putString(" drx ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+              // finish the receive operation
+              moduleRegisters.writeInterruptDisable(AVR32_TWIM_IDR_RXRDY_MASK);
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+              OSDeviceDebug::putHex(moduleRegisters.readInterruptMask());
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+            }
+        }
+
+      // Is this a TXRDY?
+      else if ((status & AVR32_TWIM_SR_TXRDY_MASK) != 0 && ((imr
+          & AVR32_TWIM_IER_TXRDY_MASK) != 0))
+        {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putString(" tx ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+
+          // no more bytes to transmit
+          if (twim_tx_nb_bytes == 0)
+            {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+              OSDeviceDebug::putString(" dtx ");
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+              // finish the transmit operation
+              moduleRegisters.writeInterruptDisable(AVR32_TWIM_IDR_TXRDY_MASK);
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+              OSDeviceDebug::putHex(moduleRegisters.readInterruptMask());
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+            }
+          else
+            {
+              uchar_t ch;
+              ch = *twim_tx_data++;
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+              OSDeviceDebug::putHex(ch);
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+              // put the byte in the Transmit Holding Register
+              moduleRegisters.writeTransmitHolding(ch);
+              // decrease transmited bytes number
+              twim_tx_nb_bytes--;
+            }
+        }
+      else
+        {
+#if defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE)
+          OSDeviceDebug::putChar('?');
+#endif /* defined(DEBUG) && defined(OS_DEBUG_AVR32_UC3_TWIM_INTERUPTSERVICEROUTINE) */
+        }
+    }
 
   // --------------------------------------------------------------------------
 
