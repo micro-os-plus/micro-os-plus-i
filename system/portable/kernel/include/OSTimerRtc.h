3d253788048c2960dc14ffe00d95bc1d8b664c23
Liviu Ionescu <ilg@livius.net>
2011-09-29 17:11:09 +0000

    OSTimerRtc added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7601 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

---
+++ system/portable/kernel/include/OSTimerRtc.h
@@ -0,0 +1,95 @@
+/*
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#ifndef OSTIMERRTC_H_
+#define OSTIMERRTC_H_
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if defined(OS_INCLUDE_OSTIMERRTC)
+
+#include "portable/kernel/include/OS.h"
+
+#if !defined(OS_CFGINT_OSTIMERTC_SIZE)
+#define OS_CFGINT_OSTIMERTC_SIZE 	(OS_CFGINT_THREADS_ARRAY_SIZE)
+#endif
+
+// the second system timer functionality, intended to measure longer intervals
+class OSTimerRtc : public OSTimer
+{
+public:
+  // initialise internal structures
+  OSTimerRtc();
+
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+  // increment the current seconds number
+  static void incrementUptime(void);
+  // return the current seconds number
+  static unsigned long getUptime(void);
+#endif
+
+#if defined(OS_INCLUDE_OSTHREAD_VIRTUALWATCHDOG)
+  // check for every thread if the software watchdog (counter) expired
+  static void checkVirtualWatchdogs(void);
+#endif
+
+  // interrupt service routine called each second
+  void
+  interruptServiceRoutine(void);
+
+protected:
+  // OSScheduler calls OSTimerSeconds::initialise() from OSScheduler::start()
+  friend class OSScheduler;
+
+  // initialise and it is called from OSScheduler::start()
+  static void
+  init(void);
+  static void
+  implAcknowledgeInterrupt(void);
+
+private:
+  // contain the timeouts(expressed in ticks) for every alarm
+  static OSTimerStruct_t m_array[OS_CFGINT_OSTIMERTC_SIZE];
+
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+  // current seconds number
+  static OSTime_t ms_uptime;
+#endif
+};
+
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+
+inline void OSTimerRtc::incrementUptime(void)
+  {
+    ms_uptime++;
+  }
+
+inline OSTime_t OSTimerRtc::getUptime(void)
+  {
+    return ms_uptime;
+  }
+
+#endif
+
+inline void
+OSTimerRtc::interruptServiceRoutine(void)
+{
+  implAcknowledgeInterrupt();
+  interruptTick();
+  incrementTicks();
+
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+  incrementUptime();
+#endif
+
+#if defined(OS_INCLUDE_OSTHREAD_VIRTUALWATCHDOG)
+  checkVirtualWatchdogs();
+#endif
+}
+
+#endif
+
+#endif /* OSTIMERRTC_H_ */
---
+++ system/portable/kernel/src/OSTimerRtc.cpp
@@ -0,0 +1,48 @@
+/*
+ *      Copyright (C) 2007-2011 Liviu Ionescu.
+ *
+ *	This file is part of the uOS++ distribution.
+ */
+
+#include "portable/kernel/include/OS_Defines.h"
+
+#if !defined(OS_EXCLUDE_MULTITASKING) && !defined(OS_EXCLUDE_OSTIMER)
+
+#if defined(OS_INCLUDE_OSTIMERRTC)
+
+#include "portable/kernel/include/OS.h"
+
+#include "portable/kernel/include/OSTimerRtc.h"
+
+// static members
+OSTimerStruct_t OSTimerRtc::m_array[OS_CFGINT_OSTIMERTC_SIZE];
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+OSTime_t OSTimerRtc::ms_uptime;
+#endif
+
+OSTimerRtc::OSTimerRtc() :
+  OSTimer(&m_array[0], sizeof(m_array) / sizeof(m_array[0]))
+{
+  OSDeviceDebug::putConstructor_P(PSTR("OSTimerRtc"), this);
+
+#if defined(OS_INCLUDE_OSTIMERRTC_UPTIME)
+  ms_uptime = 0;
+#endif
+  ;
+}
+
+#if defined(OS_INCLUDE_OSTHREAD_VIRTUALWATCHDOG)
+void OSTimerRtc::checkVirtualWatchdogs(void)
+  {
+    for (int i=0; i < OSScheduler::getThreadsCount(); ++i)
+      {
+        OSThread* pThread;
+        pThread = OSScheduler::getThread(i);
+        pThread->virtualWatchdogCheck();
+      }
+  }
+#endif
+
+#endif /* OS_INCLUDE_OSTIMERRTC */
+
+#endif /* !defined(OS_EXCLUDE_MULTITASKING) && !defined(OS_EXCLUDE_OSTIMER) */
