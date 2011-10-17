dd18d6cb5d32a84b8f2d48420665e50449b42d0a
Liviu Ionescu <ilg@livius.net>
2011-10-17 19:38:39 +0000

    nmea parsing added; hdop & satellites added
    git-svn-id: https://micro-os-plus.svn.sourceforge.net/svnroot/micro-os-plus/trunk/Eclipse@7656 ba4b06f3-5e2e-0410-8656-f0fcc57e5dc0

--- system/portable/misc/include/GpsPosition.h
+++ system/portable/misc/include/GpsPosition.h
@@ -13,12 +13,14 @@
 #include "portable/stdlib/include/ostream"
 #endif /* defined(OS_INCLUDE_OSTREAM_GPSPOSITION) */
 
-typedef int16_t GpsAltitude_t; // +/- 32Km
+typedef int32_t GpsAltitude_t; // +/- 32Km * 10
 typedef uchar_t GpsHemisphere_t; // N/S, E/W
-typedef uint8_t GpsDegrees_t; // 0-179
+typedef uint8_t GpsDegrees_t; // 0-179 (0-89)
 typedef uint8_t GpsMinutesInteger_t; // 0-59
 typedef uint16_t GpsMinutesDecimals_t; // 0-9999
-typedef uint16_t GpsFix_t;
+typedef uint8_t GpsFixStatus_t;
+typedef uint8_t GpsSatellitesInView_t;
+typedef uint_t GpsHdop_t; // >1, * 100
 
 class GpsCoordinate;
 typedef GpsCoordinate GpsCoordinate_t;
@@ -60,6 +62,14 @@ public:
   void
   setMinutesDecimals(GpsMinutesDecimals_t minutesDecimals);
 
+protected:
+
+  OSReturn_t
+  parseDegrees(const char* pString, size_t length);
+
+  OSReturn_t
+  parseMinutes(const char* pString);
+
 private:
   GpsHemisphere_t m_hemisphere; // N/S, E/W
   GpsDegrees_t m_degrees; // 0-89, 0-179
@@ -84,6 +94,11 @@ public:
   setHemisphereNorth(void);
   void
   setHemisphereSouth(void);
+
+  // Parse NMEA usual format ddmm.mmmm,N/S
+  OSReturn_t
+  parseNmea(const char* pString, const char* pHemisphere);
+
 };
 
 // ----------------------------------------------------------------------------
@@ -103,6 +118,10 @@ public:
   setHemisphereEst(void);
   void
   setHemisphereWest(void);
+
+  // Parse NMEA usual format dddmm.mmmm,E/W
+  OSReturn_t
+  parseNmea(const char* pString, const char* pHemisphere);
 };
 
 // ----------------------------------------------------------------------------
@@ -133,17 +152,43 @@ public:
   void
   setAltitude(GpsAltitude_t m_altitude);
 
-  GpsFix_t
-  getFix();
+  bool
+  hasAltitude(void);
+
+  GpsFixStatus_t
+  getFixStatus();
 
   void
-  setFix(GpsFix_t fix);
+  setFixStatus(GpsFixStatus_t fix);
+
+  GpsSatellitesInView_t
+  getSatellitesInView(void);
+
+  void
+  setSatellitesInView(GpsSatellitesInView_t satellites);
+
+  GpsHdop_t
+  getHdop(void);
+
+  void
+  setHdop(GpsHdop_t hdop);
 
 private:
   GpsLatitude_t m_latitude; // N/S
   GpsLongitude_t m_longitude; // E/W
-  GpsAltitude_t m_altitude; // altitude in meters, may be negative
-  GpsFix_t m_fix;
+  GpsAltitude_t m_altitude; // altitude in meters * 10, may be negative
+  bool m_hasAltitude;
+  GpsFixStatus_t m_fixStatus;
+
+  GpsSatellitesInView_t m_satellites; // 0-12
+  GpsHdop_t m_hdop; // HDOP * 100
+  // 1 = ideal
+  // 1-2 = excellent
+  // 2-5 = good
+  // 5-10 = moderate
+  // 10-20 = fair
+  // >20 = poor
+
 };
 
 // ----------------------------------------------------------------------------
@@ -273,22 +318,46 @@ GpsPosition::getAltitude() const
   return m_altitude;
 }
 
-inline void
-GpsPosition::setAltitude(GpsAltitude_t altitude)
+inline bool
+GpsPosition::hasAltitude(void)
 {
-  m_altitude = altitude;
+  return m_hasAltitude;
 }
 
-inline GpsFix_t
-GpsPosition::getFix()
+inline GpsFixStatus_t
+GpsPosition::getFixStatus()
 {
-  return m_fix;
+  return m_fixStatus;
 }
 
 inline void
-GpsPosition::setFix(GpsFix_t fix)
+GpsPosition::setFixStatus(GpsFixStatus_t fix)
 {
-  m_fix = fix;
+  m_fixStatus = fix;
+}
+
+inline GpsSatellitesInView_t
+GpsPosition::getSatellitesInView(void)
+{
+  return m_satellites;
+}
+
+inline void
+GpsPosition::setSatellitesInView(GpsSatellitesInView_t satellites)
+{
+  m_satellites = satellites;
+}
+
+inline GpsHdop_t
+GpsPosition::getHdop(void)
+{
+  return m_hdop;
+}
+
+inline void
+GpsPosition::setHdop(GpsHdop_t hdop)
+{
+  m_hdop = hdop;
 }
 
 // ----------------------------------------------------------------------------
--- system/portable/misc/src/GpsPosition.cpp
+++ system/portable/misc/src/GpsPosition.cpp
@@ -26,15 +26,103 @@ GpsCoordinate::~GpsCoordinate()
   debug.putDestructor_P(PSTR("GpsCoordinate"), this);
 }
 
+OSReturn_t
+GpsCoordinate::parseDegrees(const char* pString, size_t length)
+{
+  // Parse longitude 'ddd' (0-179) or latitude 'dd' (0-79)
+  for (size_t i = 0; i < length; ++i)
+    {
+      uchar_t ch;
+      ch = *pString++;
+
+      uint8_t b;
+      if (('0' <= ch) && (ch <= '9'))
+        {
+          b = ch - '0';
+        }
+      else
+        {
+          OSDeviceDebug::putString(" BAD_PARAMETER_(deg) ");
+          return OSReturn::OS_BAD_PARAMETER;
+        }
+
+      if (i == 0)
+        {
+          m_degrees = b;
+        }
+      else
+        {
+          // Normally there are only 2-3 iterations here
+          m_degrees = m_degrees * 10 + b;
+        }
+    }
+
+  return OSReturn::OS_OK;
+}
+
+OSReturn_t
+GpsCoordinate::parseMinutes(const char* pString)
+{
+  // Parse minutes with decimals 'mm.mmmm'
+  for (size_t i = 0; i < 7; ++i)
+    {
+      uchar_t ch;
+      ch = *pString++;
+
+      uint8_t b;
+      if (('0' <= ch) && (ch <= '9'))
+        {
+          b = ch - '0';
+        }
+      else if (i != 2)
+        {
+          OSDeviceDebug::putString(" BAD_PARAMETER_(min) ");
+          return OSReturn::OS_BAD_PARAMETER;
+        }
+
+      switch (i)
+        {
+      case 0:
+        m_minutesInteger = b;
+        break;
+
+      case 1:
+        m_minutesInteger = m_minutesInteger * 10 + b;
+        break;
+
+      case 2:
+        if (ch != '.')
+          {
+            OSDeviceDebug::putString(" BAD_PARAMETER_(min.) ");
+            return OSReturn::OS_BAD_PARAMETER;
+          }
+        break;
+
+      case 3:
+        m_minutesDecimals = b;
+        break;
+
+      case 4:
+      case 5:
+      case 6:
+        m_minutesDecimals = m_minutesDecimals * 10 + b;
+        break;
+        }
+    }
+  return OSReturn::OS_OK;
+}
+
 #if defined(OS_INCLUDE_OSTREAM_GPSPOSITION)
 
 std::ostream&
 operator <<(std::ostream& out, const GpsCoordinate& coordinate)
 {
   out << coordinate.getHemisphere() << std::dec
-      << (uint_t) coordinate.getDegrees() << "°"
-      << (uint_t) coordinate.getMinutesInteger() << "."
-      << coordinate.getMinutesDecimals() << "'";
+      << (uint_t) coordinate.getDegrees() << "°";
+  out.width(2);
+  out << (uint_t) coordinate.getMinutesInteger() << ".";
+  out.width(4);
+  out << coordinate.getMinutesDecimals() << "'";
 
   return out;
 }
@@ -55,6 +143,22 @@ GpsLatitude::~GpsLatitude()
   debug.putDestructor("GpsLatitude", this);
 }
 
+OSReturn_t
+GpsLatitude::parseNmea(const char* pString, const char* pHemisphere)
+{
+  OSReturn_t ret;
+
+  ret = parseDegrees(pString, 2);
+  if (ret == OSReturn::OS_OK)
+    {
+      ret = parseMinutes(pString + 2);
+    }
+
+  setHemisphere(pHemisphere[0]);
+
+  return ret;
+}
+
 // ----------------------------------------------------------------------------
 
 GpsLongitude::GpsLongitude()
@@ -69,11 +173,32 @@ GpsLongitude::~GpsLongitude()
   debug.putDestructor("GpsLongitude", this);
 }
 
+OSReturn_t
+GpsLongitude::parseNmea(const char* pString, const char* pHemisphere)
+{
+  OSReturn_t ret;
+
+  ret = parseDegrees(pString, 3);
+  if (ret == OSReturn::OS_OK)
+    {
+      ret = parseMinutes(pString + 3);
+    }
+
+  setHemisphere(pHemisphere[0]);
+
+  return ret;
+}
+
 // ----------------------------------------------------------------------------
 
 GpsPosition::GpsPosition()
 {
   debug.putConstructor("GpsPosition", this);
+
+  m_hasAltitude = false;
+  m_fixStatus = 0;
+  m_satellites = 0;
+  m_hdop = 0;
 }
 
 GpsPosition::~GpsPosition()
@@ -81,13 +206,33 @@ GpsPosition::~GpsPosition()
   debug.putDestructor("GpsPosition", this);
 }
 
+void
+GpsPosition::setAltitude(GpsAltitude_t altitude)
+{
+  m_altitude = altitude;
+  m_hasAltitude = true;
+}
+
 #if defined(OS_INCLUDE_OSTREAM_GPSPOSITION)
 
 std::ostream&
 operator <<(std::ostream& out, GpsPosition& position)
 {
-  out << position.getLatitude() << ", " << position.getLongitude() << ", "
-      << position.getAltitude() << "m";
+  out << position.getLatitude() << ", " << position.getLongitude();
+
+  if (position.hasAltitude())
+    {
+      out << ", " << (position.getAltitude() / 10) << "."
+          << (position.getAltitude() % 10) << " m";
+    }
+
+  if (position.getHdop() != 0)
+    {
+      out << ", " << (position.getHdop() / 100) << ".";
+      out.width(2);
+      out << (position.getHdop() % 100);
+      out << " hdop";
+    }
 
   return out;
 }
